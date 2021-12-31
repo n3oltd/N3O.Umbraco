cf79c0aa7ec8914883cd11ff818e4fa5e89a893d
Noor Drummond <noor.drummond@n3o.ltd>
2021-12-31 08:03:20 +0000

    Fix null reference crash

--- src/Monitoring/N3O.Umbraco.Monitoring.Sentry/SentryWebHostBuilderExtension.cs
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/SentryWebHostBuilderExtension.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Composing;
+using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Hosting;
 
 namespace N3O.Umbraco.Monitoring.Sentry {
     public class SentryWebHostBuilderExtension : IWebHostBuilderExtension {
         public void Run(IWebHostBuilder webBuilder) {
-            if (Composer.WebHostEnvironment.IsProduction()) {
+            if (webBuilder.GetSetting(WebHostDefaults.EnvironmentKey).EqualsInvariant(Environments.Production)) {
                 webBuilder.UseSentry(opt => {
                     opt.AttachStacktrace = true;
                     opt.Environment = Composer.WebHostEnvironment.EnvironmentName;
