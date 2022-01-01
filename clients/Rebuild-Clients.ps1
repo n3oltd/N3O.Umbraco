9f72db982b94c2b0f9f952f12896aad76be44fce
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-01 09:52:55 +0000

    Update clients

--- clients/Rebuild-Clients.ps1
+++ clients/Rebuild-Clients.ps1
@@ -1,1 +1,1 @@
-n3o-tool clients --url "https://localhost:5001/swagger/Cart/swagger.json" --language "TypeScript" --output-path "@n3o/umbraco-cropper-client" --name "UmbracoCropperClient" --package-name "@n3o/umbraco-cropper-client" --package-description "TypeScript client for the N3O Umbraco Cropper"
+n3o-tool clients --url "https://localhost:5001/swagger/Cart/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-cropper-client" --name "UmbracoCropperClient" --package-name "@n3oltd/umbraco-cropper-client" --package-description "TypeScript client for the N3O Umbraco Cropper"
