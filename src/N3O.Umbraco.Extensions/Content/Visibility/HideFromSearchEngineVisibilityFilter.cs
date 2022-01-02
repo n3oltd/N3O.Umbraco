2616bea75b81845d0697596cc20f666efa9d500f
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-02 13:36:51 +0000

    Fix class + file name

--- src/N3O.Umbraco.Extensions/Content/Visibility/HideFromSearchEngineVisibilityFilter.cs
+++ src/N3O.Umbraco.Extensions/Content/Visibility/HideFromSearchEngineVisibilityFilter.cs
@@ -2,7 +2,7 @@ using N3O.Umbraco.Extensions;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace N3O.Umbraco.Content {
-    public class ContentTypeVisibilityFilter : IContentVisibilityFilter {
+    public class HideFromSearchEngineVisibilityFilter : IContentVisibilityFilter {
         public bool IsFilterFor(IPublishedContent content) => true;
 
         public bool IsVisible(IPublishedContent content) {
