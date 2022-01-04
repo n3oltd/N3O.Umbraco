9288764970fdc39f3379cfb6e813cce8600b5580
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-04 18:37:07 +0000

    Fix API name

--- src/Plugins/Uploader/N3O.Umbraco.Uploader/UploaderConstants.cs
+++ src/Plugins/Uploader/N3O.Umbraco.Uploader/UploaderConstants.cs
@@ -1,6 +1,6 @@
 namespace N3O.Umbraco.Uploader {
     public static class UploaderConstants {
-        public const string ApiName = "Cropper";
+        public const string ApiName = "Uploader";
         public const string PropertyEditorAlias = "N3O.Umbraco.Uploader";
     }
 }
