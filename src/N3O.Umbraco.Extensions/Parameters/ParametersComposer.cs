6f654f405c96bf7332273b9db353503258269a63
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-06 17:14:09 +0000

    Fix registrations

--- src/N3O.Umbraco.Extensions/Parameters/ParametersComposer.cs
+++ src/N3O.Umbraco.Extensions/Parameters/ParametersComposer.cs
@@ -9,9 +9,7 @@ namespace N3O.Umbraco.Parameters {
             builder.Services.AddScoped<INamedParameterBinder, NamedParameterBinder>();
             builder.Services.AddScoped<IParameterDataSource, HttpParameterDataSource>();
         
-            builder.Services.AddScoped<FluentParameters, FluentParameters>();
-            builder.Services.AddScoped<IParameterDataSource>(serviceProvider => serviceProvider.GetRequiredService<FluentParameters>());
-            builder.Services.AddScoped<IFluentParameters>(serviceProvider => serviceProvider.GetRequiredService<FluentParameters>());
+            builder.Services.AddTransient<IFluentParameters, FluentParameters>();
 
             RegisterAll(t => t.ImplementsInterface<INamedParameter>(),
                         t => builder.Services.AddScoped(t, serviceProvider => {
