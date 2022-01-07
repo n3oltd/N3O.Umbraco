f04bf4d18d4f7cf74a1e96ca451ee9e37bc3177a
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-07 11:50:13 +0000

    Added JSON converter

---
+++ src/Plugins/Cropper/N3O.Umbraco.Cropper/Services/CroppedImageJsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using N3O.Umbraco.Cropper.Models;
+using N3O.Umbraco.Extensions;
+using Newtonsoft.Json;
+using System;
+
+namespace N3O.Umbraco.Cropper {
+    public class CroppedImageJsonConverter : JsonConverter {
+        public override bool CanRead => false;
+
+        public override bool CanConvert(Type objectType) {
+            return objectType == typeof(CroppedImage);
+        }
+
+        public override object ReadJson(JsonReader reader,
+                                        Type objectType,
+                                        object existingValue,
+                                        JsonSerializer serializer) {
+            throw new NotImplementedException();
+        }
+        
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            if (value != null) {
+                var croppedImage = (CroppedImage) value;
+
+                if (croppedImage.IsSingle()) {
+                    serializer.Serialize(writer, croppedImage.Crop);
+                } else {
+                    writer.WriteStartArray();
+
+                    foreach (var crop in croppedImage) {
+                        serializer.Serialize(writer, crop);
+                    }
+                    
+                    writer.WriteEndArray();
+                }
+            }
+        }
+    }
+}
