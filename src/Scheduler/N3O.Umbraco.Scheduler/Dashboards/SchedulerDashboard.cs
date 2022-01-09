40b54a26588d69132c35636489045e30ffe0c7c4
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-09 14:40:43 +0000

    Added dashboard

---
+++ src/Scheduler/N3O.Umbraco.Scheduler/Dashboards/SchedulerDashboard.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Core.Dashboards;
+
+namespace N3O.Umbraco.Scheduler.Dashboards {
+    public class SchedulerDashboard : IDashboard {
+        public string Alias => "schedulerDashboard";
+
+        public string[] Sections => new[] {
+            global::Umbraco.Cms.Core.Constants.Applications.Settings
+        };
+
+        public string View => "/App_Plugins/N3O.Umbraco.Scheduler/N3O.Umbraco.Scheduler.html";
+
+        public IAccessRule[] AccessRules => new[] {
+            new AccessRule {
+                Type = AccessRuleType.Grant, Value = "admin"
+            }
+        };
+    }
+}
