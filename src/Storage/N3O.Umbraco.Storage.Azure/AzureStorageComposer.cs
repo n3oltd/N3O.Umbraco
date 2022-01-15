f412409047cfc2bb2712e0279291e5842edb5a34
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-15 17:03:27 +0000

    Detect if storage configured

--- src/Storage/N3O.Umbraco.Storage.Azure/AzureStorageComposer.cs
+++ src/Storage/N3O.Umbraco.Storage.Azure/AzureStorageComposer.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Composing;
+using N3O.Umbraco.Extensions;
+using System.Linq;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
 using Umbraco.StorageProviders.AzureBlob;
@@ -9,9 +10,14 @@ using Umbraco.StorageProviders.AzureBlob;
 namespace N3O.Umbraco.Storage.Azure {
     public class AzureStorageComposer : Composer {
         public override void Compose(IUmbracoBuilder builder) {
-            if (!WebHostEnvironment.IsDevelopment()) {
-                builder.AddAzureBlobMediaFileSystem()
-                       .AddCdnMediaUrlProvider();
+            var storageConfigured = builder.Config
+                                           .GetSection("umbraco")
+                                           .GetChildren()
+                                           .Any(x => x.Key.EqualsInvariant("storage") &&
+                                                     x.GetChildren().Any(c => c.Key.EqualsInvariant("azureBlob")));
+
+            if (storageConfigured) {
+                builder.AddAzureBlobMediaFileSystem();
 
                 builder.Services.Configure<UmbracoPipelineOptions>(options => {
                     var filter = new UmbracoPipelineFilter("AzureStorage");
