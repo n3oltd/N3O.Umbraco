e11956e0c42c5d666164dffef505734fc7ed35c1
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-19 14:04:35 +0000

    Pass cart id when creating cart

--- src/Giving/N3O.Umbraco.Giving.Cart/Entities/Cart/Cart.Create.cs
+++ src/Giving/N3O.Umbraco.Giving.Cart/Entities/Cart/Cart.Create.cs
@@ -1,11 +1,13 @@
+using N3O.Umbraco.Entities;
 using N3O.Umbraco.Financial;
 using N3O.Umbraco.Giving.Allocations.Lookups;
 using N3O.Umbraco.Giving.Cart.Models;
+using System;
 
 namespace N3O.Umbraco.Giving.Cart.Entities {
     public partial class Cart {
-        public static Cart Create(Currency currency) {
-            var cart = new Cart();
+        public static Cart Create(Guid cartId, Currency currency) {
+            var cart = Entity.Create<Cart>(cartId);
             cart.Currency = currency;
             cart.Single = CartContents.Create(currency, DonationTypes.Single);
             cart.Regular = CartContents.Create(currency, DonationTypes.Regular);
--- src/Giving/N3O.Umbraco.Giving.Cart/Services/CartAccessor.cs
+++ src/Giving/N3O.Umbraco.Giving.Cart/Services/CartAccessor.cs
@@ -1,5 +1,6 @@
 ﻿using N3O.Umbraco.Context;
 using N3O.Umbraco.Entities;
+using N3O.Umbraco.Locks;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,29 +10,36 @@ namespace N3O.Umbraco.Giving.Cart {
         private readonly ICartIdAccessor _cartIdAccessor;
         private readonly IRepository<Entities.Cart> _repository;
         private readonly Lazy<ICurrencyAccessor> _currencyAccessor;
+        private readonly ILock _lock;
 
         public CartAccessor(ICartIdAccessor cartIdAccessor,
                             IRepository<Entities.Cart> repository,
-                            Lazy<ICurrencyAccessor> currencyAccessor) {
+                            Lazy<ICurrencyAccessor> currencyAccessor,
+                            ILock @lock) {
             _cartIdAccessor = cartIdAccessor;
             _repository = repository;
             _currencyAccessor = currencyAccessor;
+            _lock = @lock;
         }
     
         public async Task<Entities.Cart> GetAsync(CancellationToken cancellationToken = default) {
             var cartId = _cartIdAccessor.GetCartId();
-            var cart = await _repository.GetAsync(cartId, cancellationToken);
+            
+            var result = await _lock.LockAsync(cartId.ToString(), async () => {
+                var cart = await _repository.GetAsync(cartId, cancellationToken);
 
-            if (cart == null) {
-                var currency = _currencyAccessor.Value.GetCurrency();
-                
-                cart = Entities.Cart.Create(currency);
+                if (cart == null) {
+                    var currency = _currencyAccessor.Value.GetCurrency();
+                    
+                    cart = Entities.Cart.Create(cartId, currency);
 
-                // TODO Implement lock to prevent race condition
-                await _repository.InsertAsync(cart, cancellationToken);
-            }
+                    await _repository.InsertAsync(cart, cancellationToken);
+                }
 
-            return cart;
+                return cart;
+            });
+
+            return result;
         }
     }
 }
