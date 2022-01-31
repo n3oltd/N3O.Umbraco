f2643431b3d47a1fb5dea17f37ce134b28c07e39
Noor Drummond <noor.drummond@n3o.ltd>
2022-01-31 08:45:54 +0000

    Added client files

---
+++ clients/@n3oltd/umbraco-payments-gocardless-client/esm/index.d.ts
@@ -0,0 +1,34 @@
+export declare class GoCardlessClient {
+    private http;
+    private baseUrl;
+    protected jsonParseReviver: ((key: string, value: any) => any) | undefined;
+    constructor(baseUrl?: string, http?: {
+        fetch(url: RequestInfo, init?: RequestInit): Promise<Response>;
+    });
+    begin(flowId: string): Promise<void>;
+    protected processBegin(response: Response): Promise<void>;
+    complete(flowId: string): Promise<void>;
+    protected processComplete(response: Response): Promise<void>;
+}
+export interface ProblemDetails {
+    type?: string | undefined;
+    title?: string | undefined;
+    status?: number | undefined;
+    detail?: string | undefined;
+    instance?: string | undefined;
+}
+export declare class ApiException extends Error {
+    message: string;
+    status: number;
+    response: string;
+    headers: {
+        [key: string]: any;
+    };
+    result: any;
+    constructor(message: string, status: number, response: string, headers: {
+        [key: string]: any;
+    }, result: any);
+    protected isApiException: boolean;
+    static isApiException(obj: any): obj is ApiException;
+}
+//# sourceMappingURL=index.d.ts.map
