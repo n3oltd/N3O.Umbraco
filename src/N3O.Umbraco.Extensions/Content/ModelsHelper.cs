e6462bbdfcfa625ee787bd9a1a50c4363c23ffdb
Noor Drummond <noor.drummond@n3o.ltd>
2022-02-01 08:40:57 +0000

    Fix type resolution

--- src/N3O.Umbraco.Extensions/Content/ModelsHelper.cs
+++ src/N3O.Umbraco.Extensions/Content/ModelsHelper.cs
@@ -6,25 +6,26 @@ using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace N3O.Umbraco.Content {
     public class ModelsHelper {
         private static readonly ConcurrentDictionary<string, Type> TypeCache = new(StringComparer.InvariantCultureIgnoreCase);
     
         public static Type GetOrCreateModelsBuilderType(string modelsNamespace, string contentType) {
-            var typeName = $"{modelsNamespace}.{contentType.Pascalize()}";
+            var cacheKey = CacheKey.Generate<ModelsHelper>(contentType);
+            
+            return TypeCache.GetOrAdd(cacheKey, () => {
+                var typeName = contentType.Pascalize();
 
-            return GetOrCreateType(typeName);
-        }
-    
-        private static Type GetOrCreateType(string typeName) {
-            return TypeCache.GetOrAdd(typeName, () => {
-                var type = OurAssemblies.GetTypes(t => t.FullName == typeName).SingleOrDefault();
+                var type = OurAssemblies.GetTypes(t => t.Name == typeName &&
+                                                       t.IsSubclassOfType(typeof(PublishedContentModel)))
+                                        .SingleOrDefault();
 
                 if (type == null) {
+                    var fullTypeName = $"{modelsNamespace}.{typeName}";
                     var assemblyName = new AssemblyName($"assembly_{Guid.NewGuid()}");
                     var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
-
                     var moduleBuilder = assemblyBuilder.DefineDynamicModule($"module_{Guid.NewGuid()}");
 
                     var typeAttributes = TypeAttributes.AnsiClass |
@@ -33,7 +34,7 @@ namespace N3O.Umbraco.Content {
                                          TypeAttributes.ExplicitLayout |
                                          TypeAttributes.Public;
 
-                    var typeBuilder = moduleBuilder.DefineType(typeName, typeAttributes, null);
+                    var typeBuilder = moduleBuilder.DefineType(fullTypeName, typeAttributes, null);
 
                     type = typeBuilder.CreateType();
                 }
