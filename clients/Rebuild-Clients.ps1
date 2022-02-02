956a2d5ad19ac33cafeb670b936230556c550ba4
Noor Drummond <noor.drummond@n3o.ltd>
2022-02-02 11:08:13 +0000

    Update clients

--- clients/Rebuild-Clients.ps1
+++ clients/Rebuild-Clients.ps1
@@ -1,21 +1,19 @@
-n3o-tool clients --url "https://localhost:5001/swagger/Accounts/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-accounts-client" --name "AccountsClient" --package-name "@n3oltd/umbraco-accounts-client" --package-description "TypeScript client for the N3O Umbraco Accounts"
+n3o-tool clients --url "https://localhost:6001/swagger/Accounts/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-accounts-client" --name "AccountsClient" --package-name "@n3oltd/umbraco-accounts-client" --package-description "TypeScript client for the N3O Umbraco Accounts"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Allocations/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-allocations-client" --name "AllocationsClient" --package-name "@n3oltd/umbraco-allocations-client" --package-description "TypeScript client for the N3O Umbraco Allocations"
+n3o-tool clients --url "https://localhost:6001/swagger/Cropper/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-cropper-client" --name "CropperClient" --package-name "@n3oltd/umbraco-cropper-client" --package-description "TypeScript client for the N3O Umbraco Cropper"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Cart/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-cart-client" --name "CartClient" --package-name "@n3oltd/umbraco-cart-client" --package-description "TypeScript client for the N3O Umbraco Cart"
+n3o-tool clients --url "https://localhost:6001/swagger/Giving/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-giving-client" --name "AllocationsClient" --package-name "@n3oltd/umbraco-giving-client" --package-description "TypeScript client for the N3O Umbraco Giving"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Checkout/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-checkout-client" --name "CheckoutClient" --package-name "@n3oltd/umbraco-checkout-client" --package-description "TypeScript client for the N3O Umbraco Checkout"
+n3o-tool clients --url "https://localhost:6001/swagger/Cart/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-giving-cart-client" --name "CartClient" --package-name "@n3oltd/umbraco-giving-cart-client" --package-description "TypeScript client for the N3O Umbraco Giving Giving Cart"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Cropper/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-cropper-client" --name "CropperClient" --package-name "@n3oltd/umbraco-cropper-client" --package-description "TypeScript client for the N3O Umbraco Cropper"
+n3o-tool clients --url "https://localhost:6001/swagger/Checkout/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-giving-checkout-client" --name "CheckoutClient" --package-name "@n3oltd/umbraco-giving-checkout-client" --package-description "TypeScript client for the N3O Umbraco Giving Checkout"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Donations/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-donations-client" --name "DonationsClient" --package-name "@n3oltd/umbraco-donations-client" --package-description "TypeScript client for the N3O Umbraco Donations"
+n3o-tool clients --url "https://localhost:6001/swagger/Newsletters/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-newsletters-client" --name "NewslettersClient" --package-name "@n3oltd/umbraco-newsletters-client" --package-description "TypeScript client for the N3O Umbraco Newsletters"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Newsletters/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-newsletters-client" --name "NewslettersClient" --package-name "@n3oltd/umbraco-newsletters-client" --package-description "TypeScript client for the N3O Umbraco Newsletters"
+n3o-tool clients --url "https://localhost:6001/swagger/Payments/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-client" --name "PaymentsClient" --package-name "@n3oltd/umbraco-payments-client" --package-description "TypeScript client for the N3O Umbraco Payments"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Payments/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-client" --name "PaymentsClient" --package-name "@n3oltd/umbraco-payments-client" --package-description "TypeScript client for the N3O Umbraco Payments"
+n3o-tool clients --url "https://localhost:6001/swagger/GoCardless/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-gocardless-client" --name "GoCardlessClient" --package-name "@n3oltd/umbraco-payments-gocardless-client" --package-description "TypeScript client for the N3O Umbraco Payments GoCardless"
 
-n3o-tool clients --url "https://localhost:5001/swagger/GoCardless/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-gocardless-client" --name "GoCardlessClient" --package-name "@n3oltd/umbraco-payments-gocardless-client" --package-description "TypeScript client for the N3O Umbraco Payments GoCardless"
+n3o-tool clients --url "https://localhost:6001/swagger/Opayo/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-opayo-client" --name "OpayoClient" --package-name "@n3oltd/umbraco-payments-opayo-client" --package-description "TypeScript client for the N3O Umbraco Payments Opayo"
 
-n3o-tool clients --url "https://localhost:5001/swagger/Opayo/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-payments-opayo-client" --name "OpayoClient" --package-name "@n3oltd/umbraco-payments-opayo-client" --package-description "TypeScript client for the N3O Umbraco Payments Opayo"
-
-n3o-tool clients --url "https://localhost:5001/swagger/Uploader/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-uploader-client" --name "UploaderClient" --package-name "@n3oltd/umbraco-uploader-client" --package-description "TypeScript client for the N3O Umbraco Uploader"
+n3o-tool clients --url "https://localhost:6001/swagger/Uploader/swagger.json" --language "TypeScript" --output-path "@n3oltd/umbraco-uploader-client" --name "UploaderClient" --package-name "@n3oltd/umbraco-uploader-client" --package-description "TypeScript client for the N3O Umbraco Uploader"
