6a3d2a00629dca0b47886b87bfdcfbb12b53c1c4
Noor Drummond <noor.drummond@n3o.ltd>
2022-02-06 12:05:42 +0000

    Added absoluteUrl handlebars helper

---
+++ src/Templates/N3O.Umbraco.Templates.Handlebars/Helpers/AbsoluteUrlHelper.cs
@@ -0,0 +1,26 @@
+﻿using HandlebarsDotNet;
+using Microsoft.Extensions.Logging;
+using N3O.Umbraco.Json;
+using N3O.Umbraco.Utilities;
+
+namespace N3O.Umbraco.Templates.Handlebars.Helpers {
+    public class AbsoluteUrlHelper : Helper {
+        private readonly IUrlBuilder _urlBuilder;
+
+        public AbsoluteUrlHelper(ILogger<AbsoluteUrlHelper> logger, IJsonProvider jsonProvider, IUrlBuilder urlBuilder)
+            : base(logger, jsonProvider, 1) {
+            _urlBuilder = urlBuilder;
+        }
+
+        protected override void Execute(EncodedTextWriter writer,
+                                        HandlebarsDotNet.Context context,
+                                        HandlebarsArguments args) {
+            var path = args.Get<string>(0);
+            var absoluteUrl = _urlBuilder.Root().AppendPathSegment(path);
+            
+            writer.Write(absoluteUrl.ToString());
+        }
+        
+        public override string Name => "absoluteUrl";
+    }
+}
