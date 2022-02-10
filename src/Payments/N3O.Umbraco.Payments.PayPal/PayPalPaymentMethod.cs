d090772a933ec546df98d4dc4044ad0b832f3ce7
sirajurrahmann <siraj.urrahman@n3o.ltd>
2022-02-10 15:49:00 +0500

    Add is available to all the payment methods.

--- src/Payments/N3O.Umbraco.Payments.PayPal/PayPalPaymentMethod.cs
+++ src/Payments/N3O.Umbraco.Payments.PayPal/PayPalPaymentMethod.cs
@@ -1,7 +1,21 @@
+using N3O.Umbraco.Content;
+using N3O.Umbraco.Financial;
+using N3O.Umbraco.Lookups;
 using N3O.Umbraco.Payments.Lookups;
+using N3O.Umbraco.Payments.PayPal.Content;
 
 namespace N3O.Umbraco.Payments.PayPal {
     public class PayPalPaymentMethod : PaymentMethod {
         public PayPalPaymentMethod() : base("payPal", "PayPal", false, null, null) { }
+        
+        public override bool IsAvailable(IContentCache contentCache, Country country, Currency currency) {
+            var settings = contentCache.Single<PayPalSettingsContent>();
+            
+            if (settings == null) {
+                return false;
+            }
+            
+            return true;
+        }
     }
 }
