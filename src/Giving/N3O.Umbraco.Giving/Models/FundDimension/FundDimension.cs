81429313a0bae5a7a80c39b739184ba7bf40a502
sirajurrahmann <siraj.urrahman@n3o.ltd>
2022-02-15 17:06:48 +0500

    Add index property to fund dimension.

--- src/Giving/N3O.Umbraco.Giving/Models/FundDimension/FundDimension.cs
+++ src/Giving/N3O.Umbraco.Giving/Models/FundDimension/FundDimension.cs
@@ -6,10 +6,26 @@ namespace N3O.Giving.Models {
     public abstract class FundDimension<T, TValue> : LookupContent<T> where T : FundDimension<T, TValue> {
         public bool IsActive => GetValue(x => x.IsActive);
         public IReadOnlyList<TValue> Options => Content.Children.As<TValue>();
+        public int Index {get; }
+
+        protected FundDimension(int index) {
+            Index = index;
+        }
     }
-    
-    public class FundDimension1 : FundDimension<FundDimension1, FundDimension1Value> { }
-    public class FundDimension2 : FundDimension<FundDimension2, FundDimension2Value> { }
-    public class FundDimension3 : FundDimension<FundDimension3, FundDimension3Value> { }
-    public class FundDimension4 : FundDimension<FundDimension4, FundDimension4Value> { }
+
+    public class FundDimension1 : FundDimension<FundDimension1, FundDimension1Value> {
+        public FundDimension1() : base(1) { }
+    }
+
+    public class FundDimension2 : FundDimension<FundDimension2, FundDimension2Value> {
+        public FundDimension2() : base(2) { }
+    }
+
+    public class FundDimension3 : FundDimension<FundDimension3, FundDimension3Value> {
+        public FundDimension3() : base(3) { }
+    }
+
+    public class FundDimension4 : FundDimension<FundDimension4, FundDimension4Value> {
+        public FundDimension4() : base(4) { }
+    }
 }
