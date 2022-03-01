773c1cdd3164171bb02b5030a1b0c31080553f04
Siraj Ur Rahman <siraj.urrahman@n3o.ltd>
2022-03-01 18:50:55 +0500

    Revert "Revert "renaming variables.""
    This reverts commit 59875f672e3da9f1d20a37d79cd5939801f3d50e.

--- src/Payments/N3O.Umbraco.Payments.Stripe/Content/Settings/StripeSettingsContent.cs
+++ src/Payments/N3O.Umbraco.Payments.Stripe/Content/Settings/StripeSettingsContent.cs
@@ -4,7 +4,7 @@ namespace N3O.Umbraco.Payments.Stripe.Models {
     public class StripeSettingsContent : PaymentMethodSettingsContent<StripeSettingsContent> {
         public string ProductionClientKey => GetValue(x => x.ProductionClientKey);
         public string ProductionSecretKey => GetValue(x => x.ProductionSecretKey);
-        public string StagingClientKey => GetValue(x => x.StagingClientKey);
-        public string StagingSecretKey => GetValue(x => x.StagingSecretKey);
+        public string SandboxClientKey => GetValue(x => x.SandboxClientKey);
+        public string SandboxSecretKey => GetValue(x => x.SandboxSecretKey);
     }
 }
