c73a6a70da316a71de7528f0bcf8bf5942201654
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-03 09:46:33 +0000

    Added extension / utility methods

---
+++ src/N3O.Umbraco.Extensions/Extensions/LookupExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Extensions {
+    public static class LookupExtensions {
+        public static string ToViewName(this ILookup lookup) {
+            return lookup.Id.Pascalize();
+        }
+    }
+}
---
+++ src/N3O.Umbraco.Extensions/Types/ValueTupleFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace N3O.Umbraco {
+    public static class ValueTupleFactory {
+        public static object Create(Func<(Type, object)> item1, Func<(Type, object)> item2) {
+            var (item1Type, item1Value) = item1();
+            var (item2Type, item2Value) = item2();
+
+            var valueTupleType = typeof(ValueTuple<,>).MakeGenericType(item1Type, item2Type);
+            var valueTuple = Activator.CreateInstance(valueTupleType, item1Value, item2Value);
+
+            return valueTuple;
+        }
+    }
+}
