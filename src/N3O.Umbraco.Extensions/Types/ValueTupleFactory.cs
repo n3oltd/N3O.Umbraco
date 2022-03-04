3b9f45ecc299fbe1b46494d5927237324c7967f3
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-04 14:07:59 +0000

    Update ValueTupleFactory

--- src/N3O.Umbraco.Extensions/Types/ValueTupleFactory.cs
+++ src/N3O.Umbraco.Extensions/Types/ValueTupleFactory.cs
@@ -11,5 +11,18 @@ namespace N3O.Umbraco {
 
             return valueTuple;
         }
+        
+        public static object Create(Func<(Type, object)> item1,
+                                    Func<(Type, object)> item2,
+                                    Func<(Type, object)> item3) {
+            var (item1Type, item1Value) = item1();
+            var (item2Type, item2Value) = item2();
+            var (item3Type, item3Value) = item3();
+
+            var valueTupleType = typeof(ValueTuple<,,>).MakeGenericType(item1Type, item2Type, item3Type);
+            var valueTuple = Activator.CreateInstance(valueTupleType, item1Value, item2Value, item3Value);
+
+            return valueTuple;
+        }
     }
 }
