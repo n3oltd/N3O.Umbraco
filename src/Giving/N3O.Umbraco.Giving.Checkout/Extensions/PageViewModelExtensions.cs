e5edc90612f8156e739dccf6b5ca31aafc496baa
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-04 19:26:21 +0000

    Checkout/cart helpers

---
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Extensions/PageViewModelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using N3O.Umbraco.Content;
+using N3O.Umbraco.Extensions;
+using N3O.Umbraco.Giving.Cart;
+using N3O.Umbraco.Giving.Cart.Content;
+using N3O.Umbraco.Pages;
+
+namespace N3O.Umbraco.Giving.Checkout.Extensions {
+    public static class PageViewModelExtensions {
+        public static string GetCartUrl(this IPageViewModel pageViewModel, IContentCache contentCache) {
+            var url = new Url(contentCache.Single<CartPageContent>().Content().AbsoluteUrl());
+
+            if (pageViewModel.Content.IsCheckoutPage()) {
+                url.SetQueryParam(CartConstants.QueryString.CheckoutView);
+            }
+
+            return url;
+        }
+    }
+}
