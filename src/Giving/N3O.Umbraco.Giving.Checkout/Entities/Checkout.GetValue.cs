a6c4445c886a5ff38dd2fea6f6ba957ef6b488bb
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-06 12:15:29 +0000

    Added GetValue method to Checkout

---
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Entities/Checkout.GetValue.cs
@@ -0,0 +1,13 @@
+﻿using N3O.Umbraco.Financial;
+
+namespace N3O.Umbraco.Giving.Checkout.Entities {
+    public partial class Checkout {
+        public Money GetValue(int regularGivingMultiplier) {
+            var donationAmount = Donation?.Total.Amount ?? 0m;
+            var regularGivingAmount = (RegularGiving?.Total.Amount ?? 0m) * regularGivingMultiplier;
+            var totalAmount = donationAmount + regularGivingAmount;
+
+            return new Money(totalAmount, Currency);
+        }
+    }
+}
