9e74097745f2aa7fc2fced4a9c1145a6f44dbb8a
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-06 10:53:53 +0000

    Remove default on interface

--- src/Analytics/N3O.Umbraco.Analytics/Services/DataLayerProvider.I.cs
+++ src/Analytics/N3O.Umbraco.Analytics/Services/DataLayerProvider.I.cs
@@ -6,6 +6,6 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 namespace N3O.Umbraco.Analytics {
     public interface IDataLayerProvider {
         bool IsProviderFor(IPublishedContent page);
-        Task<IEnumerable<object>> GetAsync(IPublishedContent page, CancellationToken cancellationToken = default);
+        Task<IEnumerable<object>> GetAsync(IPublishedContent page, CancellationToken cancellationToken);
     }
 }
