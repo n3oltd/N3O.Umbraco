671be307f54fd12168b055f3e5c3ff0213562621
Noor Drummond <noor.drummond@n3o.ltd>
2022-03-08 14:21:09 +0000

    Transaction ID updates

--- src/Giving/N3O.Umbraco.Giving.Checkout/Entities/Checkout.FormatTransactionText.cs
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Entities/Checkout.FormatTransactionText.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using N3O.Umbraco.Extensions;
+using System;
 
 namespace N3O.Umbraco.Giving.Checkout.Entities {
     public partial class Checkout {
         private string FormatTransactionText(string format, string idempotencyKey = null) {
             return format.Replace("{Reference}", Reference.Text, StringComparison.InvariantCultureIgnoreCase)
                          .Replace("{Prefix}", Progress.CurrentStage.TransctionIdPrefix, StringComparison.InvariantCultureIgnoreCase)
-                         .Replace("{IdempotencyKey}", idempotencyKey, StringComparison.CurrentCultureIgnoreCase);
+                         .Replace("{IdempotencyKey}",
+                                  idempotencyKey.GetDeterministicHashCode(true).ToString(),
+                                  StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
