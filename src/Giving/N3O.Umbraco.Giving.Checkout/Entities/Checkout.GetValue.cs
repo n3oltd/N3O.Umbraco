b340fc93de900e2bca4054b75d8bd10d396d6d73
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-04 10:56:15 +0100

    Added value multiplier

--- src/Giving/N3O.Umbraco.Giving.Checkout/Entities/Checkout.GetValue.cs
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Entities/Checkout.GetValue.cs
@@ -1,10 +1,11 @@
 ﻿using N3O.Umbraco.Financial;
+using N3O.Umbraco.Giving.Lookups;
 
 namespace N3O.Umbraco.Giving.Checkout.Entities {
     public partial class Checkout {
-        public Money GetValue(int regularGivingMultiplier) {
-            var donationAmount = Donation?.Total.Amount ?? 0m;
-            var regularGivingAmount = (RegularGiving?.Total.Amount ?? 0m) * regularGivingMultiplier;
+        public Money GetValue() {
+            var donationAmount = (Donation?.Total.Amount ?? 0m) * GivingTypes.Donation.ValueMultiplier;
+            var regularGivingAmount = (RegularGiving?.Total.Amount ?? 0m) * GivingTypes.RegularGiving.ValueMultiplier;
             var totalAmount = donationAmount + regularGivingAmount;
 
             return new Money(totalAmount, Currency);
--- src/Giving/N3O.Umbraco.Giving/Lookups/GivingType.cs
+++ src/Giving/N3O.Umbraco.Giving/Lookups/GivingType.cs
@@ -2,17 +2,19 @@
 
 namespace N3O.Umbraco.Giving.Lookups {
     public class GivingType : NamedLookup {
-        public GivingType(string id, string name, string icon, int order) : base(id, name) {
+        public GivingType(string id, string name, string icon, int order, int valueMultiplier) : base(id, name) {
             Icon = icon;
             Order = order;
+            ValueMultiplier = valueMultiplier;
         }
 
         public string Icon { get; }
         public int Order { get; }
+        public int ValueMultiplier { get; }
     }
 
     public class GivingTypes : StaticLookupsCollection<GivingType> {
-        public static readonly GivingType Donation = new("donation", "Donation", "icon-coin-dollar", 0);
-        public static readonly GivingType RegularGiving = new("regularGiving", "Regular Giving", "icon-coins-dollar", 1);
+        public static readonly GivingType Donation = new("donation", "Donation", "icon-coin-dollar", 0, 1);
+        public static readonly GivingType RegularGiving = new("regularGiving", "Regular Giving", "icon-coins-dollar", 1, 12);
     }
 }
