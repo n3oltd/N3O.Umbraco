7fcb44b042e8afdb633cd9f251c400a1fee740b2
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-05 14:44:41 +0100

    Added GA4 support

--- src/Analytics/N3O.Umbraco.Analytics/AnalyticsConstants.cs
+++ src/Analytics/N3O.Umbraco.Analytics/AnalyticsConstants.cs
@@ -2,7 +2,8 @@ namespace N3O.Umbraco.Analytics {
     public static class AnalyticsConstants {
         public static class PageModuleKeys {
             public const string DataLayer = nameof(DataLayer);
-            public const string TagManager = nameof(TagManager);
+            public const string GoogleAnalytics4 = nameof(GoogleAnalytics4);
+            public const string GoogleTagManager = nameof(GoogleTagManager);
         }
     }
 }
--- src/Analytics/N3O.Umbraco.Analytics/Extensions/PageViewModelExtensions.cs
+++ src/Analytics/N3O.Umbraco.Analytics/Extensions/PageViewModelExtensions.cs
@@ -6,9 +6,13 @@ namespace N3O.Umbraco.Analytics.Extensions {
         public static Code DataLayer(this IPageViewModel pageViewModel) {
             return pageViewModel.ModulesData.Get<Code>(AnalyticsConstants.PageModuleKeys.DataLayer);
         }
+        
+        public static Code GoogleAnalytics4(this IPageViewModel pageViewModel) {
+            return pageViewModel.ModulesData.Get<Code>(AnalyticsConstants.PageModuleKeys.GoogleAnalytics4);
+        }
     
-        public static TagManagerCode TagManager(this IPageViewModel pageViewModel) {
-            return pageViewModel.ModulesData.Get<TagManagerCode>(AnalyticsConstants.PageModuleKeys.TagManager);
+        public static GoogleTagManagerCode GoogleTagManager(this IPageViewModel pageViewModel) {
+            return pageViewModel.ModulesData.Get<GoogleTagManagerCode>(AnalyticsConstants.PageModuleKeys.GoogleTagManager);
         }
     }
 }
