89f47dd10cb2b2f6ec205fadd4df8c7f0f458735
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-17 11:11:41 +0100

    Added structure for Algolia search provider

---
+++ src/Search/N3O.Umbraco.Search.Algolia/Content/Settings/AlgoliaSettingsContent.cs
@@ -0,0 +1,8 @@
+﻿using N3O.Umbraco.Content;
+
+namespace N3O.Umbraco.Search.Algolia.Content {
+    public class AlgoliaSettingsContent : UmbracoContent<AlgoliaSettingsContent> {
+        public string ApiKey => GetValue(x => x.ApiKey);
+        public string ApplicationId => GetValue(x => x.ApplicationId);
+    }
+}
