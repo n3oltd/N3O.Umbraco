03c52aee878da3cae36f886d47befd7157c66e9a
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-17 11:06:55 +0100

    Code cleanup

--- src/Search/N3O.Umbraco.Search.Google/GoogleSearchComposer.cs
+++ src/Search/N3O.Umbraco.Search.Google/GoogleSearchComposer.cs
@@ -5,7 +5,7 @@ using Umbraco.Cms.Core.DependencyInjection;
 namespace N3O.Umbraco.Search.Google {
     public class GoogleSearchComposer : Composer {
         public override void Compose(IUmbracoBuilder builder) {
-            builder.Services.AddTransient<ISearcher, GoogleCustomSearch>();
+            builder.Services.AddTransient<ISearcher, GoogleSearchSearcher>();
         }
     }
 }
--- src/Search/N3O.Umbraco.Search.Google/Services/GoogleSearchSearcher.cs
+++ src/Search/N3O.Umbraco.Search.Google/Services/GoogleSearchSearcher.cs
@@ -12,13 +12,13 @@ using Umbraco.Cms.Core.Cache;
 using Umbraco.Extensions;
 
 namespace N3O.Umbraco.Search.Google {
-    public class GoogleCustomSearch : ISearcher {
+    public class GoogleSearchSearcher : ISearcher {
         private const int PageSize = 10;
     
         private readonly IContentCache _contentCache;
         private readonly IAppPolicyCache _appCache;
 
-        public GoogleCustomSearch(IContentCache contentCache, IAppPolicyCache appCache) {
+        public GoogleSearchSearcher(IContentCache contentCache, IAppPolicyCache appCache) {
             _contentCache = contentCache;
             _appCache = appCache;
         }
@@ -34,13 +34,17 @@ namespace N3O.Umbraco.Search.Google {
             return pager;
         }
 
-        private (long TotalResults, IEnumerable<SearchResult> Results) GetCachedResults(string query, int start, int number) {
-            var cacheKey = CacheKey.Generate<GoogleCustomSearch>(nameof(Search), query, start, number);
+        private (long TotalResults, IEnumerable<SearchResult> Results) GetCachedResults(string query,
+                                                                                        int start,
+                                                                                        int number) {
+            var cacheKey = CacheKey.Generate<GoogleSearchSearcher>(nameof(Search), query, start, number);
 
             return _appCache.GetCacheItem(cacheKey, () => ExecuteQuery(query, start, number), TimeSpan.FromHours(1));
         }
 
-        private (long TotalResults, IEnumerable<SearchResult> Results) ExecuteQuery(string query, int start, int number) {
+        private (long TotalResults, IEnumerable<SearchResult> Results) ExecuteQuery(string query,
+                                                                                    int start,
+                                                                                    int number) {
             try {
                 var searchSettings = _contentCache.Single<GoogleSearchSettingsContent>();
 
