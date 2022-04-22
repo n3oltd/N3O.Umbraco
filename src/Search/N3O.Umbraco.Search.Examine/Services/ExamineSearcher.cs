aab174d3854e37ffb032d1156179d1cccb662fca
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-22 16:51:51 +0100

    Added project for Examine powered search

---
+++ src/Search/N3O.Umbraco.Search.Examine/Services/ExamineSearcher.cs
@@ -0,0 +1,11 @@
+﻿using N3O.Umbraco.Search.Models;
+using N3O.Umbraco.Utilities;
+using System;
+
+namespace N3O.Umbraco.Search.Examine {
+    public class ExamineSearcher : ISearcher {
+        public DynamicPager<SearchResult> Search(string query, string currentUrl) {
+            throw new NotImplementedException();
+        }
+    }
+}
