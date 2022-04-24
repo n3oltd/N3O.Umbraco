3a746e792a233b36a85de37d54c5aa7f87cc5049
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-24 12:05:37 +0100

    Code cleanup

--- src/N3O.Umbraco.Extensions/Localization/Text/TextFormatter.cs
+++ src/N3O.Umbraco.Extensions/Localization/Text/TextFormatter.cs
@@ -2,7 +2,6 @@ using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Lookups;
 using System;
 using System.Collections.Concurrent;
-using System.Text.RegularExpressions;
 
 namespace N3O.Umbraco.Localization {
     public class TextFormatter : ITextFormatter {
@@ -26,35 +25,6 @@ namespace N3O.Umbraco.Localization {
 
         public string FormatLookupName(INamedLookup lookup) {
             return lookup.Name;
-        }
-
-        public string FormatName(string firstName, string lastName) => FormatName(null, null, firstName, lastName);
-
-        public string FormatName(string title, string firstName, string lastName) => FormatName(null, title, firstName, lastName);
-
-        public string FormatName(string organisation, string title, string firstName, string lastName) {
-            // TODO localize and move
-            var output = "{Organisation} {CareOf} {Title} {First} {Last}";
-
-            output = CaseSensitivePlaceholderSubstitution(output, "Title", title);
-            output = CaseSensitivePlaceholderSubstitution(output, "First", firstName);
-            output = CaseSensitivePlaceholderSubstitution(output, "Last", lastName);
-            output = CaseSensitivePlaceholderSubstitution(output, "CareOf", ((firstName.HasValue() || lastName.HasValue()) && organisation.HasValue()) ? "C/O" : string.Empty);
-            output = CaseSensitivePlaceholderSubstitution(output, "Organisation", organisation);
-
-            return Regex.Replace(Regex.Replace(output, @"( )+", " ").Trim(), "\n+", "\n").Trim();
-        }
-    
-        private static string CaseSensitivePlaceholderSubstitution(string formatString,
-                                                                   string placeholderName,
-                                                                   string placeholderValue) {
-            var output = formatString;
-
-            output = output.Replace("{" + placeholderName + "}", placeholderValue ?? "");
-            output = output.Replace("{" + placeholderName.ToUpperInvariant() + "}", placeholderValue?.ToUpper() ?? "");
-            output = output.Replace("{" + placeholderName.ToLowerInvariant() + "}", placeholderValue?.ToLower() ?? "");
-
-            return output;
         }
     }
 }
