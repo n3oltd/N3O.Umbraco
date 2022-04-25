9c54ee2ad58b6560ba43508a8d8e72097ee441e8
Noor Drummond <noor.drummond@n3o.ltd>
2022-04-25 18:31:49 +0100

    Added UdiExtensions

---
+++ src/N3O.Umbraco.Extensions/Extensions/UdiExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Umbraco.Cms.Core;
+
+namespace N3O.Umbraco.Extensions {
+    public static class UdiExtensions {
+        public static Guid? ToId(this Udi udi) {
+            return (udi as GuidUdi)?.Guid;
+        }
+    }
+}
