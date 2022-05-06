ac19870b657a79803dbc9042b7e7baabf8737764
Noor Drummond <noor.drummond@n3o.ltd>
2022-05-06 17:53:06 +0100

    Added restricted collection days to payment method settings

---
+++ src/N3O.Umbraco.Extensions/Lookups/DayOfMonth/DayOfMonthDataSource.cs
@@ -0,0 +1,11 @@
+﻿namespace N3O.Umbraco.Lookups {
+    public class DayOfMonthDataSource : LookupsDataSource<DayOfMonth> {
+        public DayOfMonthDataSource(ILookups lookups) : base(lookups) { }
+        
+        public override string Name => "Day of Month";
+        public override string Description => "Data source for day of month";
+        public override string Icon => "icon-calendar";
+
+        protected override string GetIcon(DayOfMonth consentChannel) => "icon-calendar";
+    }
+}
--- src/Payments/N3O.Umbraco.Payments/Content/PaymentMethodSettings.I.cs
+++ src/Payments/N3O.Umbraco.Payments/Content/PaymentMethodSettings.I.cs
@@ -1,6 +1,10 @@
-﻿namespace N3O.Umbraco.Payments.Content {
+﻿using N3O.Umbraco.Lookups;
+using System.Collections.Generic;
+
+namespace N3O.Umbraco.Payments.Content {
     public interface IPaymentMethodSettings {
         string TransactionDescription { get; }
         string TransactionId { get; }
+        IEnumerable<DayOfMonth> RestrictCollectionDaysTo { get; }
     }
 }
