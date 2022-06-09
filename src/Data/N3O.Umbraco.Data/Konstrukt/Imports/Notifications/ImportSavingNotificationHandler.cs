9255e46fb8040def32276e0637e405d646b8214f
Noor Drummond <noor@ankabut.com>
2022-06-09 12:27:27 +0100

    Update ImportSavingNotificationHandler.cs

--- src/Data/N3O.Umbraco.Data/Konstrukt/Imports/Notifications/ImportSavingNotificationHandler.cs
+++ src/Data/N3O.Umbraco.Data/Konstrukt/Imports/Notifications/ImportSavingNotificationHandler.cs
@@ -8,12 +8,11 @@ namespace N3O.Umbraco.Data.Konstrukt.Notifications {
     public class ImportSavingNotificationHandler : INotificationAsyncHandler<KonstruktEntitySavingNotification> {
         public Task HandleAsync(KonstruktEntitySavingNotification notification, CancellationToken cancellationToken) {
             if (notification.Entity.After is Import import && !import.CanProcess) {
-                //Below does not show message in current version of Konstrukt
-                /*notification.Cancel = true;
+                // TODO The below does not show a message in current version of Konstrukt
+                notification.Cancel = true;
                 notification.Messages.Add(new EventMessage("Error",
                                                            "This record has been imported and can no longer be updated",
-                                                           EventMessageType.Error));*/
-                throw new Exception("This record has been imported and can no longer be updated");
+                                                           EventMessageType.Error));
             }
 
             return Task.CompletedTask;
