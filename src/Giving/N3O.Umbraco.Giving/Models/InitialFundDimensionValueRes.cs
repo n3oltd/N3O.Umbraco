b9da6cc1d4dbbb554787a0aa9c0b4ba5edf8609f
sirajurrahmann <siraj.urrahman@n3o.ltd>
2022-06-15 12:32:16 +0500

    Add suggested dimension value to the initial fund dimensions res where the fixed and default is null.

--- src/Giving/N3O.Umbraco.Giving/Models/InitialFundDimensionValueRes.cs
+++ src/Giving/N3O.Umbraco.Giving/Models/InitialFundDimensionValueRes.cs
@@ -1,8 +1,9 @@
 ﻿using N3O.Giving.Models;
 
 namespace N3O.Umbraco.Giving.Models {
-    public class FixedOrDefaultFundDimensionValueRes {
+    public class InitialFundDimensionValueRes {
         public FundDimensionValueRes Fixed { get; set; }
         public FundDimensionValueRes Default { get; set; }
+        public FundDimensionValueRes Suggested { get; set; }
     }
 }
