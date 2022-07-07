30124986a543ef5819be5d239fca295177448f19
Noor Drummond <noor.drummond@n3o.ltd>
2022-07-07 18:36:36 +0100

    Fix seed values

--- src/Data/N3O.Umbraco.Data/Lookups/ImportReferenceType.cs
+++ src/Data/N3O.Umbraco.Data/Lookups/ImportReferenceType.cs
@@ -3,5 +3,5 @@ using N3O.Umbraco.References;
 namespace N3O.Umbraco.Data.Lookups;
 
 public class ImportReferenceType : ReferenceType {
-    public ImportReferenceType() : base("IM", 100_000) { }
+    public ImportReferenceType() : base("IM", 100_001) { }
 }
--- src/Giving/N3O.Umbraco.Giving.Checkout/Lookups/CheckoutReferenceType.cs
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Lookups/CheckoutReferenceType.cs
@@ -3,5 +3,5 @@ using N3O.Umbraco.References;
 namespace N3O.Umbraco.Giving.Checkout.Lookups;
 
 public class CheckoutReferenceType : ReferenceType {
-    public CheckoutReferenceType() : base("WC", 1_000_000) { }
+    public CheckoutReferenceType() : base("WC", 1_000_001) { }
 }
