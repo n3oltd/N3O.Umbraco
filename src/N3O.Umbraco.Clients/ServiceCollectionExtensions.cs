30c53aefb2f4be15c430ca8a44d834f0eb1794a8
Noor Drummond <noor.drummond@n3o.ltd>
2022-07-14 14:43:11 +0100

    Allow custom http client factory for clients

--- src/N3O.Umbraco.Clients/ServiceCollectionExtensions.cs
+++ src/N3O.Umbraco.Clients/ServiceCollectionExtensions.cs
@@ -23,42 +23,46 @@ using System.Net.Http;
 namespace N3O.Umbraco.Data.Clients {
     public static class ServiceCollectionExtensions {
         public static void AddUmbracoClients(this IServiceCollection services,
-                                             Func<IServiceProvider, string> baseUrlResolver) {
-            AddUmbracoClient<IAccountsClient, AccountsClient>(services, baseUrlResolver);
+                                             Func<IServiceProvider, string> baseUrlResolver,
+                                             Func<IServiceProvider, HttpClient> httpClientFactory = null) {
+            httpClientFactory ??= serviceProvider => serviceProvider.GetRequiredService<IHttpClientFactory>()
+                                                                    .CreateClient();
             
-            AddUmbracoClient<IContentClient, ContentClient>(services, baseUrlResolver);
-            AddUmbracoClient<IContentTypesClient, ContentTypesClient>(services, baseUrlResolver);
-            AddUmbracoClient<IDataTypesClient, DataTypesClient>(services, baseUrlResolver);
-            AddUmbracoClient<IExportsClient, ExportsClient>(services, baseUrlResolver);
-            AddUmbracoClient<IImportsClient, ImportsClient>(services, baseUrlResolver);
+            AddUmbracoClient<IAccountsClient, AccountsClient>(services, baseUrlResolver, httpClientFactory);
+            
+            AddUmbracoClient<IContentClient, ContentClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IContentTypesClient, ContentTypesClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IDataTypesClient, DataTypesClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IExportsClient, ExportsClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IImportsClient, ImportsClient>(services, baseUrlResolver, httpClientFactory);
 
-            AddUmbracoClient<IGivingClient, GivingClient>(services, baseUrlResolver);
-            AddUmbracoClient<ICartClient, CartClient>(services, baseUrlResolver);
-            AddUmbracoClient<ICheckoutClient, CheckoutClient>(services, baseUrlResolver);
+            AddUmbracoClient<IGivingClient, GivingClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<ICartClient, CartClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<ICheckoutClient, CheckoutClient>(services, baseUrlResolver, httpClientFactory);
             
-            AddUmbracoClient<INewslettersClient, NewslettersClient>(services, baseUrlResolver);
+            AddUmbracoClient<INewslettersClient, NewslettersClient>(services, baseUrlResolver, httpClientFactory);
             
-            AddUmbracoClient<IPaymentsClient, PaymentsClient>(services, baseUrlResolver);
-            AddUmbracoClient<IBamboraClient, BamboraClient>(services, baseUrlResolver);
-            AddUmbracoClient<IGoCardlessClient, GoCardlessClient>(services, baseUrlResolver);
-            AddUmbracoClient<IOpayoClient, OpayoClient>(services, baseUrlResolver);
-            AddUmbracoClient<IPayPalClient, PayPalClient>(services, baseUrlResolver);
-            AddUmbracoClient<IStripeClient, StripeClient>(services, baseUrlResolver);
+            AddUmbracoClient<IPaymentsClient, PaymentsClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IBamboraClient, BamboraClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IGoCardlessClient, GoCardlessClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IOpayoClient, OpayoClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IPayPalClient, PayPalClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IStripeClient, StripeClient>(services, baseUrlResolver, httpClientFactory);
             
-            AddUmbracoClient<ICropperClient, CropperClient>(services, baseUrlResolver);
-            AddUmbracoClient<IUploaderClient, UploaderClient>(services, baseUrlResolver);
+            AddUmbracoClient<ICropperClient, CropperClient>(services, baseUrlResolver, httpClientFactory);
+            AddUmbracoClient<IUploaderClient, UploaderClient>(services, baseUrlResolver, httpClientFactory);
         }
 
         private static void AddUmbracoClient<TService, TImplementation>(IServiceCollection services,
-                                                                        Func<IServiceProvider, string> baseUrlResolver)
+                                                                        Func<IServiceProvider, string> baseUrlResolver,
+                                                                        Func<IServiceProvider, HttpClient> httpClientFactory)
             where TService : class
             where TImplementation : class, TService {
             services.AddHttpClient();
             
             services.AddTransient<TService, TImplementation>(serviceProvider => {
                 var baseUrl = baseUrlResolver(serviceProvider);
-                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient();
+                var httpClient = httpClientFactory(serviceProvider);
                 
                 var implementation = (TImplementation) Activator.CreateInstance(typeof(TImplementation), httpClient);
                 
