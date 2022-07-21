d373e30293354e59d1ae37603216b8fb71a10d01
Noor Drummond <noor.drummond@n3o.ltd>
2022-07-21 15:26:56 +0100

    Added message bus projects

---
+++ src/MessageBus/N3O.Umbraco.MessageBus.Azure/AzureMessageBusComposer.cs
@@ -0,0 +1,10 @@
+﻿using N3O.Umbraco.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace N3O.Umbraco.MessageBus.Azure; 
+
+public class AzureMessageBusComposer : Composer {
+    public override void Compose(IUmbracoBuilder builder) {
+        
+    }
+}
