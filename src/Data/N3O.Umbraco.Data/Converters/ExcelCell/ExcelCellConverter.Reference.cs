deb057bb1dbca8b11ac564d37f230d2908e1174e
Noor Drummond <noor.drummond@n3o.ltd>
2022-07-22 08:47:45 +0100

    Prevent crash if reference is null

--- src/Data/N3O.Umbraco.Data/Converters/ExcelCell/ExcelCellConverter.Reference.cs
+++ src/Data/N3O.Umbraco.Data/Converters/ExcelCell/ExcelCellConverter.Reference.cs
@@ -5,7 +5,7 @@ namespace N3O.Umbraco.Data.Converters;
 
 public class ReferenceExcelCellConverter : ExcelCellConverter<Reference>, IDefaultExcelCellConverter {
     protected override object GetExcelValue(Column column, Reference value) {
-        return value.Text;
+        return value?.Text;
     }
 
     protected override ExcelNumberFormat GetNumberFormat(Column column, Reference value) {
