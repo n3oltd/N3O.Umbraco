efa213faf5aa596ec033c789c702c81d1b9b324e
PC <talhamalik20013@gmail.com>
2022-08-17 11:49:18 +0500

    Clear cache on usync item imported

--- src/N3O.Umbraco.Extensions/Content/ContentCache.I.cs
+++ src/N3O.Umbraco.Extensions/Content/ContentCache.I.cs
@@ -8,6 +8,7 @@ public interface IContentCache {
     IReadOnlyList<T> All<T>(Func<T, bool> predicate = null);
     IReadOnlyList<IPublishedContent> All(string contentTypeAlias, Func<IPublishedContent, bool> predicate = null);
     void Flush(IEnumerable<string> contentTypeAliases);
+    void Flush(string contentTypeAliases);
     T Single<T>(Func<T, bool> predicate = null);
     IPublishedContent Single(string contentTypeAlias, Func<IPublishedContent, bool> predicate = null);
 }
---
+++ src/Sync/N3O.Umbraco.Sync/Notifications/UsyncItemImported.cs
@@ -0,0 +1,23 @@
+﻿using N3O.Umbraco.Content;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Events;
+using uSync.BackOffice;
+
+namespace N3O.Umbraco.Sync;
+
+public class UsyncItemImported : INotificationAsyncHandler<uSyncImportedItemNotification> {
+    private readonly IContentCache _contentCache;
+
+    public UsyncItemImported(IContentCache contentCache) {
+        _contentCache = contentCache;
+    }
+
+    public Task HandleAsync(uSyncImportedItemNotification notification, CancellationToken cancellationToken) {
+        var alias = notification.Item.Attribute("Alias")?.Value;
+        
+        _contentCache.Flush(alias);
+        
+        return Task.CompletedTask;
+    }
+}
