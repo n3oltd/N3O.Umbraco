2ef9df2084393e17a3ce868eb5c56ae32ab42f44
PC <talhamalik20013@gmail.com>
2022-08-24 09:54:39 +0500

    Reorder

--- src/Authentication/N3O.Umbraco.Authentication.Auth0/Extensions/UmbracoBuilderExtensions.BackOffice.cs
+++ src/Authentication/N3O.Umbraco.Authentication.Auth0/Extensions/UmbracoBuilderExtensions.BackOffice.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
 using N3O.Umbraco.Authentication.Extensions;
 using System;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.BackOffice.Security;
@@ -37,20 +35,18 @@ public static partial class UmbracoBuilderExtensions {
                             opt.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
                             opt.TokenValidationParameters.NameClaimType = "name";
                             opt.TokenValidationParameters.RoleClaimType = "role";
-                            opt.TokenValidationParameters = new TokenValidationParameters {
-                                NameClaimType = ClaimTypes.NameIdentifier
-                            };
                             opt.RequireHttpsMetadata = true;
                             opt.GetClaimsFromUserInfoEndpoint = true;
                             opt.SaveTokens = true;
                             opt.UsePkce = true;
-                            opt.Scope.Add("openid");
-                            opt.Scope.Add("email");
-                            opt.Scope.Add("roles");
                             opt.Events.OnRedirectToIdentityProvider = context => {
                                 context.ProtocolMessage.SetParameter("audience", "https://n3o.ltd/karakoram");
                                 return Task.CompletedTask;
                             };
+                            
+                            opt.Scope.Add("openid");
+                            opt.Scope.Add("email");
+                            opt.Scope.Add("roles");
                         });
                 });
         });
