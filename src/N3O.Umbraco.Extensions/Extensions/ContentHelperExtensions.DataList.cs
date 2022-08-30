92ba100e5499caa14a0c8a73d25719cc47027df8
Noor Drummond <noor.drummond@n3o.ltd>
2022-08-30 14:33:53 +0100

    Added ContentHelper extensions for data list

---
+++ src/N3O.Umbraco.Extensions/Extensions/ContentHelperExtensions.DataList.cs
@@ -0,0 +1,24 @@
+﻿using N3O.Umbraco.Content;
+using System.Collections.Generic;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Community.Contentment.DataEditors;
+
+namespace N3O.Umbraco.Extensions; 
+
+public partial class ContentHelperExtensions {
+    public static T GetDataListValue<T>(this IContentHelper contentHelper,
+                                        IContent content,
+                                        string propertyAlias) {
+        return contentHelper.GetConvertedValue<DataListValueConverter, T>(content.ContentType.Alias,
+                                                                          propertyAlias,
+                                                                          content.GetValue(propertyAlias));
+    }
+    
+    public static IEnumerable<T> GetDataListValues<T>(this IContentHelper contentHelper,
+                                                      IContent content,
+                                                      string propertyAlias) {
+        return contentHelper.GetConvertedValue<DataListValueConverter, IEnumerable<T>>(content.ContentType.Alias,
+                                                                                       propertyAlias,
+                                                                                       content.GetValue(propertyAlias));
+    }
+}
