8c77762d53578402636ae18cdfa880f067fd342d
Noor Drummond <noor.drummond@n3o.ltd>
2022-09-02 19:57:15 +0100

    Added GetNestedContentPropertyByAlias extension

--- src/N3O.Umbraco.Extensions/Extensions/ContentPropertiesExtensions.cs
+++ src/N3O.Umbraco.Extensions/Extensions/ContentPropertiesExtensions.cs
@@ -4,6 +4,13 @@ using System.Linq;
 namespace N3O.Umbraco.Extensions;
 
 public static class ContentPropertiesExtensions {
+    public static NestedContentProperty GetNestedContentPropertyByAlias(this ContentProperties contentProperties, string alias) {
+        var nestedContentProperty = contentProperties.NestedContentProperties
+                                                     .SingleOrDefault(x => x.Alias.EqualsInvariant(alias));
+
+        return nestedContentProperty;
+    }
+    
     public static IContentProperty GetPropertyByAlias(this ContentProperties contentProperties, string alias) {
         IContentProperty contentProperty = contentProperties.Properties
                                                             .SingleOrDefault(x => x.Alias.EqualsInvariant(alias));
