963faf76deb7d164cc7a60dd186c3f510bcf62ba
PC <talhamalik20013@gmail.com>
2022-09-05 16:53:28 +0500

    Added Extension Method for Content Service

--- src/N3O.Umbraco.Extensions/Extensions/ContentServiceExtensions.cs
+++ src/N3O.Umbraco.Extensions/Extensions/ContentServiceExtensions.cs
@@ -1,5 +1,6 @@
 using N3O.Umbraco.Content;
 using System;
+using System.Collections.Generic;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
 
@@ -12,5 +13,17 @@ public static class ContentServiceExtensions {
 
     public static IContent Create<T>(this IContentService contentService, string name, Guid parentId) {
         return contentService.Create(name, parentId, AliasHelper<T>.ContentTypeAlias());
+    }
+
+    public static IEnumerable<IContent> GetAncestorsOrSelf(this IContentService contentService, IContent content) {
+        var list = new List<IContent>();
+        
+        list.Add(content);
+        
+        if (content.ParentId > 0) {
+            list.AddRange(contentService.GetAncestors(content.ParentId));
+        }
+
+        return list;
     }
 }
