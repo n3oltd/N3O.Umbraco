35c5aff2b3e8062cd4d27f7a57be2d73a4f09221
PC <talhamalik20013@gmail.com>
2022-09-30 15:50:54 +0500

    Moved constants to extensions

---
+++ src/N3O.Umbraco.Extensions/Constants/UserGroupPermissions.cs
@@ -0,0 +1,19 @@
+﻿namespace N3O.Umbraco.Constants; 
+
+public static class UserGroupPermissions {
+    public const string CulturesAndHostnames = "I";
+    public const string PublicAccess = "P";
+    public const string Rollback = "K";
+    public const string BrowseNode = "F";
+    public const string CreateContentTemplate = "ï";
+    public const string Delete = "D";
+    public const string Create = "C";
+    public const string Publish = "U";
+    public const string Permissions = "R";
+    public const string SendToPublish = "H";
+    public const string Unpublish = "Z";
+    public const string Update = "A";
+    public const string Copy = "O";
+    public const string Move = "M";
+    public const string Sort = "S";
+}
