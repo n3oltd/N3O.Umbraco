1bea8bccd391860b3042026c09dcdecb283ec2eb
Noor Drummond <noor.drummond@n3o.ltd>
2022-09-30 11:50:22 +0100

    Added umbraco permissions as constants

---
+++ src/N3O.Umbraco.Extensions/Constants/UmbracoPermissions.cs
@@ -0,0 +1,19 @@
+﻿namespace N3O.Umbraco.Constants; 
+
+public class UmbracoPermissions {
+    public const string BrowseNode = "F";
+    public const string Copy = "O";
+    public const string Create = "C";
+    public const string CreateContentTemplate = "ï";
+    public const string CulturesAndHostnames = "I";
+    public const string Delete = "D";
+    public const string Move = "M";
+    public const string Permissions = "R";
+    public const string PublicAccess = "P";
+    public const string Publish = "U";
+    public const string Rollback = "K";
+    public const string SendToPublish = "H";
+    public const string Sort = "S";
+    public const string Unpublish = "Z";
+    public const string Update = "A";
+}
