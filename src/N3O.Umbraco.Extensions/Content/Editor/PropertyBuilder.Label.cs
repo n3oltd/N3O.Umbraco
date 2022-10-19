f9e82480fc4e5c6dcad71af6f9172bc13472da71
sirajurrahmann <siraj.urrahman@n3o.ltd>
2022-10-19 11:46:57 +0500

    Resolve conflict.

--- src/N3O.Umbraco.Extensions/Content/Editor/PropertyBuilder.Label.cs
+++ src/N3O.Umbraco.Extensions/Content/Editor/PropertyBuilder.Label.cs
@@ -6,12 +6,4 @@ public class LabelPropertyBuilder : PropertyBuilder {
     public void Set(string value) {
         Value = value;
     }
-    
-    public void Set(DateTime? value) {
-        Value = value;
-    }
-    
-    public void Set(decimal? value) {
-        Value = value;
-    }
 }
