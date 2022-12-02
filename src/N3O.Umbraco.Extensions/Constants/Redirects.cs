ad497e15145dafec5cf3cab565e19f72613c9126
Noor Drummond <noor.drummond@n3o.ltd>
2022-12-02 16:33:04 +0000

    Added content validator for redirects

---
+++ src/N3O.Umbraco.Extensions/Constants/Redirects.cs
@@ -0,0 +1,13 @@
+﻿namespace N3O.Umbraco.Constants;
+
+public static class Redirects {
+    public const string ContentType = "redirect";
+
+    public static class Properties {
+        public const string LinkContent = "linkContent";
+        public const string LinkExternalUrl = "linkExternalUrl";
+        public const string Temporary = "temporary";
+        public const string HitCount = "hitCount";
+        public const string LastHitDate = "lastHitDate";
+    }
+}
---
+++ src/N3O.Umbraco.Extensions/Redirects/Validation/RedirectContentValidator.cs
@@ -0,0 +1,31 @@
+﻿using N3O.Umbraco.Content;
+using N3O.Umbraco.Extensions;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using RedirectsConstants = N3O.Umbraco.Constants.Redirects;
+
+namespace N3O.Umbraco.Redirects.Validation;
+
+public class RedirectContentValidator : ContentValidator  {
+    private readonly IContentHelper _contentHelper;
+
+    public RedirectContentValidator(IContentHelper contentHelper) : base(contentHelper) {
+        _contentHelper = contentHelper;
+    }
+    public override bool IsValidator(ContentProperties content) {
+        return content.ContentTypeAlias.EqualsInvariant(RedirectsConstants.ContentType);
+    }
+
+    public override void Validate(ContentProperties content) {
+        var externalUrl = content.GetPropertyValueByAlias<string>(RedirectsConstants.Properties.LinkExternalUrl);
+        var linkContent = _contentHelper.GetPickerValue<IPublishedContent>(content,
+                                                                           RedirectsConstants.Properties.LinkContent);
+
+        if (externalUrl == null && linkContent == null) {
+            ErrorResult("Either content or an external URL must be specified");
+        }
+
+        if (externalUrl.HasValue() && linkContent.HasValue()) {
+            ErrorResult("Content and an external URL cannot both be specified");
+        }
+    }
+}
