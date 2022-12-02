fdc073e3f04d89f98e069abf2f8b6fff717b05af
PC <talhamalik20013@gmail.com>
2022-12-02 17:13:58 +0500

    Added redirect validation

---
+++ src/N3O.Umbraco.Extensions/Redirects/RedirectConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace N3O.Umbraco.Redirects; 
+
+public static class ReadConstants {
+    public static class Redirect {
+        public const string ContentType = "redirect";
+
+        public static class Properties {
+            public const string LinkContent = "linkContent";
+            public const string LinkExternalUrl = "linkExternalUrl";
+            public const string Temporary = "temporary";
+            public const string HitCount = "hitCount";
+            public const string LastHitDate = "lastHitDate";
+        } 
+    }
+}
---
+++ src/N3O.Umbraco.Extensions/Redirects/Validation/RedirectValidator.cs
@@ -0,0 +1,29 @@
+﻿using N3O.Umbraco.Content;
+using N3O.Umbraco.Extensions;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace N3O.Umbraco.Redirects.Validation; 
+
+public class RedirectValidator : ContentValidator {
+    private readonly IContentHelper _contentHelper;
+
+    public RedirectValidator(IContentHelper contentHelper) : base(contentHelper) {
+        _contentHelper = contentHelper;
+    }
+    public override bool IsValidator(ContentProperties content) {
+        return content.ContentTypeAlias.EqualsInvariant(ReadConstants.Redirect.ContentType);
+    }
+
+    public override void Validate(ContentProperties content) {
+        var linkContent = _contentHelper.GetPickerValue<IPublishedContent>(content, ReadConstants.Redirect.Properties.LinkContent);
+        var externalUrl = content.GetPropertyValueByAlias<string>(ReadConstants.Redirect.Properties.LinkExternalUrl);
+
+        if (externalUrl == null && linkContent == null) {
+            ErrorResult("Both link content and external url cannot be null");
+        }
+
+        if (externalUrl != null && linkContent != null) {
+            ErrorResult("Only link content or external url can be specified");
+        }
+    }
+}
