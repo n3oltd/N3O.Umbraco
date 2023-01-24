33b7a1887e34da0ef60dadf61df1419c975ad8e8
Noor Drummond <noor.drummond@n3o.ltd>
2023-01-24 08:55:10 +0000

    Added missing registrations

--- src/Blazor/N3O.Umbraco.Blazor/BlazorComposer.cs
+++ src/Blazor/N3O.Umbraco.Blazor/BlazorComposer.cs
@@ -26,5 +26,19 @@ public class BlazorComposer : Composer {
 
             opt.AddFilter(filter);
         });
+
+        builder.Services.Configure<UmbracoPipelineOptions>(opt => {
+            var filter = new UmbracoPipelineFilter("MapBlazorHub");
+
+            filter.PrePipeline = app => {
+                var runtimeState = app.ApplicationServices.GetRequiredService<IRuntimeState>();
+
+                if (runtimeState.Level == RuntimeLevel.Run) {
+                    app.UseEndpoints(endpoints => endpoints.MapBlazorHub());
+                }
+            };
+
+            opt.AddFilter(filter);
+        });
     }
 }
---
+++ src/Blazor/N3O.Umbraco.Blazor/Extensions/UmbracoBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Server;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace N3O.Umbraco.Blazor.Extensions;
+
+public static class UmbracoBuilderExtensions {
+    public static IUmbracoBuilder AddBlazor(this IUmbracoBuilder builder, Action<CircuitOptions> configureOptions) {
+        builder.Services.AddServerSideBlazor(configureOptions);
+
+        return builder;
+    }
+}
