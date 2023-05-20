bb41f7bc9383c04af5e0c44bf1b3143c3fb30298
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2023-05-20 08:21:18 +0500

    Remove unused property

--- src/Giving/N3O.Umbraco.Giving/Models/FeedbackCustomField/NewFeedbackCustomField/FeedbackNewCustomFieldReqValidator.cs
+++ src/Giving/N3O.Umbraco.Giving/Models/FeedbackCustomField/NewFeedbackCustomField/FeedbackNewCustomFieldReqValidator.cs
@@ -1,16 +1,11 @@
 ﻿using FluentValidation;
 using N3O.Umbraco.Localization;
-using N3O.Umbraco.Lookups;
 using N3O.Umbraco.Validation;
 
 namespace N3O.Umbraco.Giving.Models;
 
 public class FeedbackNewCustomFieldReqValidator : ModelValidator<FeedbackNewCustomFieldReq> {
-    private readonly ILookups _lookups;
-
-    public FeedbackNewCustomFieldReqValidator(IFormatter formatter, ILookups lookups) : base(formatter) {
-        _lookups = lookups;
-        
+    public FeedbackNewCustomFieldReqValidator(IFormatter formatter) : base(formatter) {
         RuleFor(x => x.Alias)
            .NotNull()
            .WithMessage(Get<Strings>(s => s.SpecifyAlias));
