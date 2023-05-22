6027eec2964c2afbe0133662f22edd72c7eba8c6
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2023-05-22 11:08:35 +0500

    Add max alias length to request validator

--- src/Giving/N3O.Umbraco.Giving/Models/FeedbackCustomField/NewFeedbackCustomField/FeedbackNewCustomFieldReqValidator.cs
+++ src/Giving/N3O.Umbraco.Giving/Models/FeedbackCustomField/NewFeedbackCustomField/FeedbackNewCustomFieldReqValidator.cs
@@ -5,13 +5,21 @@ using N3O.Umbraco.Validation;
 namespace N3O.Umbraco.Giving.Models;
 
 public class FeedbackNewCustomFieldReqValidator : ModelValidator<FeedbackNewCustomFieldReq> {
+    private const int AliasMaxLength = 100;
+    
     public FeedbackNewCustomFieldReqValidator(IFormatter formatter) : base(formatter) {
         RuleFor(x => x.Alias)
            .NotNull()
            .WithMessage(Get<Strings>(s => s.SpecifyAlias));
+        
+        RuleFor(x => x.Alias)
+           .MaximumLength(AliasMaxLength)
+           .WithMessage(Get<Strings>(s => s.AliasTooLong_1, AliasMaxLength));
+            
     }
     
     public class Strings : ValidationStrings {
+        public string AliasTooLong_1 => "Alias exceeds maximum length of {0} characters";
         public string SpecifyAlias => "Please specify the custom field alias";
     }
 }
