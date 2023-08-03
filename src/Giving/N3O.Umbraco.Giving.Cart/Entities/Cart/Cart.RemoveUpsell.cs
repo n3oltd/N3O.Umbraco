6d20626881be2a6feebf7a4096d787aca1fbb56a
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2023-08-03 20:14:34 +0500

    Update addToUpsellHandler for multiple giving types

--- src/Giving/N3O.Umbraco.Giving.Cart/Entities/Cart/Cart.RemoveUpsell.cs
+++ src/Giving/N3O.Umbraco.Giving.Cart/Entities/Cart/Cart.RemoveUpsell.cs
@@ -1,16 +1,32 @@
+using N3O.Umbraco.Content;
 using N3O.Umbraco.Extensions;
+using N3O.Umbraco.Forex;
 using N3O.Umbraco.Giving.Cart.Models;
+using N3O.Umbraco.Giving.Lookups;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace N3O.Umbraco.Giving.Cart.Entities;
 
 public partial class Cart {
-    public void RemoveUpsell(CartContents contents, Guid upsellId) {
+    public async Task RemoveUpsellAsync(IContentLocator contentLocator,
+                                        IForexConverter forexConverter,
+                                        IPriceCalculator priceCalculator,
+                                        GivingType givingType,
+                                        Guid upsellId) {
+        await ReplaceContentsAsync(contentLocator,
+                                   forexConverter,
+                                   priceCalculator,
+                                   givingType,
+                                   c => RemoveContents(c, upsellId));
+    }
+
+    private CartContents RemoveContents(CartContents contents, Guid upsellId) {
         var newAllocations = contents.Allocations
                                      .ExceptWhere(x => x.UpsellId == upsellId)
                                      .ToList();
 
-        Donation = new CartContents(Currency, contents.Type, newAllocations);
+        return new CartContents(Currency, contents.Type, newAllocations);
     }
 }
