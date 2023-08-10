463765a8a4587289c169dd6e630ffdbcbada96fd
Noor Drummond <noor.drummond@n3o.ltd>
2023-08-10 09:51:46 +0100

    Allow custom checkout stages

---
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using N3O.Umbraco.Giving.Checkout.Lookups;
+using N3O.Umbraco.Lookups;
+using System.Linq;
+
+namespace N3O.Umbraco.Giving.Checkout.Extensions; 
+
+public static class ServiceCollectionExtensions {
+    public static IServiceCollection UseCustomCheckoutStages<T>(this IServiceCollection services)
+        where T : LookupsCollection<CheckoutStage> {
+        var serviceDescriptor = services.FirstOrDefault(x => x.ServiceType == typeof(CheckoutStagesCollection));
+
+        if (serviceDescriptor != null) {
+            services.Remove(serviceDescriptor);
+        }
+
+        services.AddTransient<ILookupsCollection<CheckoutStage>, T>();
+
+        return services;
+    }
+}
