f35b433ab3d89554e4dc01d95a43e4a74da7d76f
Noor Drummond <noor.drummond@n3o.ltd>
2023-09-21 11:32:32 +0100

    Fallback to app settings for SMTP

--- src/Email/N3O.Umbraco.Email.Smtp/SmtpEmailComposer.cs
+++ src/Email/N3O.Umbraco.Email.Smtp/SmtpEmailComposer.cs
@@ -1,11 +1,13 @@
 using FluentEmail.Core.Interfaces;
 using FluentEmail.Smtp;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using N3O.Umbraco.Composing;
 using N3O.Umbraco.Content;
 using N3O.Umbraco.Email.Smtp.Content;
 using System.Net;
 using System.Net.Mail;
+using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DependencyInjection;
 
 namespace N3O.Umbraco.Email.Smtp;
@@ -14,12 +16,31 @@ public class SmtpEmailComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.Services.AddSingleton<ISender>(serviceProvider => {
             var contentCache = serviceProvider.GetRequiredService<IContentCache>();
-            var settings = contentCache.Single<SmtpSettingsContent>();
+            var contentSettings = contentCache.Single<SmtpSettingsContent>();
 
-            var smtpClient = new SmtpClient(settings.Host, settings.Port);
+            string host;
+            int port;
+            string username;
+            string password;
+
+            if (contentSettings != null) {
+                host = contentSettings.Host;
+                port = contentSettings.Port;
+                username = contentSettings.Username;
+                password = contentSettings.Password;
+            } else {
+                var appSettings = serviceProvider.GetRequiredService<IOptions<GlobalSettings>>().Value.Smtp;
+                
+                host = appSettings.Host;
+                port = appSettings.Port;
+                username = appSettings.Username;
+                password = appSettings.Password;
+            }
+            
+            var smtpClient = new SmtpClient(host, port);
             smtpClient.EnableSsl = true;
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpClient.Credentials = new NetworkCredential(settings.Username, settings.Password);
+            smtpClient.Credentials = new NetworkCredential(username, password);
 
             return new SmtpSender(smtpClient);
         });
