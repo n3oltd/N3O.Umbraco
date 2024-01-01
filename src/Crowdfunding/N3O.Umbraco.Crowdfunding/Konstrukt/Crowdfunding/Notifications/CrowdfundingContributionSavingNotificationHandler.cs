51dd1329e3b7e54fd10aa5b1e9c211b4ab88bcce
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-01-01 13:28:54 +0500

    Added notification handler to reset name if updated by a user in konstrukt app

---
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Konstrukt/Crowdfunding/Notifications/CrowdfundingContributionSavingNotificationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Konstrukt.Events;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Events;
+
+namespace N3O.Umbraco.Crowdfunding.Konstrukt.Notifications; 
+
+public class CrowdfundingContributionSavingNotificationHandler : INotificationAsyncHandler<KonstruktEntitySavingNotification> {
+    public Task HandleAsync(KonstruktEntitySavingNotification notification, CancellationToken cancellationToken) {
+        if (notification.Entity.After is CrowdfundingContribution after) {
+            var before = (CrowdfundingContribution) notification.Entity.Before;
+
+            if (before.CheckoutReference != after.CheckoutReference) {
+                after.CheckoutReference = before.CheckoutReference;
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
