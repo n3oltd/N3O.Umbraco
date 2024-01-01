c9d433c160d8e35e2aa834478ecc9b2df7be7dfd
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-01-01 13:27:51 +0500

    Move to crowdfunding folder

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Konstrukt/Crowdfunding/CrowdfundingContributionsConfigurator.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Konstrukt/Crowdfunding/CrowdfundingContributionsConfigurator.cs
@@ -5,7 +5,7 @@ namespace N3O.Umbraco.Crowdfunding.Konstrukt;
 
 public class CrowdfundingContributionsConfigurator : IKonstruktConfigurator {
     public void Configure(KonstruktConfigBuilder builder) {
-        var section = builder.WithSection("crowdfunding");
+        var section = builder.AddSection("Crowdfunding");
         var dashboard = section.AddDashboard("Crowdfunding Donations");
 
         ConfigureCollection(dashboard);
@@ -22,7 +22,14 @@ public class CrowdfundingContributionsConfigurator : IKonstruktConfigurator {
         collection.SetAlias("allCrowdfundingDonations");
         collection.SetDateCreatedProperty(c => c.Timestamp);
         collection.SetNameProperty(c => c.CheckoutReference);
+        collection.AddFilterableProperty(c => c.CheckoutReference);
+        collection.AddSearchableProperty(c => c.Email);
+        collection.AddSearchableProperty(c => c.Name);
+        collection.AddSearchableProperty(c => c.TeamName);
+        collection.AddSearchableProperty(c => c.CampaignName);
+        collection.AddSearchableProperty(c => c.Comment);
         collection.DisableCreate();
+        collection.DisableDelete();
         collection.SetDataViewsBuilder<CrowdfundingContributionStatusDataViewsBuilder>();
 
         ConfigureListView(collection);
@@ -33,6 +40,9 @@ public class CrowdfundingContributionsConfigurator : IKonstruktConfigurator {
     private void ConfigureListView(KonstruktDashboardCollectionConfigBuilder<CrowdfundingContribution> collection) {
         var listView = collection.ListView();
         
+        listView.AddField(c => c.CampaignName);
+        listView.AddField(c => c.TeamName);
+        listView.AddField(c => c.Timestamp);
         listView.AddField(c => c.Status);
     }
 
@@ -41,7 +51,17 @@ public class CrowdfundingContributionsConfigurator : IKonstruktConfigurator {
         var recordTab = editor.AddTab("Donation");
         
         var recordFieldset = recordTab.AddFieldset("Donation");
+        recordFieldset.AddField(c => c.CheckoutReference).MakeReadOnly();
+        recordFieldset.AddField(c => c.Name).MakeReadOnly();
+        recordFieldset.AddField(c => c.Email).MakeReadOnly();
+        recordFieldset.AddField(c => c.Currency).MakeReadOnly();
+        recordFieldset.AddField(c => c.BaseAmount).MakeReadOnly();
+        recordFieldset.AddField(c => c.PageUrl).MakeReadOnly();
+        recordFieldset.AddField(c => c.CampaignName).MakeReadOnly();
+        recordFieldset.AddField(c => c.TeamName).MakeReadOnly();
+        recordFieldset.AddField(c => c.BaseTaxReliefAmount).MakeReadOnly();
         recordFieldset.AddField(c => c.Timestamp).MakeReadOnly();
         recordFieldset.AddField(c => c.Status).MakeReadOnly();
+        recordFieldset.AddField(c => c.Comment);
     }
 }
