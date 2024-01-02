6862c243ad45c639ee43aa7dfa76bf41044c617a
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-01-02 12:27:06 +0500

    Register crowdfunding writer

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/CrowdfundingComposer.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/CrowdfundingComposer.cs
@@ -1,4 +1,5 @@
-﻿using N3O.Umbraco.Composing;
+﻿using Microsoft.Extensions.DependencyInjection;
+using N3O.Umbraco.Composing;
 using N3O.Umbraco.Crowdfunding.Konstrukt;
 using Umbraco.Cms.Core.DependencyInjection;
 
@@ -7,5 +8,7 @@ namespace N3O.Umbraco.Crowdfunding;
 public class CrowdfundingComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.Components().Append<CrowdfundingContributionsMigrationsComponent>();
+        
+        builder.Services.AddSingleton<ICrowdfundingWriter, CrowdfundingWriter>();
     }
 }
