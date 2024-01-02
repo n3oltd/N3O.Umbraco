e87b0015e955fcb501914c7dd65409501dbb059e
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-01-02 12:27:33 +0500

    Update crowdfunding checkout feed

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/ChangeFeeds/CheckoutChangeFeed.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/ChangeFeeds/CheckoutChangeFeed.cs
@@ -2,30 +2,79 @@
 using N3O.Umbraco.Crowdfunding.Models;
 using N3O.Umbraco.Entities;
 using N3O.Umbraco.Giving.Checkout.Entities;
+using N3O.Umbraco.Giving.Lookups;
+using N3O.Umbraco.Giving.Models;
+using NodaTime;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace N3O.Umbraco.Crowdfunding.ChangeFeeds;
 
 public class CheckoutChangeFeed : ChangeFeed<Checkout> {
-    public CheckoutChangeFeed(ILogger<CheckoutChangeFeed> logger) : base(logger) { }
+    private readonly ICrowdfundingWriter _crowdfundingQ;
+    private readonly IClock _clock;
+    
+    public CheckoutChangeFeed(ILogger<CheckoutChangeFeed> logger,
+                              ICrowdfundingWriter crowdfundingQ,
+                              IClock clock) : base(logger) {
+        _crowdfundingQ = crowdfundingQ;
+        _clock = clock;
+    }
     
     protected override Task ProcessChangeAsync(EntityChange<Checkout> entityChange) {
-        if (entityChange.SessionEntity.Donation.Allocations.Any(x => x.HasCrowdfundingData())) {
-            if (entityChange.Operation == EntityOperations.Update) {
-                Process(entityChange, x => x.Donation.IsComplete, x => {
-                            
+        if (entityChange.Operation == EntityOperations.Update) {
+            if (entityChange.SessionEntity.Donation.Allocations.Any(x => x.HasCrowdfundingData())) {
+                Process(entityChange, x => x.Donation.IsComplete, async c => {
+                    await CommitAsync(GivingTypes.Donation, c);
+                });
+            }
+            
+            if (entityChange.SessionEntity.RegularGiving.Allocations.Any(x => x.HasCrowdfundingData())) {
+                Process(entityChange, x => x.RegularGiving.IsComplete, async c => {
+                    await CommitAsync(GivingTypes.RegularGiving, c);
                 });
             }
         }
-        // TODO If the checkout is newly completed, we will see if any of the allocation lines
-        // are for crowdfunding, and if they are we will extract those lines and insert them into the 
-        // SQL table.
-
+        
         return Task.CompletedTask;
     }
-    
+
+    private async Task CommitAsync(GivingType givingType, Checkout checkout) {
+        var crowdfundingAllocations = GetCrowdfundingAllocations(givingType, checkout);
+        
+        foreach (var crowdfundingAllocation in crowdfundingAllocations) {
+            var crowdfundingData = crowdfundingAllocation.GetCrowdfundingData();
+
+            await _crowdfundingQ.AppendAsync(checkout.Reference.Text,
+                                             _clock.GetCurrentInstant(),
+                                             crowdfundingData.CampaignId,
+                                             crowdfundingData.TeamId,
+                                             "sa",
+                                             crowdfundingData.PageId,
+                                             true,
+                                             crowdfundingData.PageUrl,
+                                             crowdfundingData.Comment,
+                                             crowdfundingAllocation.Value,
+                                             checkout.Account?.Email?.Address);
+        }
+
+        await _crowdfundingQ.CommitAsync();
+    }
+
+    private IReadOnlyList<Allocation> GetCrowdfundingAllocations(GivingType givingType, Checkout checkout) {
+        var crowdfundingAllocations = new List<Allocation>();
+
+        if (givingType == GivingTypes.Donation) {
+            crowdfundingAllocations = checkout.Donation.Allocations.Where(x => x.HasCrowdfundingData()).ToList();
+        } else if (givingType == GivingTypes.RegularGiving) {
+            crowdfundingAllocations = checkout.RegularGiving.Allocations.Where(x => x.HasCrowdfundingData()).ToList();
+        }
+
+        return crowdfundingAllocations;
+    }
+
     private void Process(EntityChange<Checkout> entityChange,
                          Func<Checkout, bool> getComplete,
                          Action<Checkout> action) {
