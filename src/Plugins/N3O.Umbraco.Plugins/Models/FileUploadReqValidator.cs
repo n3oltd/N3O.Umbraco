3931caeaac7c3e60bf7a2b4eb3b0259cbe873a6d
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-02-02 09:42:24 +0500

    remove namespaces

--- src/Plugins/N3O.Umbraco.Plugins/Models/FileUploadReqValidator.cs
+++ src/Plugins/N3O.Umbraco.Plugins/Models/FileUploadReqValidator.cs
@@ -1,8 +1,6 @@
-using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using N3O.Umbraco.Localization;
 using N3O.Umbraco.Plugins.Lookups;
-using N3O.Umbraco.Validation;
 using System.Linq;
 
 namespace N3O.Umbraco.Plugins.Models;
