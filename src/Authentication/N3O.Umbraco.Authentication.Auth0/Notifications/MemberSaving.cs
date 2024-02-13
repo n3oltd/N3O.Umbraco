7bf74fe789063ad89cefc2899e16fd6f3d93a3a7
Noor Drummond <noor.drummond@n3o.ltd>
2024-02-13 14:25:19 +0000

    Updates

---
+++ src/Authentication/N3O.Umbraco.Authentication.Auth0/Notifications/MemberSaving.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Options;
+using N3O.Umbraco.Authentication.Auth0.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace N3O.Umbraco.Authentication.Auth0.Notifications;
+
+public class MemberSaving : INotificationAsyncHandler<MemberSavingNotification> {
+    private readonly Lazy<IUserDirectory> _userDirectory;
+    private readonly Auth0MemberAuthenticationOptions _auth0MemberOptions;
+
+    public MemberSaving(Lazy<IUserDirectory> userDirectory,
+                        IOptions<Auth0MemberAuthenticationOptions> auth0MemberOptions) {
+        _userDirectory = userDirectory;
+        _auth0MemberOptions = auth0MemberOptions.Value;
+    }
+
+    public async Task HandleAsync(MemberSavingNotification notification, CancellationToken cancellationToken) {
+        if (_auth0MemberOptions.AutoCreateDirectoryUser) {
+            foreach (var user in notification.SavedEntities) {
+                await _userDirectory.Value.CreateUserIfNotExistsAsync(_auth0MemberOptions.ClientId,
+                                                                      _auth0MemberOptions.ConnectionName,
+                                                                      user.Email,
+                                                                      user.Name,
+                                                                      lastName: null);
+            }
+        }
+    }
+}
