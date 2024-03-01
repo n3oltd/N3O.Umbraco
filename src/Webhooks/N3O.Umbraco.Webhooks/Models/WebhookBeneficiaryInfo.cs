1a8b98d8b9c0a6a60e96148330dfa7b6237f20c9
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-03-01 12:09:20 +0500

    Add beneficiary webhook model

---
+++ src/Webhooks/N3O.Umbraco.Webhooks/Models/WebhookBeneficiaryInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace N3O.Umbraco.Webhooks.Models;
+
+public class WebhookBeneficiaryInfo : Value {
+    public WebhookBeneficiaryInfo(Guid id,
+                                  WebhookLookup scheme,
+                                  string type,
+                                  string reference,
+                                  string name,
+                                  string location) {
+        Id = id;
+        Scheme = scheme;
+        Type = type;
+        Reference = reference;
+        Name = name;
+        Location = location;
+    }
+
+    public Guid Id { get; }
+    public WebhookLookup Scheme { get; }
+    public string Type { get; }
+    public string Reference { get; }
+    public string Name { get; }
+    public string Location { get; }
+
+    protected override IEnumerable<object> GetAtomicValues() {
+        yield return Id;
+        yield return Scheme;
+        yield return Type;
+        yield return Reference;
+        yield return Name;
+        yield return Location;
+    }
+}
