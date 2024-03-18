194dad5990bee0d91d84fe662aeeddd39ec3838f
Noor Drummond <noor.drummond@n3o.ltd>
2024-03-18 19:51:57 +0000

    Upgrade NuGet

--- src/N3O.Umbraco.Extensions/Utilities/UtilitiesComposer.cs
+++ src/N3O.Umbraco.Extensions/Utilities/UtilitiesComposer.cs
@@ -1,3 +1,5 @@
+using Flurl.Http;
+using Flurl.Http.Newtonsoft;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using N3O.Umbraco.Composing;
@@ -8,6 +10,8 @@ namespace N3O.Umbraco.Utilities;
 
 public class UtilitiesComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
+        FlurlHttp.Clients.UseNewtonsoft();
+        
         builder.Services.AddTransient(typeof(Lazy<>), typeof(Lazier<>));
         builder.Services.TryAddSingleton<IUrlBuilder, UrlBuilder>();
     }
