ce3643561211394ed1dc230205f7f875929ebe7b
Noor Drummond <noor.drummond@n3o.ltd>
2024-03-26 20:59:14 +0000

    Code cleanup

--- src/N3O.Umbraco.Extensions/Utilities/CacheKeyBuilder.cs
+++ src/N3O.Umbraco.Extensions/Utilities/CacheKeyBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using N3O.Umbraco.Extensions;
+using Newtonsoft.Json;
+using NodaTime;
 using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 
 namespace N3O.Umbraco.Utilities;
 
@@ -9,14 +11,44 @@ public class CacheKeyBuilder {
 
     private CacheKeyBuilder() { }
     
-    public CacheKeyBuilder Append(string s) {
-        _values.Add(s);
+    public CacheKeyBuilder Append(string value) {
+        if (value.HasValue()) {
+            _values.Add(value.Trim().ToLowerInvariant());
+        }
 
         return this;
     }
+    
+    public CacheKeyBuilder Append(int? value) {
+        return Append(value?.ToString(CultureInfo.InvariantCulture));
+    }
+
+    public CacheKeyBuilder Append(long? value) {
+        return Append(value?.ToString(CultureInfo.InvariantCulture));
+    }
+    
+    public CacheKeyBuilder Append(float? value) {
+        return Append(value?.ToString(CultureInfo.InvariantCulture));
+    }
+    
+    public CacheKeyBuilder Append(decimal? value) {
+        return Append(value?.ToString(CultureInfo.InvariantCulture));
+    }
+    
+    public CacheKeyBuilder Append(bool? value) {
+        return Append(value?.ToString(CultureInfo.InvariantCulture));
+    }
+
+    public CacheKeyBuilder Append(LocalDate? value) {
+        return Append(value?.ToYearMonthDayString());
+    }
+    
+    public CacheKeyBuilder Append(object obj) {
+        return Append(obj.IfNotNull(JsonConvert.SerializeObject));
+    }
 
     public string Build() {
-        return _values.ExceptNull().Select(x => x.Trim()).Join("_").Sha1();
+        return _values.Join("_").Sha1();
     }
     
     public static CacheKeyBuilder Create() {
