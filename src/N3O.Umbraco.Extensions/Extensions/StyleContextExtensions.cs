d0c2c913e99909a8e4748fb09967532456be67c8
Noor Drummond <noor.drummond@n3o.ltd>
2024-03-31 18:56:40 +0100

    Added TemplateStyleId

---
+++ src/N3O.Umbraco.Extensions/Extensions/StyleContextExtensions.cs
@@ -0,0 +1,9 @@
+﻿using N3O.Umbraco.Templates;
+
+namespace N3O.Umbraco.Extensions;
+
+public static class StyleContextExtensions {
+    public static bool Has(this IStyleContext styleContext, string category, string name) {
+        return styleContext.Has(TemplateStyleId.Generate(category, name));
+    }
+}
---
+++ src/N3O.Umbraco.Extensions/Templates/TemplateStyleId.cs
@@ -0,0 +1,9 @@
+﻿using Humanizer;
+
+namespace N3O.Umbraco.Templates;
+
+public static class TemplateStyleId {
+    public static string Generate(string category, string name) {
+        return $"{category.Camelize()}_{name.Camelize()}";
+    }
+}
