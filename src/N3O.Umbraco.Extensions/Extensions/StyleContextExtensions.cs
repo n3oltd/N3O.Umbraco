ce811a467fc744dc4e99317f1269e0af03af412c
Noor Drummond <noor.drummond@n3o.ltd>
2024-04-01 09:55:48 +0100

    Added IsActive extension method

--- src/N3O.Umbraco.Extensions/Extensions/StyleContextExtensions.cs
+++ src/N3O.Umbraco.Extensions/Extensions/StyleContextExtensions.cs
@@ -6,4 +6,8 @@ public static class StyleContextExtensions {
     public static bool Has(this IStyleContext styleContext, string category, string name) {
         return styleContext.Has(TemplateStyleId.Generate(category, name));
     }
+    
+    public static bool IsActive(this IStyleContext styleContext, string category, string name) {
+        return styleContext.Get(category)?.Name.EqualsInvariant(name) ?? false;
+    }
 }
