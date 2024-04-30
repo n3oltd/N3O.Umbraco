1002dae677c674e40309a3a6ad78dd9262e91b30
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-04-30 11:58:31 +0500

    Used LatestChargeId property

--- src/Payments/N3O.Umbraco.Payments.Stripe/Models/StripePayment/StripePayment.IntentUpdated.cs
+++ src/Payments/N3O.Umbraco.Payments.Stripe/Models/StripePayment/StripePayment.IntentUpdated.cs
@@ -1,6 +1,5 @@
 using N3O.Umbraco.Exceptions;
 using Stripe;
-using System.Linq;
 
 namespace N3O.Umbraco.Payments.Stripe.Models;
 
@@ -9,9 +8,7 @@ public partial class StripePayment {
         ClearErrors();
 
         if (paymentIntent.Status == "succeeded") {
-            var charge = paymentIntent.LatestCharge;
-
-            Paid(charge.Id);
+            Paid(paymentIntent.LatestChargeId);
         } else if (paymentIntent.Status == "requires_action") {
             ActionRequired = true;
         } else {
