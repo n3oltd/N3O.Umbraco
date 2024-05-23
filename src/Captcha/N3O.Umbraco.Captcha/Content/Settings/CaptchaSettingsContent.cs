fec330ec51655867ff86a0840b8f234a4d89e41d
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2024-05-23 16:09:55 +0500

    cleanup

--- src/Captcha/N3O.Umbraco.Captcha/Content/Settings/CaptchaSettingsContent.cs
+++
@@ -1,8 +0,0 @@
-using N3O.Umbraco.Content;
-
-namespace N3O.Umbraco.Captcha.Content;
-
-public class CaptchaSettingsContent : UmbracoContent<CaptchaSettingsContent> {
-    public string SecretKey => GetValue(x => x.SecretKey);
-    public double Threshold => GetValue(x => x.Threshold);
-}
