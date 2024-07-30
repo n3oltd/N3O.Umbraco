9a22021029f06b716d273010e2a32b206ac99861
Talha Malik <n3o@n3o.ltd>
2024-07-30 15:22:22 +0500

    Add targets file to move assets and views into build project from crowdfunding

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding.StaticAssets/js/vite.config.ts
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding.StaticAssets/js/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "./build",
+    emptyOutDir: true,
+    assetsDir: './',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+      output: {
+        entryFileNames: 'crowdfunding.js',
+        assetFileNames: 'crowdfunding.css'
+      },
+    },
+  }
 })
