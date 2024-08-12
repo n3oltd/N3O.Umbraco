38482aaf4f4c8b53b8614092c98d93cf61356a59
Talha Malik <n3o@n3o.ltd>
2024-08-12 11:05:07 +0500

    Rename and move FundraisingPageUrls to CrowdfundingPageUrlConstants

---
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/CrowdfundingPageUrlConstants.cs
@@ -0,0 +1,24 @@
+﻿using N3O.Umbraco.Extensions;
+using System;
+
+namespace N3O.Umbraco.Crowdfunding;
+
+public class CrowdfundingPageUrlConstants {
+    public const string CrowdfundingContentPagePathPattern = "/Pages/([0-9]+)/([a-z0-9-]+)";
+    public const string CrowdfundingSignInPagePath = "/SignIn";
+    
+    public static string CrowdfundingPageRootPath;
+
+    public static string GetCrowdfundingPagePath(string path) {
+        return path.Substring(path.IndexOf(CrowdfundingPageRootPath, StringComparison.InvariantCultureIgnoreCase) +
+                              CrowdfundingPageRootPath.Length);
+    }
+    
+    public static void SetRootPath(string path) {
+        if (path.EqualsInvariant(CrowdfundingPageRootPath)) {
+            return;
+        }
+
+        CrowdfundingPageRootPath = path;
+    }
+}
