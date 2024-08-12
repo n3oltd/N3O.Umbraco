16399444e8260f996b38691722f74cc2fa23858d
Talha Malik <n3o@n3o.ltd>
2024-08-12 11:02:29 +0500

    Update FundraisingContentPageExtensions to poupulate properties

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Extensions/CrowdfundingContentPageExtensions.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Extensions/CrowdfundingContentPageExtensions.cs
@@ -1,4 +1,5 @@
-﻿using N3O.Umbraco.Crowdfunding.Content;
+﻿using N3O.Umbraco.Constants;
+using N3O.Umbraco.Crowdfunding.Content;
 using N3O.Umbraco.CrowdFunding.Models.FundraisingPage;
 using N3O.Umbraco.CrowdFunding.Services;
 using N3O.Umbraco.Crowdfunding.UIBuilder;
@@ -10,27 +11,29 @@ using Umbraco.Extensions;
 
 namespace N3O.Umbraco.CrowdFunding.Extensions;
 
-public static class FundraisingContentPageExtensions {
-    public static async Task<FundraisingContentPageViewModel> GetFundraisingContentPageViewModelAsync(this CrowdfundingPageContent content,
+public static class CrowdfundingContentPageExtensions {
+    public static async Task<CrowdfundingContentPageViewModel> GetCrowdfundingContentPageViewModelAsync(this CrowdfundingPageContent content,
                                                                                                       FundraisingPageHelper pageHelper) {
         var pageContributions = await pageHelper.GetContributionsForPageAsync(content.Content().Key);
 
-        var model = new FundraisingContentPageViewModel();
+        var model = new CrowdfundingContentPageViewModel();
         model.Content = content;
         model.Allocations = GetAllocations(pageHelper, content);
+        model.CampaignFundraisers = await GetCampaignFundraisersAsync(pageHelper, content);
+        model.OwnerInfo = GetOwnerInfo(content);
         model.Progress = GetPageProgress(pageHelper, pageContributions, content);
         model.PageDonations = GetPageDonations(pageHelper, pageContributions);
-        model.CampaignFundraisers = await GetCampaignFundraisersAsync(pageHelper, content);
+        
 
         return model;
     }
 
-    private static IEnumerable<FundraisingContentPageAllocation> GetAllocations(FundraisingPageHelper pageHelper,
-                                                                                CrowdfundingPageContent content) {
-        var allocations = new List<FundraisingContentPageAllocation>();
+    private static IEnumerable<CrowdfundingPageAllocation> GetAllocations(FundraisingPageHelper pageHelper,
+                                                                          CrowdfundingPageContent content) {
+        var allocations = new List<CrowdfundingPageAllocation>();
 
         foreach (var allocation in content.Allocations) {
-            var newAllocation = new FundraisingContentPageAllocation();
+            var newAllocation = new CrowdfundingPageAllocation();
             
             newAllocation.Title = allocation.Title;
             newAllocation.Amount = allocation.Amount;
@@ -42,10 +45,11 @@ public static class FundraisingContentPageExtensions {
             newAllocation.SponsorshipScheme = allocation.SponsorshipScheme;
             newAllocation.FeedbackScheme = allocation.FeedbackScheme;
             newAllocation.FeedbackScheme = allocation.FeedbackScheme;
-            var priceHandles = new List<FundraisingContentPageAllocationPriceHandle>();
+            
+            var priceHandles = new List<CrowdfundingPageAllocationPriceHandle>();
 
             foreach (var priceHandle in allocation.PriceHandles) {
-                var newPriceHandle = new FundraisingContentPageAllocationPriceHandle();
+                var newPriceHandle = new CrowdfundingPageAllocationPriceHandle();
                 newPriceHandle.Amount = pageHelper.GetQuoteMoney(priceHandle.Amount);
                 newPriceHandle.Description = priceHandle.Description;
                 
@@ -60,18 +64,20 @@ public static class FundraisingContentPageExtensions {
         return allocations;
     }
 
-    private static async Task<IEnumerable<FundraisingContentPageCampaignFundraisers>> GetCampaignFundraisersAsync(FundraisingPageHelper pageHelper,
-                                                                                                                  CrowdfundingPageContent content) {
+    private static async Task<IEnumerable<CrowdfundingPageCampaignFundraisers>> GetCampaignFundraisersAsync(FundraisingPageHelper pageHelper,
+                                                                                                            CrowdfundingPageContent content) {
         var campaignPages = pageHelper.GetAllPagesForCampaign(content.Campaign.Content().Key);
         var pageIds = campaignPages.OrEmpty().Select(x => x.Content().Key);
         var contributions = await pageHelper.GetContributionsForPagesAsync(pageIds);
-        var fundraisers = new List<FundraisingContentPageCampaignFundraisers>();
+        
+        var fundraisers = new List<CrowdfundingPageCampaignFundraisers>();
         
         foreach (var campaignPage in campaignPages) {
             var pageContributions = contributions.Where(x => x.PageId == campaignPage.Content().Key);
             
-            var fundraiser = new FundraisingContentPageCampaignFundraisers();
-            fundraiser.Name = campaignPage.FundraiserName;
+            var fundraiser = new CrowdfundingPageCampaignFundraisers();
+            fundraiser.Name = campaignPage.Fundraiser?.Name;
+            fundraiser.AvatarLink = campaignPage.Fundraiser?.Value<string>(MemberConstants.Member.Properties.AvatarLink);
             fundraiser.TargetAmount = pageHelper.GetQuoteMoney(campaignPage.Allocations.Sum(x => x.Amount));
             fundraiser.RaisedAmount = pageHelper.GetQuoteMoney(pageContributions.Sum(x => x.QuoteAmount.Normalize()));
 
@@ -80,7 +86,15 @@ public static class FundraisingContentPageExtensions {
 
         return fundraisers;
     }
-    
+
+    private static CrowdfundingPageOwner GetOwnerInfo(CrowdfundingPageContent content) {
+        var owner = new CrowdfundingPageOwner();
+        owner.Name = content.Fundraiser?.Name;
+        owner.AvatarLink = content.Fundraiser?.Value<string>(MemberConstants.Member.Properties.AvatarLink);
+
+        return owner;
+    }
+
     private static IReadOnlyList<FundraisingContentPageDonations> GetPageDonations(FundraisingPageHelper pageHelper,
                                                                                    IReadOnlyList<CrowdfundingContribution> contributions) {
         var donations = new List<FundraisingContentPageDonations>();
@@ -91,6 +105,9 @@ public static class FundraisingContentPageExtensions {
             donation.Comment = contribution.Comment;
             donation.DonatedAt = contribution.Timestamp;
             donation.Amount = pageHelper.GetQuoteMoney(contribution.QuoteAmount.Normalize());
+            donation.AvatarLink = GravatarGeneratorExtensions.GetGravatarUrl(contribution.Anonymous ?
+                                                                                 "Anonymous" :
+                                                                                 contribution.Name);
 
             if (!donation.IsAnonymous) {
                 donation.Name = contribution.Name;
@@ -102,10 +119,10 @@ public static class FundraisingContentPageExtensions {
         return donations;
     }
 
-    private static FundraisingContentPageProgress GetPageProgress(FundraisingPageHelper pageHelper,
-                                                                  IReadOnlyList<CrowdfundingContribution> pageContributions,
-                                                                  CrowdfundingPageContent content) {
-        var progress = new FundraisingContentPageProgress();
+    private static CrowdfundingPageProgress GetPageProgress(FundraisingPageHelper pageHelper,
+                                                            IReadOnlyList<CrowdfundingContribution> pageContributions,
+                                                            CrowdfundingPageContent content) {
+        var progress = new CrowdfundingPageProgress();
         progress.TargetAmount = pageHelper.GetQuoteMoney(content.Allocations.Sum(x => x.Amount));
         progress.RaisedAmount = pageHelper.GetQuoteMoney(pageContributions.Sum(x => x.BaseAmount.Normalize()));
         progress.SupportersCount = pageContributions.Count;
