738926d7d1c8f6e3dd6f3ee0fc9772e2018aa7ac
Talha Malik <n3o@n3o.ltd>
2024-08-12 11:06:34 +0500

    Remove unused files

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/FundraisingPage/ViewModels/FundraisingContentPage/FundraisingContentPageOwner.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace N3O.Umbraco.CrowdFunding.Models.FundraisingPage;
-
-public class FundraisingContentPageOwner {
-    public string OwnerName { get; set; }
-    public string OwnerAvatarLink { get; set; }
-    public string TeamName { get; set; }
-}
