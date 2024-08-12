902c41a3a281ae657266615de8a03be76e2ff474
Talha Malik <n3o@n3o.ltd>
2024-08-12 11:05:22 +0500

    Added extension to generate gravatr link

---
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Extensions/GravatarGeneratorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Flurl;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace N3O.Umbraco.CrowdFunding.Extensions;
+
+public static class GravatarGeneratorExtensions {
+    private const string GravatarBaseUrl = "https://www.gravatar.com/avatar";
+    
+    public static string GetGravatarUrl(string email, int size = 80) {
+        var emailHash = GetMd5Hash(email);
+        
+        var url = new Url(GravatarBaseUrl);
+        url.AppendPathSegment(emailHash);
+        url.SetQueryParam("s", size);
+        url.SetQueryParam("d", "identicon");
+        url.SetQueryParam("r", "g");
+
+        return url;
+    }
+
+    private static string GetMd5Hash(string email) {
+        using (MD5 md5 = MD5.Create()) {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(email);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+    }
+}
