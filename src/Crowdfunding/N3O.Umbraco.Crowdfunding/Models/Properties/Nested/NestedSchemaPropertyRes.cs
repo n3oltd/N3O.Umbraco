8a912987e2a56f73893b4076c13fac15d42d84dc
Talha Malik <n3o@n3o.ltd>
2024-08-15 15:53:43 +0500

    Added schema response to nested value response

---
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Properties/Nested/NestedSchemaPropertyRes.cs
@@ -0,0 +1,9 @@
+﻿using N3O.Umbraco.Attributes;
+using N3O.Umbraco.Crowdfunding.Lookups;
+
+namespace N3O.Umbraco.Crowdfunding.Models;
+
+public class NestedSchemaPropertyRes {
+    public string Alias { get; set; }
+    public PropertyType Type { get; set; }
+}
