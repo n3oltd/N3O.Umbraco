ee447cadcf45a3cbaf39a96ddab8f9e7b65b297c
Talha Malik <n3o@n3o.ltd>
2024-08-19 10:26:19 +0500

    CLeanup

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Properties/Nested/NestedSchemaItemRes.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Properties/Nested/NestedSchemaItemRes.cs
@@ -1,5 +1,4 @@
-﻿using N3O.Umbraco.Attributes;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace N3O.Umbraco.Crowdfunding.Models;
 
--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Properties/Nested/NestedSchemaPropertyRes.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Properties/Nested/NestedSchemaPropertyRes.cs
@@ -1,5 +1,4 @@
-﻿using N3O.Umbraco.Attributes;
-using N3O.Umbraco.Crowdfunding.Lookups;
+﻿using N3O.Umbraco.Crowdfunding.Lookups;
 
 namespace N3O.Umbraco.Crowdfunding.Models;
 
