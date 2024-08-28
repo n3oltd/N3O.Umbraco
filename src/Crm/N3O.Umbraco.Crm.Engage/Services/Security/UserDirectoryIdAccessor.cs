c57666ced28c973310f38afccb323e8912e01034
Talha Malik <n3o@n3o.ltd>
2024-08-28 10:26:41 +0500

    Build errors fix

--- src/Crm/N3O.Umbraco.Crm.Engage/Services/Security/UserDirectoryIdAccessor.cs
+++ src/Crm/N3O.Umbraco.Crm.Engage/Services/Security/UserDirectoryIdAccessor.cs
@@ -5,6 +5,7 @@ using N3O.Umbraco.Extensions;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Services;
 
 namespace N3O.Umbraco.Crm.Engage;
 
@@ -12,15 +13,18 @@ public class UserDirectoryIdAccessor : IUserDirectoryIdAccessor {
     private static readonly ConcurrentDictionary<string, string> Cache = new();
 
     private readonly IMemberManager _memberManager;
+    private readonly IMemberService _memberService;
     private readonly IUserDirectory _userDirectory;
     private readonly UserDirectoryId _userDirectoryId;
     private string _value;
 
     public UserDirectoryIdAccessor(IMemberManager memberManager,
                                    IUserDirectory userDirectory,
+                                   IMemberService memberService,
                                    UserDirectoryId userDirectoryId = null) {
         _memberManager = memberManager;
         _userDirectory = userDirectory;
+        _memberService = memberService;
         _userDirectoryId = userDirectoryId;
     }
 
@@ -29,11 +33,12 @@ public class UserDirectoryIdAccessor : IUserDirectoryIdAccessor {
             _value ??= _userDirectoryId?.Value;
 
             if (_value == null) {
-                var currentMember = await _memberManager.GetCurrentPublishedMemberAsync();
-
-                if (currentMember != null) {
-                    _value = await Cache.GetOrAddAtomicAsync(currentMember.Email, async () => {
-                        var directoryUser = await _userDirectory.GetUserByEmailAsync(ClientTypes.Members, currentMember.Email);
+                var publishedMember = await _memberManager.GetCurrentPublishedMemberAsync();
+                var member = _memberService.GetById(publishedMember.Id);
+                
+                if (member != null) {
+                    _value = await Cache.GetOrAddAtomicAsync(member.Email, async () => {
+                        var directoryUser = await _userDirectory.GetUserByEmailAsync(ClientTypes.Members, member.Email);
 
                         return directoryUser?.UserId;
                     });
