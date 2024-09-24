febf6f316a7549bb47cb2b65657e3e5868600436
Noor Drummond <noor.drummond@n3o.ltd>
2024-09-24 07:21:20 +0100

    Updated crowdfunding stats

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding.StaticAssets/App_Plugins/N3O.Umbraco.Crowdfunding.Statistics/N3O.Umbraco.Crowdfunding.Statistics.Controller.js
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding.StaticAssets/App_Plugins/N3O.Umbraco.Crowdfunding.Statistics/N3O.Umbraco.Crowdfunding.Statistics.Controller.js
@@ -1,4 +1,4 @@
 angular.module("umbraco")
-    .controller("N3O.Umbraco.Crowdfunding.Statistics", function ($scope, editorState, contentResource) {
-        
+    .controller("N3O.Umbraco.Crowdfunding.Statistics", function ($scope, editorState, contentResource, assetsService) {
+        assetsService.loadCss("~/App_Plugins/N3O.Umbraco.Crowdfunding.Statistics/N3O.Umbraco.Crowdfunding.Statistics.css");
     });
