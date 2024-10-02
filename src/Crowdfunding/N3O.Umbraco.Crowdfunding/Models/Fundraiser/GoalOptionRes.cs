709287f9b618ee6cfe38d7fbc7cdc3fdaf7b6494
Talha Malik <n3o@n3o.ltd>
2024-10-02 10:31:03 +0500

    Fix res

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Fundraiser/GoalOptionRes.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Fundraiser/GoalOptionRes.cs
@@ -8,7 +8,7 @@ public class GoalOptionRes {
     public string Id { get; set; }
     public string Name { get; set; }
     public AllocationType Type { get; set; }
-    public IEnumerable<string> Tags { get; set; }
+    public IEnumerable<TagRes> Tags { get; set; }
     public GoalOptionFundDimensionRes Dimension1 { get; set; }
     public GoalOptionFundDimensionRes Dimension2 { get; set; }
     public GoalOptionFundDimensionRes Dimension3 { get; set; }
