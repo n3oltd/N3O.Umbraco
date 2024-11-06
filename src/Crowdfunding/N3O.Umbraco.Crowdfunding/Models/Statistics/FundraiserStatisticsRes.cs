1d722002535eb7508004c7a87b1e6b744b50c2ab
Talha Malik <n3o@n3o.ltd>
2024-11-06 10:35:47 +0500

    Update models

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Statistics/FundraiserStatisticsRes.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Models/Statistics/FundraiserStatisticsRes.cs
@@ -2,12 +2,9 @@
 
 namespace N3O.Umbraco.Crowdfunding.Models;
 
-//TOP RIght Where counts
 public class FundraiserStatisticsRes : CrowdfunderStatisticsRes {
     public int ActiveCount { get; set; }
     public int NewCount { get; set; }
     public int CompletedCount { get; set; }
-    
-    //Secon chart on bottom
     public IEnumerable<FundraiserByCampaignStatisticsRes> ByCampaign { get; set; }
 }
