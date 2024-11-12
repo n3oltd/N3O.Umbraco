b20eae7767c1d0996f2c6dde5cf8e753007236e1
Talha Malik <n3o@n3o.ltd>
2024-11-12 11:30:56 +0500

    Add ordering to fix blocks not showing for BlockCategory

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Blocks/BlockCategories.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Blocks/BlockCategories.cs
@@ -1,8 +1,10 @@
+using N3O.Umbraco.Attributes;
 using N3O.Umbraco.Blocks;
 using N3O.Umbraco.Lookups;
 
 namespace N3O.Umbraco.Crowdfunding.Blocks; 
 
+[Order(int.MinValue)]
 public class BlockCategories : StaticLookupsCollection<BlockCategory> {
     public static readonly BlockCategory Advanced = new("advanced", "Advanced", "icon-code", 2);
 }
