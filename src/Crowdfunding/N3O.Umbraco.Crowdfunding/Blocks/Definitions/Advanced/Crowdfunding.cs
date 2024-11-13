b83a87711c36b1a4cf40dec4d8f6348dbd628fba
Talha Malik <talha.malik@n3o.ltd>
2024-11-13 16:35:07 +0500

    Find block category instead of using static Crowdfunding collection

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Blocks/Definitions/Advanced/Crowdfunding.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Blocks/Definitions/Advanced/Crowdfunding.cs
@@ -1,15 +1,23 @@
 using N3O.Umbraco.Blocks;
 using N3O.Umbraco.Crowdfunding.Content;
+using N3O.Umbraco.Lookups;
+using System;
 
 namespace N3O.Umbraco.Crowdfunding.Blocks.Definitions; 
 
 public class Crowdfunding : BlockBuilder {
     public Crowdfunding() {
+        var advancedCategory = StaticLookups.FindById<BlockCategory>("advanced");
+
+        if (advancedCategory == null) {
+            throw new Exception("A block category with ID advanced is required for crowdfunding");
+        }
+        
         WithAlias("crowdfunding");
         WithName("Crowdfunding");
         WithIcon("icon-users-alt");
         WithDescription("Block required to enable crowdfunding functionality");
-        AddToCategory(BlockCategories.Advanced);
+        AddToCategory(advancedCategory);
         LimitTo<HomePageContent>();
         SingleLayout();
     }
