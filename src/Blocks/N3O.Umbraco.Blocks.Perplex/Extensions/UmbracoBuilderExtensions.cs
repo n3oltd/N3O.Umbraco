ce1291b7dde9ccaaed598f2f7ff59b8362e43e31
Noor Drummond <noor@ankabut.com>
2024-12-04 10:21:20 +0000

    Fix type of model

--- src/Blocks/N3O.Umbraco.Blocks.Perplex/Extensions/UmbracoBuilderExtensions.cs
+++ src/Blocks/N3O.Umbraco.Blocks.Perplex/Extensions/UmbracoBuilderExtensions.cs
@@ -9,50 +9,50 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 namespace N3O.Umbraco.Blocks.Perplex.Extensions;
 
 public static class UmbracoBuilderExtensions {
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel>(this IUmbracoBuilder builder,
-                                                                        Func<BlockParameters<TBlock>, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel>(this IUmbracoBuilder builder,
+                                                                               Func<PerplexBlockParameters<TBlock>, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        return AddBlockViewModel<TBlock, TViewModel, None>(builder, (p, _) => constructor(p));
+        return AddPerplexBlockViewModel<TBlock, TViewModel, None>(builder, (p, _) => constructor(p));
     }
 
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T>(this IUmbracoBuilder builder,
-                                                                           Func<BlockParameters<TBlock>, T, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T>(this IUmbracoBuilder builder,
+                                                                                  Func<PerplexBlockParameters<TBlock>, T, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        return AddBlockViewModel<TBlock, TViewModel, T, None>(builder, (p, arg, _) => constructor(p, arg));
+        return AddPerplexBlockViewModel<TBlock, TViewModel, T, None>(builder, (p, arg, _) => constructor(p, arg));
     }
 
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T1, T2>(this IUmbracoBuilder builder,
-                                                                                Func<BlockParameters<TBlock>, T1, T2, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2>(this IUmbracoBuilder builder,
+                                                                                       Func<PerplexBlockParameters<TBlock>, T1, T2, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        return AddBlockViewModel<TBlock, TViewModel, T1, T2, None>(builder,
+        return AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, None>(builder,
                                                                    (p, arg1, arg2, _) => constructor(p, arg1, arg2));
     }
 
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T1, T2, T3>(this IUmbracoBuilder builder,
-                                                                                    Func<BlockParameters<TBlock>, T1, T2, T3, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3>(this IUmbracoBuilder builder,
+                                                                                           Func<PerplexBlockParameters<TBlock>, T1, T2, T3, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        return AddBlockViewModel<TBlock, TViewModel, T1, T2, T3, None>(builder,
+        return AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3, None>(builder,
                                                                        (p, arg1, arg2, arg3, _) => constructor(p, arg1, arg2, arg3));
     }
 
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4>(this IUmbracoBuilder builder,
-                                                                                        Func<BlockParameters<TBlock>, T1, T2, T3, T4, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4>(this IUmbracoBuilder builder,
+                                                                                               Func<PerplexBlockParameters<TBlock>, T1, T2, T3, T4, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        return AddBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, None>(builder,
-                                                                           (p, arg1, arg2, arg3, arg4, _) => constructor(p, arg1, arg2, arg3, arg4));
+        return AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, None>(builder,
+                                                                                  (p, arg1, arg2, arg3, arg4, _) => constructor(p, arg1, arg2, arg3, arg4));
                                                                        
     }
 
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, T5>(this IUmbracoBuilder builder,
-                                                                                            Func<BlockParameters<TBlock>, T1, T2, T3, T4, T5, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, T5>(this IUmbracoBuilder builder,
+                                                                                                   Func<PerplexBlockParameters<TBlock>, T1, T2, T3, T4, T5, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        AddBlockViewModel<TBlock, TViewModel>(builder, (s, p) => {
+        AddPerplexBlockViewModel<TBlock, TViewModel>(builder, (s, p) => {
             var arg1 = s.GetRequiredService<T1>();
             var arg2 = s.GetRequiredService<T2>();
             var arg3 = s.GetRequiredService<T3>();
@@ -65,11 +65,11 @@ public static class UmbracoBuilderExtensions {
         return builder;
     }
     
-    public static IUmbracoBuilder AddBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, T5, T6>(this IUmbracoBuilder builder,
-                                                                                                Func<BlockParameters<TBlock>, T1, T2, T3, T4, T5, T6, TViewModel> constructor)
+    public static IUmbracoBuilder AddPerplexBlockViewModel<TBlock, TViewModel, T1, T2, T3, T4, T5, T6>(this IUmbracoBuilder builder,
+                                                                                                       Func<PerplexBlockParameters<TBlock>, T1, T2, T3, T4, T5, T6, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
-        AddBlockViewModel<TBlock, TViewModel>(builder, (s, p) => {
+        AddPerplexBlockViewModel<TBlock, TViewModel>(builder, (s, p) => {
             var arg1 = s.GetRequiredService<T1>();
             var arg2 = s.GetRequiredService<T2>();
             var arg3 = s.GetRequiredService<T3>();
@@ -94,8 +94,8 @@ public static class UmbracoBuilderExtensions {
         return builder;
     }
 
-    private static void AddBlockViewModel<TBlock, TViewModel>(IUmbracoBuilder builder,
-                                                              Func<IServiceProvider, BlockParameters<TBlock>, TViewModel> constructor)
+    private static void AddPerplexBlockViewModel<TBlock, TViewModel>(IUmbracoBuilder builder,
+                                                                     Func<IServiceProvider, PerplexBlockParameters<TBlock>, TViewModel> constructor)
         where TViewModel : IPerplexBlockViewModel<TBlock>
         where TBlock : class, IPublishedElement {
         builder.Services.AddTransient<IContentBlockViewModelFactory<TBlock>>(s => {
