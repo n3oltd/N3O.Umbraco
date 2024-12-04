a52d63b1e74915432433993c21840b9bedb77682
Shagufta Humayun <shagufta.humayun@n3o.ltd>
2024-12-04 12:37:31 +0500

    added contents for checkoutSurvey

---
+++ src/Accounts/N3O.Umbraco.Accounts/Content/Settings/CheckoutSurveyQuestionContent.cs
@@ -0,0 +1,10 @@
+﻿using N3O.Umbraco.Content;
+using System.Collections.Generic;
+
+namespace N3O.Umbraco.Accounts.Content;
+
+public class CheckoutSurveyQuestionContent : UmbracoContent<CheckoutSurveyQuestionContent> {
+    public string Question => GetValue(x => x.Question);
+    public IEnumerable<string> Options => GetPickedAs(x => x.Options);
+    public int DimensionIndex => GetValue(x => x.DimensionIndex);
+}
