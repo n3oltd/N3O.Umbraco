b4c6a6834ba3a046b5aa3a9806bee257ef7284e8
Talha Malik <talha.malik@n3o.ltd>
2024-12-06 13:15:00 +0500

    Added FeedbackCustomFieldTypeDataSource

---
+++ src/Giving/N3O.Umbraco.Giving.Allocations/Lookups/FeedbackCustomFieldTypeDataSource.cs
@@ -0,0 +1,13 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Giving.Allocations.Lookups;
+
+public class FeedbackCustomFieldTypeDataSource : LookupsDataSource<FeedbackCustomFieldType> {
+    public FeedbackCustomFieldTypeDataSource(ILookups lookups) : base(lookups) { }
+    
+    public override string Name => "Feedback Custom Fields";
+    public override string Description => "Data source for feedback custom fields";
+    public override string Icon => " icon-edit";
+
+    protected override string GetIcon(FeedbackCustomFieldType feedbackCustomField) => " icon-edit";
+}
