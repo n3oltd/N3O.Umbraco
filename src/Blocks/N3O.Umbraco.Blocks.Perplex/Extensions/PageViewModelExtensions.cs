1e5b093fa155645a8b7b9a13468046781a1a7624
Noor Drummond <noor@ankabut.com>
2024-12-06 07:51:02 +0000

    Move from custom page view models to extension methods

---
+++ src/Blocks/N3O.Umbraco.Blocks.Perplex/Extensions/PageViewModelExtensions.cs
@@ -0,0 +1,12 @@
+﻿using N3O.Umbraco.Pages;
+using Perplex.ContentBlocks.Rendering;
+using System.Collections.Generic;
+using Umbraco.Cms.Core.Models.Blocks;
+
+namespace N3O.Umbraco.Blocks.Perplex.Extensions;
+
+public static class PageViewModelExtensions {
+    public static IContentBlocks ContentBlocks(this IPageViewModel pageViewModel) {
+        return pageViewModel.Content.GetProperty("blocks")?.GetValue() as IContentBlocks;
+    }
+}
---
+++ src/Blocks/N3O.Umbraco.Blocks/Extensions/PageViewModelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using N3O.Umbraco.Pages;
+using System.Collections.Generic;
+using Umbraco.Cms.Core.Models.Blocks;
+
+namespace N3O.Umbraco.Blocks.Extensions;
+
+public static class PageViewModelExtensions {
+    public static BlockGridModel BlockGrid(this IPageViewModel pageViewModel) {
+        return pageViewModel.Content.GetProperty("blocks")?.GetValue() as BlockGridModel;
+    }
+    
+    public static IEnumerable<BlockListItem> BlockListItems(this IPageViewModel pageViewModel) {
+        return pageViewModel.Content.GetProperty("blocks")?.GetValue() as IEnumerable<BlockListItem>;
+    }
+}
