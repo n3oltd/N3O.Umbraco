bc4bead0afb147d8d4b454c2bd128cb62c812d78
Noor Drummond <noor@ankabut.com>
2024-12-09 16:16:35 +0000

    Added extension method

---
+++ src/Blocks/N3O.Umbraco.Blocks/Extensions/BlockListModelExtensions.cs
@@ -0,0 +1,16 @@
+﻿using N3O.Umbraco.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Models.Blocks;
+
+namespace N3O.Umbraco.Blocks.Extensions;
+
+public static class BlockListModelExtensions {
+    public static T As<T>(this BlockListModel blockListModel) {
+        return (T) blockListModel.OrEmpty().SingleOrDefault()?.Content;
+    }
+    
+    public static IReadOnlyList<T> AsCollectionOf<T>(this BlockListModel blockListModel) {
+        return blockListModel.OrEmpty().Select(x => (T) x.Content).ToList();
+    }
+}
