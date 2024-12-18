69175f09add2c03d5ef5ff03f6b5ff7eee0e3d41
Noor Drummond <noor@ankabut.com>
2024-12-18 12:39:09 +0000

    Use our markup rendering engine when referencing our package

---
+++ src/Markup/N3O.Umbraco.Markup.Markdown/DataTypes/OurMarkdownValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+using Umbraco.Cms.Core.Strings;
+using Umbraco.Cms.Core.Templates;
+using Umbraco.Extensions;
+
+namespace N3O.Umbraco.Markup.Markdown.DataTypes;
+
+public class OurMarkdownValueConverter : MarkdownEditorValueConverter {
+    private readonly IMarkupEngine _markupEngine;
+
+    public OurMarkdownValueConverter(HtmlLocalLinkParser localLinkParser,
+                                     HtmlUrlParser urlParser,
+                                     IMarkupEngine markupEngine)
+        : base(localLinkParser, urlParser) {
+        _markupEngine = markupEngine;
+    }
+
+    public override Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(IHtmlEncodedString);
+
+    public override object ConvertIntermediateToObject(IPublishedElement owner,
+                                                       IPublishedPropertyType propertyType,
+                                                       PropertyCacheLevel referenceCacheLevel,
+                                                       object inter,
+                                                       bool preview) {
+        var html = default(IHtmlEncodedString);
+        
+        if (inter is string markdown) {
+            html = _markupEngine.RenderHtml(markdown).IfNotNull(x => new HtmlEncodedString(x.ToString()));
+        }
+        
+        return html;
+    }
+}
--- src/Markup/N3O.Umbraco.Markup.Markdown/MarkdownComposer.cs
+++ src/Markup/N3O.Umbraco.Markup.Markdown/MarkdownComposer.cs
@@ -2,13 +2,17 @@ using Markdig;
 using Microsoft.Extensions.DependencyInjection;
 using N3O.Umbraco.Composing;
 using N3O.Umbraco.Extensions;
+using N3O.Umbraco.Markup.Markdown.DataTypes;
 using System.Linq;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
 
 namespace N3O.Umbraco.Markup.Markdown;
 
-public class MarkdownMarkupComposer : Composer {
+public class MarkdownComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
+        builder.PropertyValueConverters().Replace<MarkdownEditorValueConverter, OurMarkdownValueConverter>();
+        
         builder.Services.AddSingleton<IMarkupEngine, MarkdownEngine>();
 
         RegisterAll(t => t.ImplementsInterface<IMarkdownExtension>(),
