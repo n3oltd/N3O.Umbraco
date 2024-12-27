b60df1eac7dd3e821e86b4731f4ec1b8c3a4f456
Talha Malik <talha.malik@n3o.ltd>
2024-12-27 11:57:26 +0500

    Move GetYouTubeVideoId to extension methods

---
+++ src/Video/N3O.Umbraco.Video.YouTube/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace N3O.Umbraco.Video.YouTube.Extensions;
+
+public static class StringExtensions {
+    public static string GetYouTubeVideoId(this string videoUrl) {
+        try {
+            var match = Regex.Match(videoUrl, @"((?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)");
+
+            if (match.Success) {
+                return match.Groups[0].Value;
+            }
+        } catch { }
+
+        return null;
+    }
+}
