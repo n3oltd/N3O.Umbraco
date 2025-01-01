a5964e7c4fdcc93ed8057a6815821a89888506e5
Talha Malik <talha.malik@n3o.ltd>
2025-01-01 14:41:46 +0500

    Remove configure smidge

--- src/Bundling/N3O.Umbraco.Bundling/BundlingComposer.cs
+++ src/Bundling/N3O.Umbraco.Bundling/BundlingComposer.cs
@@ -1,13 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using N3O.Umbraco.Composing;
 using N3O.Umbraco.Extensions;
-using Smidge;
-using Smidge.Cache;
-using Smidge.FileProcessors;
-using Smidge.Nuglify;
-using Smidge.Options;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Web.Common.ApplicationBuilder;
 
 namespace N3O.Umbraco.Bundling;
 
@@ -17,31 +11,5 @@ public class BundlingComposer : Composer {
         
         RegisterAll(t => t.ImplementsInterface<IAssetBundle>(),
                     t => builder.Services.AddTransient(typeof(IAssetBundle), t));
-
-        ConfigureSmidge(builder);
-    }
-
-    private void ConfigureSmidge(IUmbracoBuilder builder) {
-        builder.Services.AddSmidgeNuglify();
-        
-        builder.Services.Configure<SmidgeOptions>(opt => {
-            opt.CacheOptions = new SmidgeCacheOptions {
-                UseInMemoryCache = true
-            };
-            
-            opt.DefaultBundleOptions.DebugOptions.SetCacheBusterType<AppDomainLifetimeCacheBuster>();
-            opt.DefaultBundleOptions.ProductionOptions.SetCacheBusterType<AppDomainLifetimeCacheBuster>();
-            
-            opt.PipelineFactory.OnCreateDefault = (_, pipeline) => {
-                return pipeline.Replace<JsMinifier, NuglifyJs>(opt.PipelineFactory);
-            };
-        });
-        
-        builder.Services.Configure<UmbracoPipelineOptions>(opt => {
-            var filter = new UmbracoPipelineFilter("Bundling");
-            filter.Endpoints = app => app.UseSmidge();
-            
-            opt.AddFilter(filter);
-        });
     }
 }
