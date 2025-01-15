e360382c5b16c46cdb0a018fdf040aa3921d9af2
Talha Malik <n3o@n3o.ltd>
2025-01-15 12:35:54 +0500

    Added CrowdfundingApiHeaderOperationProcessor

---
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Filters/CrowdfundingApiHeaderOperationProcessor.cs
@@ -0,0 +1,18 @@
+﻿using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+
+namespace N3O.Umbraco.Crowdfunding.Attributes;
+
+public class CrowdfundingApiHeaderOperationProcessor : IOperationProcessor {
+    public bool Process(OperationProcessorContext context) {
+        context.OperationDescription.Operation.Parameters.Add(new OpenApiParameter {
+            Name = CrowdfundingConstants.Http.Headers.ApiHeaderKey,
+            Kind = OpenApiParameterKind.Header,
+            Type = NJsonSchema.JsonObjectType.String,
+            IsRequired = false
+        });
+
+        return true;
+    }
+}
