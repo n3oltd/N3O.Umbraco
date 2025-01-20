ba53651ff298a9543397cca7cf3adf78bed82003
Talha Malik <n3o@n3o.ltd>
2025-01-20 19:04:16 +0500

    Added elements configuration

---
+++ src/Elements/N3O.Umbraco.Elements/Notifications/ElementsSettingsPublished.cs
@@ -0,0 +1,27 @@
+﻿using N3O.Umbraco.Content;
+using N3O.Umbraco.Elements.Extensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace N3O.Umbraco.Elements.Notifications;
+
+public class ElementsSettingsPublished : INotificationAsyncHandler<ContentPublishedNotification> {
+    private readonly Lazy<IElementsManager> _elementsManager;
+
+    public ElementsSettingsPublished(Lazy<IElementsManager> elementsManager) {
+        _elementsManager = elementsManager;
+    }
+    
+    public Task HandleAsync(ContentPublishedNotification notification, CancellationToken cancellationToken) {
+        foreach (var content in notification.PublishedEntities) {
+            if (content.IsElementsSettings()) {
+                _elementsManager.Value.SaveAndPublishElementsSettings().GetAwaiter().GetResult();
+            }
+        }
+        
+        return Task.CompletedTask;
+    }
+}
