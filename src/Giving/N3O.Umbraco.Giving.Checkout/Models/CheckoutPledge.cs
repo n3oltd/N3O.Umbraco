e7dd2f0f6ea558f0945bf1e425a2a3d877545ac8
Shagufta Humayun <shagufta.humayun@n3o.ltd>
2025-02-07 12:48:39 +0500

    updated the donationCheckout

---
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Models/CheckoutPledge.cs
@@ -0,0 +1,8 @@
+﻿namespace N3O.Umbraco.Giving.Checkout.Models;
+
+public class CheckoutPledge {
+    public CheckoutPledge(string pledgeUrl) {
+        PledgeUrl = pledgeUrl;
+    }
+    public string PledgeUrl { get; }
+}
