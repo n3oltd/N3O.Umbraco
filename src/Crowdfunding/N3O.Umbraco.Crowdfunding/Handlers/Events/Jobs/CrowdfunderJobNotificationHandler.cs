d03d995d5e5909c1719bcc0f5c2013b8fa360e21
Talha Malik <talha.malik@n3o.ltd>
2025-02-14 19:12:53 +0500

    Remove suppress notification

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Handlers/Events/Jobs/CrowdfunderJobNotificationHandler.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Handlers/Events/Jobs/CrowdfunderJobNotificationHandler.cs
@@ -18,50 +18,36 @@ namespace N3O.Umbraco.Crowdfunding.Handlers;
 public abstract class CrowdfunderJobNotificationHandler<TJobNotification> :
     IRequestHandler<TJobNotification, JobResult, None>
     where TJobNotification : CrowdfunderJobNotification {
-    private readonly AsyncKeyedLocker<string> _asyncKeyedLocker;
     private readonly IContentService _contentService;
     private readonly IBackgroundJob _backgroundJob;
-    private readonly ICoreScopeProvider _coreScopeProvider;
 
     protected CrowdfunderJobNotificationHandler(AsyncKeyedLocker<string> asyncKeyedLocker,
                                                 IContentService contentService,
                                                 IBackgroundJob backgroundJob,
                                                 ICoreScopeProvider coreScopeProvider) {
-        _asyncKeyedLocker = asyncKeyedLocker;
         _contentService = contentService;
         _backgroundJob = backgroundJob;
-        _coreScopeProvider = coreScopeProvider;
     }
 
     public async Task<None> Handle(TJobNotification req, CancellationToken cancellationToken) {
-        using (await _asyncKeyedLocker.LockAsync(req.ContentId.Value.ToString(), cancellationToken)) {
-            var content = GetContent(req.ContentId.Value);
-
-            if (content.HasValue()) {
-                if (req.Model.Success) {
-                    await HandleNotificationAsync(req, content);
+        var content = GetContent(req.ContentId.Value);
+             
+        if (content.HasValue()) {
+            if (req.Model.Success) {
+                await HandleNotificationAsync(req, content);
+                             
+                ClearError(content);
                 
-                    ClearError(content);
-                } else {
-                    SetError(content, req.Model);
-                    
-                    content.SetValue(CrowdfundingConstants.Crowdfunder.Properties.ToggleStatus, false);
-                }
+                _contentService.SaveAndPublish(content);
+            } else {
+                SetError(content, req.Model);
+                                 
+                content.SetValue(CrowdfundingConstants.Crowdfunder.Properties.ToggleStatus, false);
                 
-                if (typeof(TJobNotification) == typeof(CrowdfunderUpdatedJobNotification)) {
-                    using var scope = _coreScopeProvider.CreateCoreScope(autoComplete:true);
-                    
-                    using (_ = scope.Notifications.Suppress()) {
-                        _contentService.SaveAndPublish(content);
-
-                        scope.Complete();
-                    }
-                } else {
-                    _contentService.SaveAndPublish(content);
-                }
-                
-                _backgroundJob.EnqueueCrowdfunderUpdated(content.Key, content.ContentType.Alias.ToCrowdfunderType());
+                _contentService.Save(content);
             }
+                             
+            _backgroundJob.EnqueueCrowdfunderUpdated(content.Key, content.ContentType.Alias.ToCrowdfunderType());
         }
 
         return None.Empty;
