9bbb25c1c1209bfb99ae5fb34583b605527b5a07
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2025-05-05 11:54:02 +0500

    Formatting

--- src/N3O.Umbraco.Extensions/Extensions/DateTimeExtensions.cs
+++ src/N3O.Umbraco.Extensions/Extensions/DateTimeExtensions.cs
@@ -2,18 +2,29 @@ using N3O.Umbraco.Localization;
 using NodaTime;
 using NodaTime.Extensions;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace N3O.Umbraco.Extensions;
 
 public static class DateTimeExtensions {
     private static List<string> HijriMonthNames = new() {
-            "Muharram", "Safar", "Rabi' al-awwal", "Rabi' al-thani",
-            "Jumada al-awwal", "Jumada al-thani", "Rajab", "Sha'ban",
-            "Ramadan", "Shawwal", "Dhu al-Qi'dah", "Dhu al-Hijjah"
-        };
-    
+        "Muharram",
+        "Safar",
+        "Rabi' al-awwal",
+        "Rabi' al-thani",
+        "Jumada al-awwal",
+        "Jumada al-thani",
+        "Rajab",
+        "Sha'ban",
+        "Ramadan",
+        "Shawwal",
+        "Dhu al-Qi'dah",
+        "Dhu al-Hijjah"
+    };
+
     public static bool HasValue(this DateTime dt) {
-        return HasValue((DateTime?)dt);
+        return HasValue((DateTime?) dt);
     }
 
     public static bool HasValue(this DateTime? dt) {
@@ -30,29 +41,29 @@ public static class DateTimeExtensions {
         }
 
         var zonedDateTime = new ZonedDateTime(Instant.FromDateTimeUtc(dateTime), timezone.Zone);
-    
-        return  zonedDateTime;
+
+        return zonedDateTime;
     }
     
     public static DateTime ToHijriDateTime(this DateTime dateTime) {
-            var hijriCalendar = new HijriCalendar();
-    
-            var day = hijriCalendar.GetDayOfMonth(dateTime);
-            var month = hijriCalendar.GetMonth(dateTime);
-            var year = hijriCalendar.GetYear(dateTime);
-    
-            var hour = hijriCalendar.GetHour(dateTime);
-            var minute = hijriCalendar.GetMinute(dateTime);
-            var second = hijriCalendar.GetSecond(dateTime);
-            var milliseconds = (int) hijriCalendar.GetMilliseconds(dateTime);
-    
-            return new DateTime(year, month, day, hour, minute, second, milliseconds);
-        }
-    
-        public static string ToHijriMonthName(this DateTime dateTime) {
-            return HijriMonthNames[dateTime.Month];
-        }
-    
+        var hijriCalendar = new HijriCalendar();
+
+        var day = hijriCalendar.GetDayOfMonth(dateTime);
+        var month = hijriCalendar.GetMonth(dateTime);
+        var year = hijriCalendar.GetYear(dateTime);
+
+        var hour = hijriCalendar.GetHour(dateTime);
+        var minute = hijriCalendar.GetMinute(dateTime);
+        var second = hijriCalendar.GetSecond(dateTime);
+        var milliseconds = (int) hijriCalendar.GetMilliseconds(dateTime);
+
+        return new DateTime(year, month, day, hour, minute, second, milliseconds);
+    }
+
+    public static string ToHijriMonthName(this DateTime dateTime) {
+        return HijriMonthNames[dateTime.Month];
+    }
+
     public static LocalDate ToLocalDate(this DateTime dateTime) {
         return dateTime.ToLocalDateTime().Date;
     }
