9df8b0e40a30ae4633caa54c5ee730ef84f50193
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2025-05-05 11:52:02 +0500

    Added extension method to convert to hijri date

--- src/N3O.Umbraco.Extensions/Extensions/DateTimeExtensions.cs
+++ src/N3O.Umbraco.Extensions/Extensions/DateTimeExtensions.cs
@@ -6,6 +6,12 @@ using System;
 namespace N3O.Umbraco.Extensions;
 
 public static class DateTimeExtensions {
+    private static List<string> HijriMonthNames = new() {
+            "Muharram", "Safar", "Rabi' al-awwal", "Rabi' al-thani",
+            "Jumada al-awwal", "Jumada al-thani", "Rajab", "Sha'ban",
+            "Ramadan", "Shawwal", "Dhu al-Qi'dah", "Dhu al-Hijjah"
+        };
+    
     public static bool HasValue(this DateTime dt) {
         return HasValue((DateTime?)dt);
     }
@@ -27,6 +33,25 @@ public static class DateTimeExtensions {
     
         return  zonedDateTime;
     }
+    
+    public static DateTime ToHijriDateTime(this DateTime dateTime) {
+            var hijriCalendar = new HijriCalendar();
+    
+            var day = hijriCalendar.GetDayOfMonth(dateTime);
+            var month = hijriCalendar.GetMonth(dateTime);
+            var year = hijriCalendar.GetYear(dateTime);
+    
+            var hour = hijriCalendar.GetHour(dateTime);
+            var minute = hijriCalendar.GetMinute(dateTime);
+            var second = hijriCalendar.GetSecond(dateTime);
+            var milliseconds = (int) hijriCalendar.GetMilliseconds(dateTime);
+    
+            return new DateTime(year, month, day, hour, minute, second, milliseconds);
+        }
+    
+        public static string ToHijriMonthName(this DateTime dateTime) {
+            return HijriMonthNames[dateTime.Month];
+        }
     
     public static LocalDate ToLocalDate(this DateTime dateTime) {
         return dateTime.ToLocalDateTime().Date;
