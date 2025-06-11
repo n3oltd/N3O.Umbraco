b52b52bd4081b10a27b6db46832f45be10d1e0b3
Talha Malik <talha.malik@n3o.ltd>
2025-06-11 10:58:14 +0500

    Update

--- src/Blocks/N3O.Umbraco.Blocks.Perplex/Services/BlocksRenderer.Perplex.cs
+++ src/Blocks/N3O.Umbraco.Blocks.Perplex/Services/BlocksRenderer.Perplex.cs
@@ -1,25 +1,35 @@
-﻿using Microsoft.AspNetCore.Html;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Http;
 using Perplex.ContentBlocks.Rendering;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace N3O.Umbraco.Blocks.Perplex;
 
 public class PerplexBlocksRenderer : BlocksRenderer<ContentBlocks> {
-    private readonly IContentBlockRenderer _contentBlockRenderer;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public PerplexBlocksRenderer(IContentBlockRenderer contentBlockRenderer) {
-        _contentBlockRenderer = contentBlockRenderer;
+    public PerplexBlocksRenderer(IHttpContextAccessor httpContextAccessor) {
+        _httpContextAccessor = httpContextAccessor;
     }
 
     protected override IEnumerable<IBlockViewModel> GetViewModels(ContentBlocks contentBlocks) {
-        var factory = new PerplexBlockViewModelFactory<IPublishedElement, EmailBuilderBlockViewModel>(_httpContextAccessor,
-                                                                                                      (_, p) => new EmailBuilderBlockViewModel(p, composition, _assets, _emailLinkBuilder, _styleContext));
+        foreach (var contentBlockModel in contentBlocks.Blocks) {
+            var factory = new PerplexBlockViewModelFactory<IPublishedElement, PerplexBlockViewModel<IPublishedElement>>(_httpContextAccessor,
+                                                                                                                        (_, p) => new PerplexBlockViewModel<IPublishedElement>(p));
+        
+            var blockModel = (IPerplexBlockViewModel) factory.Create(contentBlockModel.Content,
+                                                                     contentBlockModel.Id,
+                                                                     contentBlockModel.DefinitionId,
+                                                                     contentBlockModel.LayoutId);
+
+            yield return blockModel;
+        }
     }
 
     protected override IEnumerable<string> GetViewPaths(ContentBlocks contentBlocks) {
-        throw new System.NotImplementedException();
+        foreach (var blockItem in contentBlocks.Blocks) {
+            yield return $"Views/Blocks/{blockItem.Content.ContentType.Alias.Pascalize()}/Default.cshtml";
+        }
     }
 }
