e45ce57188b08eca5afc9fac3d387c2212b779b0
Talha Malik <talha.malik@n3o.ltd>
2025-06-23 18:52:28 +0500

    Fix layout

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Content/Settings/DataEntry/NameEntryContent.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Content/Settings/DataEntry/NameEntryContent.cs
@@ -6,5 +6,5 @@ namespace N3O.Umbraco.Cloud.Platforms.Content;
 
 [UmbracoContent(PlatformsConstants.Settings.DataEntry.Name)]
 public class NameEntryContent : UmbracoContent<NameEntryContent> {
-    public AddressLayout Layout => GetValue(x => x.Layout);
+    public NameLayout Layout => GetValue(x => x.Layout);
 }
