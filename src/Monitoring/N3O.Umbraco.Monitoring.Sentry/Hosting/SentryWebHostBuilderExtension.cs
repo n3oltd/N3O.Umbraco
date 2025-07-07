44ab37d92aa137cb4b28e209c0216ccd5a543204
Noor <noor@ankabut.com>
2025-07-07 22:19:02 +0100

    Code cleanup

--- src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryWebHostBuilderExtension.cs
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryWebHostBuilderExtension.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using N3O.Umbraco.Composing;
-using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Hosting;
 
 namespace N3O.Umbraco.Monitoring.Sentry;
 
 public class SentryWebHostBuilderExtension : IWebHostBuilderExtension {
     public void Run(IWebHostBuilder webBuilder) {
-        if (webBuilder.GetSetting(WebHostDefaults.EnvironmentKey).EqualsInvariant(Environments.Production)) {
+        if (Composer.WebHostEnvironment.IsProduction()) {
             webBuilder.UseSentry(opt => {
-                opt.AttachStacktrace = true;
-                opt.Environment = Composer.WebHostEnvironment.EnvironmentName;
+                opt.InitializeSdk = false;
+                opt.MinimumEventLevel = LogLevel.Error;
+                opt.MinimumBreadcrumbLevel = LogLevel.Error;
             });
         }
     }
--- src/Monitoring/N3O.Umbraco.Monitoring.Sentry/SentryMonitoringComposer.cs
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/SentryMonitoringComposer.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Composing;
+using N3O.Umbraco.Monitoring.Sentry.Configuration;
+using Sentry;
+using Sentry.Extensibility;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
 
@@ -9,9 +13,21 @@ namespace N3O.Umbraco.Monitoring.Sentry;
 
 public class SentryMonitoringComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
-        builder.Services.AddTransient<OurEventProcessor>();
-        
+        builder.Services.AddTransient<ISentryEventProcessor, OurEventProcessor>();
+
         if (WebHostEnvironment.IsProduction()) {
+            var config = new SentryConfiguration();
+
+            builder.Config.GetSection("Sentry").Bind(config);
+            
+            SentrySdk.Init(opt => {
+                opt.Dsn = config.Dsn;
+                opt.ReportAssembliesMode = ReportAssembliesMode.InformationalVersion;
+                opt.SendDefaultPii = true;
+                opt.Environment = WebHostEnvironment.EnvironmentName;
+                opt.DiagnosticLevel = SentryLevel.Error;
+            });
+            
             builder.Services.Configure<UmbracoPipelineOptions>(opt => {
                 var filter = new UmbracoPipelineFilter("SentryMonitoring");
                 filter.Endpoints = app => app.UseSentryTracing();
