84d1edd242b7a67e7514657802889cde548b4ac3
Talha Malik <talha.malik@n3o.ltd>
2025-07-14 09:55:36 +0500

    Fix mapping

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Extensions/DesignationContentExtensions.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Extensions/DesignationContentExtensions.cs
@@ -21,7 +21,7 @@ public static class DesignationContentExtensions {
         fundDimensions.Dimension3 = ToPublishedDesignationFundDimension(designationContent.Dimension3,
                                                                         fundDimensionOptions.Dimension3Options);
         
-        fundDimensions.Dimension3 = ToPublishedDesignationFundDimension(designationContent.Dimension4,
+        fundDimensions.Dimension4 = ToPublishedDesignationFundDimension(designationContent.Dimension4,
                                                                         fundDimensionOptions.Dimension4Options);
 
         return fundDimensions;
