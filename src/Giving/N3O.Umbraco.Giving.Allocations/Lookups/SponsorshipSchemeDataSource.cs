95d8e3f5b6d37ed32c02e5caef2a502cd8bcc56c
Talha Malik <talha.malik@n3o.ltd>
2025-07-16 18:18:39 +0500

    Added data sources

---
+++ src/Giving/N3O.Umbraco.Giving.Allocations/Lookups/SponsorshipSchemeDataSource.cs
@@ -0,0 +1,13 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Giving.Allocations.Lookups;
+
+public class SponsorshipSchemeDataSource : LookupsDataSource<SponsorshipScheme> {
+    public SponsorshipSchemeDataSource(ILookups lookups) : base(lookups) { }
+    
+    public override string Name => "Sponsorship Schemes";
+    public override string Description => "Data source for sponsorship schemes";
+    public override string Icon => "icon-user-female color-black";
+
+    protected override string GetIcon(SponsorshipScheme currency) => "icon-user-female color-black";
+}
---
+++ src/Giving/N3O.Umbraco.Giving.Allocations/Models/FundDimensionValue/FundDimensionValueDataSource.cs
@@ -0,0 +1,33 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Giving.Allocations.Models;
+
+public abstract class FundDimensionValueDataSource<T> : LookupsDataSource<T> where T : ContentOrPublishedLookup {
+    public FundDimensionValueDataSource(ILookups lookups, int index) : base(lookups) {
+        Index = index;
+    }
+
+    private int Index { get; }
+    
+    public override string Name => $"Fund Dimension {Index} Values";
+    public override string Description => $"Data source for fund dimension {Index} values";
+    public override string Icon => "icon-checkbox";
+
+    protected override string GetIcon(T lookup) => "icon-checkbox";
+}
+
+public class FundDimensionValue1DataSource : FundDimensionValueDataSource<FundDimension1Value> {
+    public FundDimensionValue1DataSource(ILookups lookups) : base(lookups, 1) { }
+}
+
+public class FundDimensionValue2DataSource : FundDimensionValueDataSource<FundDimension2Value> {
+    public FundDimensionValue2DataSource(ILookups lookups) : base(lookups, 2) { }
+}
+
+public class FundDimensionValue3DataSource : FundDimensionValueDataSource<FundDimension3Value> {
+    public FundDimensionValue3DataSource(ILookups lookups) : base(lookups, 3) { }
+}
+
+public class FundDimensionValue4DataSource : FundDimensionValueDataSource<FundDimension4Value> {
+    public FundDimensionValue4DataSource(ILookups lookups) : base(lookups, 4) { }
+}
