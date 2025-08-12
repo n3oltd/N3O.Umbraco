36c0dc37802eb6916cd71c5a8e87d846e1b4f66d
Talha Malik <talha.malik@n3o.ltd>
2025-08-12 15:46:13 +0500

    Remove attribute

--- src/Search/N3O.Umbraco.Search.Typesense/TypesenseSearchComposer.cs
+++ src/Search/N3O.Umbraco.Search.Typesense/TypesenseSearchComposer.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using N3O.Umbraco.Attributes;
 using N3O.Umbraco.Composing;
 using N3O.Umbraco.Extensions;
 using System.Net;
@@ -11,7 +10,6 @@ using Umbraco.Cms.Core.DependencyInjection;
 
 namespace N3O.Umbraco.Search.Typesense;
 
-[Order(-1)]
 public class TypesenseSearchComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.Services.AddSingleton<IConfigureOptions<Config>, TypesenseOptions>();
