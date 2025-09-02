238b065d8fe0f8f270fdcb1811b0e58a034e24fc
Noor Drummond <noor@ankabut.com>
2025-09-02 15:44:23 +0100

    Marketing packages cleanup

--- src/Marketing/N3O.Umbraco.Marketing/App_Plugins/Umbraco.Engage/addons/ums-addons.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module("engage").constant("UMS_ADDONS", {
-    // This object can be extended by addons such as Umbraco.Engage.Forms to let the main
-    // package know they are installed.
-});
-//TODO DEPRECATE THIS METHOD AND USE THE API VERSION INSTEAD FOR FORMS
--- src/Marketing/N3O.Umbraco.Marketing/App_Plugins/Umbraco.Engage/engage-init.js
+++
@@ -1,5 +0,0 @@
-﻿// Register with Angular
-angular.module("engage", ["rzSlider", "as.sortable"]);
-
-// Register as dependency for Umbraco
-angular.module("umbraco").requires.push("engage");
