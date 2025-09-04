931ee9876063bcd096bcb40c3f13bf64804f3a95
Talha Malik <talha.malik@n3o.ltd>
2025-09-04 17:03:00 +0500

    Populate tag name

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Elements/ElementPublishingNotification.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Elements/ElementPublishingNotification.cs
@@ -1,22 +1,27 @@
-﻿using N3O.Umbraco.Cloud.Platforms.Content;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using N3O.Umbraco.Cloud.Platforms.Content;
 using N3O.Umbraco.Cloud.Platforms.Extensions;
+using N3O.Umbraco.Cloud.Platforms.Lookups;
 using N3O.Umbraco.Content;
 using System.Threading;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Extensions;
 
 namespace N3O.Umbraco.Cloud.Platforms.Notifications.Elements;
 
 public class ElementPublishingNotification : INotificationAsyncHandler<ContentPublishingNotification> {
     public Task HandleAsync(ContentPublishingNotification notification, CancellationToken cancellationToken) {
         foreach (var content in notification.PublishedEntities) {
-            if (content.IsDonationFormElement()) {
-                    content.SetValue(AliasHelper<ElementContent>.PropertyAlias(x => x.EmbedCode),
-                                     $"<n3o-donation-form id=\"{content.Key}\">");
-            } else if (content.IsDonateButtonElement()) {
-                content.SetValue(AliasHelper<ElementContent>.PropertyAlias(x => x.EmbedCode),
-                                 $"<n3o-donate-button id=\"{content.Key}\">");
+            if (content.IsDonationFormElement() || content.IsDonateButtonElement()) {
+                var type = content.IsDonationFormElement() ? ElementTypes.DonationForm : ElementTypes.DonateButton;
+                
+                var tag = new TagBuilder(type.TagName);
+        
+                tag.Attributes.Add("id", content.Key.ToString());
+        
+                content.SetValue(AliasHelper<ElementContent>.PropertyAlias(x => x.EmbedCode), tag.ToHtmlString());
             }
         }
         
