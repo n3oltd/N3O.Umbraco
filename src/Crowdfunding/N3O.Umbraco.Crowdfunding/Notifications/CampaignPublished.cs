895fdef0767286ce1fbb35251d914d3c8c95cf9e
Talha Malik <talha.malik@n3o.ltd>
2025-09-22 17:29:42 +0500

    Remove redundant code

--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Notifications/CampaignPublished.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Notifications/CampaignPublished.cs
@@ -1,14 +1,11 @@
-﻿using Flurl;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Attributes;
-using N3O.Umbraco.Cloud.Engage;
-using N3O.Umbraco.Content;
-using N3O.Umbraco.Crowdfunding.Content;
+using N3O.Umbraco.Crowdfunding.Commands;
 using N3O.Umbraco.Extensions;
-using N3O.Umbraco.Utilities;
-using N3O.Umbraco.Webhooks;
-using System.Collections.Generic;
+using N3O.Umbraco.Parameters;
+using N3O.Umbraco.Scheduler;
+using N3O.Umbraco.Scheduler.Extensions;
 using System.Threading;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Events;
@@ -18,50 +15,27 @@ namespace N3O.Umbraco.Crowdfunding.Notifications;
 
 [SkipDuringSync]
 public class CampaignPublished : INotificationAsyncHandler<ContentPublishedNotification> {
-    private readonly ICrowdfunderManager _crowdfunderManager;
-    private readonly IContentLocator _contentLocator;
     private readonly IWebHostEnvironment _webHostEnvironment;
+    private readonly IBackgroundJob _backgroundJob;
 
-    public CampaignPublished(ICrowdfunderManager crowdfunderManager,
-                             IContentLocator contentLocator,
-                             IWebHostEnvironment webHostEnvironment) {
-        _crowdfunderManager = crowdfunderManager;
+    public CampaignPublished(
+        IWebHostEnvironment webHostEnvironment,
+                             IBackgroundJob backgroundJob) {
         _webHostEnvironment = webHostEnvironment;
-        _contentLocator = contentLocator;
+        _backgroundJob = backgroundJob;
     }
 
-    public async Task HandleAsync(ContentPublishedNotification notification, CancellationToken cancellationToken) {
+    public Task HandleAsync(ContentPublishedNotification notification, CancellationToken cancellationToken) {
         if (_webHostEnvironment.IsProduction()) {
             foreach (var content in notification.PublishedEntities) {
                 if (content.ContentType.Alias.EqualsInvariant(CrowdfundingConstants.Campaign.Alias)) {
-                    var campaign = _contentLocator.ById<CampaignContent>(content.Key);
-                    var urlSettingsContent = _contentLocator.Single<UrlSettingsContent>();
-
-                    if (!campaign.Status.HasValue()) {
-                        await _crowdfunderManager.CreateCampaignAsync(campaign, GetWebhookUrls(urlSettingsContent));
-                    } else {
-                        await _crowdfunderManager.UpdateCrowdfunderAsync(campaign.Key.ToString(),
-                                                                         campaign,
-                                                                         campaign.ToggleStatus,
-                                                                         GetWebhookUrls(urlSettingsContent));
-                    }
+                    _backgroundJob.EnqueueCommand<CampaignPublishedNotification>(p => {
+                        p.Add<ContentId>(content.Key.ToString());
+                    });
                 }
             }
         }
-    }
-    
-    private IEnumerable<string> GetWebhookUrls(UrlSettingsContent urlSettingsContent) {
-        var webhookUrls = new List<string>();
         
-        webhookUrls.Add(GetWebhookUrl(urlSettingsContent.ProductionBaseUrl));
-        
-        return webhookUrls;
-    }
-
-    private string GetWebhookUrl(string baseUrl) {
-        var webhookUrl = new Url(baseUrl.TrimEnd('/'));
-        webhookUrl.AppendPathSegment($"umbraco/api/{WebhooksConstants.ApiName}/{CrowdfundingConstants.Webhooks.HookIds.Crowdfunder}");
-        
-        return webhookUrl.ToString();
+        return Task.CompletedTask;
     }
 }
--- src/Crowdfunding/N3O.Umbraco.Crowdfunding/Notifications/FundraiserPublished.cs
+++ src/Crowdfunding/N3O.Umbraco.Crowdfunding/Notifications/FundraiserPublished.cs
@@ -1,14 +1,11 @@
-﻿using Flurl;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Attributes;
-using N3O.Umbraco.Cloud.Engage;
-using N3O.Umbraco.Content;
-using N3O.Umbraco.Crowdfunding.Content;
+using N3O.Umbraco.Crowdfunding.Commands;
 using N3O.Umbraco.Extensions;
-using N3O.Umbraco.Utilities;
-using N3O.Umbraco.Webhooks;
-using System.Collections.Generic;
+using N3O.Umbraco.Parameters;
+using N3O.Umbraco.Scheduler;
+using N3O.Umbraco.Scheduler.Extensions;
 using System.Threading;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Events;
@@ -18,55 +15,26 @@ namespace N3O.Umbraco.Crowdfunding.Notifications;
 
 [SkipDuringSync]
 public class FundraiserPublished : INotificationAsyncHandler<ContentPublishedNotification> {
-    private readonly ICrowdfunderManager _crowdfunderManager;
-    private readonly IContentLocator _contentLocator;
     private readonly IWebHostEnvironment _webHostEnvironment;
+    private readonly IBackgroundJob _backgroundJob;
 
-    public FundraiserPublished(ICrowdfunderManager crowdfunderManager,
-                               IContentLocator contentLocator,
-                               IWebHostEnvironment webHostEnvironment) {
-        _crowdfunderManager = crowdfunderManager;
-        _contentLocator = contentLocator;
+    public FundraiserPublished(IWebHostEnvironment webHostEnvironment,
+                               IBackgroundJob backgroundJob) {
         _webHostEnvironment = webHostEnvironment;
+        _backgroundJob = backgroundJob;
     }
 
-    public async Task HandleAsync(ContentPublishedNotification notification, CancellationToken cancellationToken) {
+    public Task HandleAsync(ContentPublishedNotification notification, CancellationToken cancellationToken) {
         if (_webHostEnvironment.IsProduction()) {
             foreach (var content in notification.PublishedEntities) {
                 if (content.ContentType.Alias.EqualsInvariant(CrowdfundingConstants.Fundraiser.Alias)) {
-                    var fundraiser = _contentLocator.ById<FundraiserContent>(content.Key);
-                
-                    if (!fundraiser.Status.HasValue()) {
-                        await _crowdfunderManager.CreateFundraiserAsync(fundraiser, GetWebhookUrls());
-                    } else {
-                        await _crowdfunderManager.UpdateCrowdfunderAsync(fundraiser.Key.ToString(),
-                                                                         fundraiser,
-                                                                         fundraiser.ToggleStatus,
-                                                                         GetWebhookUrls());
-                    }
+                    _backgroundJob.EnqueueCommand<FundraiserPublishedNotification>(p => {
+                        p.Add<ContentId>(content.Key.ToString());
+                    });
                 }
             }
         }
-    }
-    
-    private IEnumerable<string> GetWebhookUrls() {
-        var urlSettingsContent = _contentLocator.Single<UrlSettingsContent>();
         
-        var webhookUrls = new List<string>();
-
-        if (_webHostEnvironment.IsProduction()) {
-            webhookUrls.Add(GetWebhookUrl(urlSettingsContent.ProductionBaseUrl));
-        } else {
-            webhookUrls.Add(GetWebhookUrl(urlSettingsContent.StagingBaseUrl));
-        }
-        
-        return webhookUrls;
-    }
-
-    private string GetWebhookUrl(string baseUrl) {
-        var webhookUrl = new Url(baseUrl.TrimEnd('/'));
-        webhookUrl.AppendPathSegment($"umbraco/api/{WebhooksConstants.ApiName}/{CrowdfundingConstants.Webhooks.HookIds.Crowdfunder}");
-
-        return webhookUrl.ToString();
+        return Task.CompletedTask;
     }
 }
