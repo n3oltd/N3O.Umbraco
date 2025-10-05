214183b33c04456a70a380bfe5242e60a969eeed
Talha Malik <talha.malik@n3o.ltd>
2025-10-05 13:09:04 +0500

    Updates

--- src/Sync/N3O.Umbraco.Sync.Extensions/Handlers/SyncContentHandler.cs
+++ src/Sync/N3O.Umbraco.Sync.Extensions/Handlers/SyncContentHandler.cs
@@ -1,4 +1,5 @@
-﻿using N3O.Umbraco.Extensions;
+﻿using N3O.Umbraco.Content;
+using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Mediator;
 using N3O.Umbraco.Sync.Extensions.Commands;
 using N3O.Umbraco.Sync.Extensions.Models;
@@ -18,29 +19,35 @@ public class SyncContentHandler : IRequestHandler<SyncContentCommand, SyncConten
     private static readonly string Document = global::Umbraco.Cms.Core.Constants.UdiEntityType.Document;
     
     private readonly IPublisherStateService _publisherStateService;
+    private readonly IContentLocator _contentLocator;
 
-    public SyncContentHandler(IPublisherStateService publisherStateService) {
+    public SyncContentHandler(IPublisherStateService publisherStateService, IContentLocator contentLocator) {
         _publisherStateService = publisherStateService;
+        _contentLocator = contentLocator;
     }
-    
+
     public async Task<None> Handle(SyncContentCommand req, CancellationToken cancellationToken) {
         var requestId = req.Model.RequestId.GetValueOrThrow();
+        var content = _contentLocator.ById(req.Model.ContentId.GetValueOrThrow());
 
         if (!_publisherStateService.HasProcess(requestId)) {
             var syncItem = new SyncItem();
             syncItem.Change = ChangeType.Create;
             syncItem.Flags = DependencyFlags.PublishedDependencies;
             syncItem.Udi = Udi.Create(Document, req.Model.ContentId.GetValueOrThrow());
-            // TODO
-            syncItem.Name = "";
-            
+            syncItem.Name = content.Name;
+
             _publisherStateService.Intiailize(requestId, req.Model.ServerAlias, PublishMode.Push, [syncItem]);
         }
         
-        var result = await _publisherStateService.Process(requestId);
+        SyncActionState state;
+        
+        do {
+            state = await _publisherStateService.Process(requestId);
+        } while (!state.IsComplete);
 
-        if (!result.Sucess) {
-            throw new Exception($"Sync of {req.Model.ContentId} failed with error: {result.Message}");
+        if (!state.Sucess) {
+            throw new Exception($"Sync of {req.Model.ContentId} failed with error: {state.Message}");
         }
         
         return None.Empty;
