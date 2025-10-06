4feee4940ac3152c31a2e5df39f1daf2acea4396
TalhaMalik <talha.malik@n3o.ltd>
2025-10-06 11:24:57 +0500

    Revert "Updates"
    This reverts commit 0320d8dc9e86c1f0acb7122f7c2bf78fb820dfe1.

--- src/Scheduler/N3O.Umbraco.Scheduler/Services/JobTrigger.cs
+++ src/Scheduler/N3O.Umbraco.Scheduler/Services/JobTrigger.cs
@@ -28,6 +28,8 @@ public class JobTrigger {
                                    IReadOnlyDictionary<string, string> parameterData) {
         var req = GetProxyReq(triggerKey, modelJson, parameterData);
         
+        parameterData.TryGetValue(SchedulerConstants.Parameters.Culture, out var culture);
+
         var baseUrl = GetBasUrl();
         
         using (var httpClient = new HttpClient()) {
@@ -38,8 +40,8 @@ public class JobTrigger {
             
             request.Headers.Add("accept", "*/*");
 
-            if (parameterData?.ContainsKey(SchedulerConstants.Parameters.Culture) == true) {
-                request.Headers.Add("Accept-Language", parameterData[SchedulerConstants.Parameters.Culture]);
+            if (culture.HasValue()) {
+                request.Headers.Add("Accept-Language", culture);
             }
 
             var response = await httpClient.SendAsync(request);
@@ -64,7 +66,7 @@ public class JobTrigger {
         req.CommandType = requestType;
         req.RequestType = modelType;
         req.RequestBody = modelJson;
-        req.ParameterData = parameterData?.ToDictionary();
+        req.ParameterData = parameterData.ToDictionary();
         
         return req;
     }
