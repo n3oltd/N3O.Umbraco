e7cd083c65ff1706ab6567ba0a6bb498347de515
TalhaMalik <talha.malik@n3o.ltd>
2025-10-21 15:04:42 +0500

    Add middleware for allowing all mime types for platforms templates

---
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Hosting/Middleware/PlatformsTemplatesMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using HeyRed.Mime;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace N3O.Umbraco.Cloud.Platforms.Hosting;
+
+public class PlatformsTemplatesMiddleware : IMiddleware {
+    private const string Prefix = "/platforms/templates/";
+    
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public PlatformsTemplatesMiddleware(IWebHostEnvironment webHostEnvironment) {
+        _webHostEnvironment = webHostEnvironment;
+    }
+    
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
+        var requestPath = context.Request.Path.Value;
+
+        if (requestPath != null &&
+            !requestPath.Contains("..") &&
+            requestPath.StartsWith(Prefix, StringComparison.InvariantCultureIgnoreCase)) {
+            var filePath = Path.Combine(_webHostEnvironment.WebRootPath, requestPath.TrimStart('/'));
+
+            if (File.Exists(filePath)) {
+                context.Response.ContentType = MimeTypesMap.GetMimeType(filePath);
+
+                await context.Response.SendFileAsync(filePath);
+            } else {
+                context.Response.StatusCode = 404;
+            }
+        } else {
+            await next(context);
+        }
+    }
+}
