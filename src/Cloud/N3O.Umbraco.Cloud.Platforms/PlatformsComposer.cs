802aa9f7ae76bf5084bef21de0ccd5e8ed339bee
TalhaMalik <talha.malik@n3o.ltd>
2025-10-23 16:08:41 +0500

    Register Middleware

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/PlatformsComposer.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/PlatformsComposer.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using N3O.Umbraco.Cloud.Platforms.Hosting;
 using N3O.Umbraco.Composing;
 using N3O.Umbraco.Extensions;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
 
 namespace N3O.Umbraco.Cloud.Platforms;
 
@@ -10,8 +15,23 @@ public class PlatformsComposer : Composer {
         builder.Services.AddOpenApiDocument(PlatformsConstants.BackOfficeApiName);
 
         builder.Services.AddSingleton<IPlatformsPageAccessor, PlatformsPageAccessor>();
+        builder.Services.AddScoped<PlatformsTemplatesMiddleware>();
         
         RegisterAll(t => t.ImplementsInterface<IPreviewTagGenerator>(),
                     t => builder.Services.AddTransient(typeof(IPreviewTagGenerator), t));
+        
+        builder.Services.Configure<UmbracoPipelineOptions>(opt => {
+            var filter = new UmbracoPipelineFilter(nameof(PlatformsTemplatesMiddleware));
+            
+            filter.PostPipeline = app => {
+                var runtimeState = app.ApplicationServices.GetRequiredService<IRuntimeState>();
+
+                if (runtimeState.Level == RuntimeLevel.Run) {
+                    app.UseMiddleware<PlatformsTemplatesMiddleware>();
+                }
+            };
+
+            opt.AddFilter(filter);
+        });
     }
 }
