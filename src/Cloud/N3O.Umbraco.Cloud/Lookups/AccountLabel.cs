fb25fa69a0bd9a35f6a2aaa42cacc0da42f9094f
Noor Drummond <noor@ankabut.com>
2025-10-28 09:59:30 +0000

    Added account labels

---
+++ src/Cloud/N3O.Umbraco.Cloud/Lookups/AccountLabel.cs
@@ -0,0 +1,12 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Cloud.Lookups;
+
+public class AccountLabel : NamedLookup {
+    public AccountLabel(string id, string name) : base(id, name) { }
+}
+
+public class AccountLabels : StaticLookupsCollection<AccountLabel> {
+    public static readonly AccountLabel FirstTimeDonor = new("firstTimeDonor", "First Time Donor");
+    public static readonly AccountLabel OrphanSponsor = new("orphanSponsor", "Orphan Sponsor");
+}
---
+++ src/Cloud/N3O.Umbraco.Cloud/Lookups/AccountLabelDataSource.cs
@@ -0,0 +1,13 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Cloud.Lookups;
+
+public class AccountLabelDataSource : LookupsDataSource<AccountLabel> {
+    public AccountLabelDataSource(ILookups lookups) : base(lookups) { }
+    
+    public override string Name => "Account Labels";
+    public override string Description => "Data source for account labels";
+    public override string Icon => "icon-tag";
+    
+    protected override string GetIcon(AccountLabel accountLabel) => "icon-tag";
+}
