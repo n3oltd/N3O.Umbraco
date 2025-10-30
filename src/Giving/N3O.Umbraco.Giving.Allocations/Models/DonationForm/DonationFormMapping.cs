89385c5c8c9e104fa0bc3144b544a3b6c67d20b4
Noor Drummond <noor@ankabut.com>
2025-10-30 15:48:49 +0000

    Code cleanup

--- src/Giving/N3O.Umbraco.Giving.Allocations/Models/DonationForm/DonationFormMapping.cs
+++ src/Giving/N3O.Umbraco.Giving.Allocations/Models/DonationForm/DonationFormMapping.cs
@@ -1,8 +1,8 @@
 using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Giving.Allocations.Content;
+using N3O.Umbraco.Localization;
 using N3O.Umbraco.Lookups;
 using System.Linq;
-using System.Threading;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
@@ -10,9 +10,11 @@ namespace N3O.Umbraco.Giving.Allocations.Models;
 
 public class DonationFormMapping : IMapDefinition {
     private readonly ILookups _lookups;
+    private readonly IVariationContextAccessor _variationContextAccessor;
 
-    public DonationFormMapping(ILookups lookups) {
+    public DonationFormMapping(ILookups lookups, IVariationContextAccessor variationContextAccessor) {
         _lookups = lookups;
+        _variationContextAccessor = variationContextAccessor;
     }
     
     public void DefineMaps(IUmbracoMapper mapper) {
@@ -22,7 +24,7 @@ public class DonationFormMapping : IMapDefinition {
     // Umbraco.Code.MapAll
     private void Map(DonationFormContent src, DonationFormRes dest, MapperContext ctx) {
         dest.Title = src.Title;
-        dest.Options = src.GetOptions(_lookups, new VariationContext(Thread.CurrentThread.CurrentCulture.Name))
+        dest.Options = src.GetOptions(_lookups, new VariationContext(LocalizationSettings.CultureCode))
                           .OrEmpty()
                           .Select(ctx.Map<DonationOptionContent, DonationOptionRes>)
                           .ToList();
