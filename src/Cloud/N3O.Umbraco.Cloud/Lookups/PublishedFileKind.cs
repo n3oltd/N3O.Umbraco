b2b7118b40c92024dd25f4eec775e128a1477319
aminjafer-n3o <129605331+aminjafer-n3o@users.noreply.github.com>
2025-12-15 16:30:48 +0500

    Platforms Crowdfunder Added

--- src/Cloud/N3O.Umbraco.Cloud/Lookups/PublishedFileKind.cs
+++ src/Cloud/N3O.Umbraco.Cloud/Lookups/PublishedFileKind.cs
@@ -13,6 +13,7 @@ public class PublishedFileKind : Lookup {
 
 public class PublishedFileKinds : StaticLookupsCollection<PublishedFileKind> {
     public static readonly PublishedFileKind Campaign = new("campaign", "n3o-campaign-id");
+    public static readonly PublishedFileKind Crowdfunder = new("crowdfunder", "n3o-crowdfunder-id");
     public static readonly PublishedFileKind Offering = new("offering", "n3o-offering-id");
     public static readonly PublishedFileKind Element = new("element", null);
     public static readonly PublishedFileKind Subscription = new("subscription", null);
