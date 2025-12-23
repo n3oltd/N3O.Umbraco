6fafddb9c24fb9b5b230067c72320c1fc1343185
TalhaMalik <talha.malik@n3o.ltd>
2025-12-23 17:10:17 +0500

    Temp change

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Cloud/CloudContentNotification.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Cloud/CloudContentNotification.cs
@@ -25,7 +25,7 @@ public abstract class CloudContentNotification {
         req.Body = body;
         req.Url = _cloudUrl.ForWebhook(HookId);
                 
-        _backgroundJob.EnqueueCommand<DispatchWebhookCommand, DispatchWebhookReq>(req);
+        //_backgroundJob.EnqueueCommand<DispatchWebhookCommand, DispatchWebhookReq>(req); TODO revert once the platforms work is done
     }
 
     protected abstract string HookId { get; }
