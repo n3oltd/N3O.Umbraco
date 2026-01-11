8fb8080a02efd731af911d35007f8f5fc635ff75
Noor Drummond <noor@ankabut.com>
2026-01-11 12:27:53 +0000

    Added ZakatCalculatorFieldType

---
+++ src/Cloud/N3O.Umbraco.Cloud.Zakat/Lookups/ZakatCalculatorFieldType.cs
@@ -0,0 +1,12 @@
+﻿using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Cloud.Zakat;
+
+public class ZakatCalculatorFieldType : NamedLookup {
+    public ZakatCalculatorFieldType(string id, string name) : base(id, name) { }
+}
+
+public class ZakatCalculatorFieldTypes : StaticLookupsCollection<ZakatCalculatorFieldType> {
+    public static readonly ZakatCalculatorFieldType Asset = new("asset", "Asset");
+    public static readonly ZakatCalculatorFieldType Liability = new("liability", "Liability");
+}
