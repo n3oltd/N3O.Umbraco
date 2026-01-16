849b9fab0dacfd9d4bdc689dee068dc3ecabdbb9
TalhaMalik <talha.malik@n3o.ltd>
2026-01-16 18:04:07 +0500

    Added data sources for content feeds

---
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Lookups/Api/Elements/ElementKindDataSource.ContentFeedCollection.cs
@@ -0,0 +1,18 @@
+﻿using N3O.Umbraco.Cloud.Platforms.Clients;
+using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Cloud.Platforms.Lookups;
+
+public class ContentFeedCollectionElementKindDataSource : ElementKindDataSource {
+    private readonly ILookups _lookups;
+    
+    public ContentFeedCollectionElementKindDataSource(ILookups lookups) : base(lookups) {
+        _lookups = lookups;
+    }
+    
+    public override  string Name => "Content Feed (Collection) Elements";
+    public override  string Description => "Data source for collection content feed elements";
+    public override  string Icon => "icon-thumbnails";
+
+    protected override ElementKind Kind => ElementKind.ContentFeedCollection;
+}
---
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Lookups/Api/Elements/ElementKindDataSource.ContentFeedLibrary.cs
@@ -0,0 +1,14 @@
+﻿using N3O.Umbraco.Cloud.Platforms.Clients;
+using N3O.Umbraco.Lookups;
+
+namespace N3O.Umbraco.Cloud.Platforms.Lookups;
+
+public class ContentFeedLibraryElementKindDataSource : ElementKindDataSource {
+    public ContentFeedLibraryElementKindDataSource(ILookups lookups) : base(lookups) { }
+    
+    public override  string Name => "Content Feed (Library) Elements";
+    public override  string Description => "Data source for library content feed elements";
+    public override  string Icon => "icon-categories";
+
+    protected override ElementKind Kind => ElementKind.ContentFeedLibrary;
+}
