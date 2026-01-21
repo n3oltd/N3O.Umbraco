e0e2d2b51122ae0fa275f1b315d9a08139afb5be
TalhaMalik <talha.malik@n3o.ltd>
2026-01-21 12:34:53 +0500

    Fix values

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Content/Zakat/ZakatCalculatorSectionSettingsContent.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Content/Zakat/ZakatCalculatorSectionSettingsContent.cs
@@ -8,7 +8,7 @@ namespace N3O.Umbraco.Cloud.Platforms.Content;
 
 [UmbracoContent(PlatformsConstants.Zakat.Settings.Calculator.Section.Alias)]
 public class ZakatCalculatorSectionSettingsContent : UmbracoContent<ZakatCalculatorSectionSettingsContent> {
-    public string Alias => GetAs(x => x.Alias);
+    public string Alias => GetValue(x => x.Alias);
     public string Name => Content().Name;
     public new IHtmlEncodedString Content => GetValue(x => x.Content);
     public IEnumerable<ZakatCalculatorFieldSettingsContent> Fields => Content().Children.As<ZakatCalculatorFieldSettingsContent>();
