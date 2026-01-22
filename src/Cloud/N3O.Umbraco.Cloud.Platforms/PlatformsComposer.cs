76d341b04e2ab6a4df384d7ac2cf55561f6df1bf
TalhaMalik <talha.malik@n3o.ltd>
2026-01-22 14:44:11 +0500

    register ICrowdfunderTemplatePublisher

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/PlatformsComposer.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/PlatformsComposer.cs
@@ -22,6 +22,9 @@ public class PlatformsComposer : Composer {
         RegisterAll(t => t.ImplementsInterface<IPreviewHtmlGenerator>(),
                     t => builder.Services.AddTransient(typeof(IPreviewHtmlGenerator), t));
         
+        RegisterAll(t => t.ImplementsInterface<ICrowdfunderTemplatePublisher>(),
+                    t => builder.Services.AddTransient(typeof(ICrowdfunderTemplatePublisher), t));
+        
         builder.Services.Configure<UmbracoPipelineOptions>(opt => {
             var filter = new UmbracoPipelineFilter(nameof(PlatformsTemplatesMiddleware));
             
