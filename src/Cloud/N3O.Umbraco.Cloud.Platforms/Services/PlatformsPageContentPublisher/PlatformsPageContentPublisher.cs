ac0cadff92b1e6339f102e669546a013d3b4506e
TalhaMalik <talha.malik@n3o.ltd>
2026-01-26 11:12:31 +0500

    Add non generic implementation

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Services/PlatformsPageContentPublisher/PlatformsPageContentPublisher.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Services/PlatformsPageContentPublisher/PlatformsPageContentPublisher.cs
@@ -5,10 +5,14 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace N3O.Umbraco.Cloud.Platforms;
 
-public abstract class PlatformsPageContentPublisher<T> : IPlatformsPageContentPublisher where T : IPublishedContent {
+public abstract class PlatformsPageContentPublisher : IPlatformsPageContentPublisher {
     public abstract bool IsPublisherFor(PlatformsSchema page);
 
-    public IEnumerable<PropertyContentReq> GetContentProperties(IPublishedContent publishedContent) {
+    public abstract IEnumerable<PropertyContentReq> GetContentProperties(IPublishedContent publishedContent);
+}
+
+public abstract class PlatformsPageContentPublisher<T> : PlatformsPageContentPublisher where T : IPublishedContent {
+    public override IEnumerable<PropertyContentReq> GetContentProperties(IPublishedContent publishedContent) {
         return GetContentProperties((T) publishedContent);
     }
     
