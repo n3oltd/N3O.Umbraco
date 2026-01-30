0542ad5ee60aecf76cb3cd7ba9fdf2a859b9dc93
TalhaMalik <talha.malik@n3o.ltd>
2026-01-30 09:30:52 +0500

    ADd dev flag to enable live testing

--- src/N3O.Umbraco.Extensions/Dev/DevFlags.Flags.cs
+++ src/N3O.Umbraco.Extensions/Dev/DevFlags.Flags.cs
@@ -4,4 +4,5 @@ public static partial class DevFlags {
     public static readonly string DisableNotificationRegistrations = nameof(DisableNotificationRegistrations);
     public static readonly string DisableTextLocalization = nameof(DisableTextLocalization);
     public static readonly string EnableExperimentalFeatures = nameof(EnableExperimentalFeatures);
+    public static readonly string EnableLiveTesting = nameof(EnableLiveTesting);
 }
