495b05efc0005691daea7ed9df64bb75315934ba
TalhaMalik <talha.malik@n3o.ltd>
2026-01-31 17:13:14 +0500

    Set the urls on the campaign and offerings

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Offerings/OfferingSending.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Notifications/Offerings/OfferingSending.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Flurl;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using N3O.Umbraco.Cloud.Extensions;
 using N3O.Umbraco.Cloud.Platforms.Clients;
 using N3O.Umbraco.Cloud.Platforms.Content;
 using N3O.Umbraco.Cloud.Platforms.Lookups;
 using N3O.Umbraco.Content;
 using N3O.Umbraco.Extensions;
+using N3O.Umbraco.Lookups;
 using System;
 using System.Linq;
 using System.Threading;
@@ -12,11 +14,20 @@ using System.Threading.Tasks;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Routing;
 using Umbraco.Extensions;
 
 namespace N3O.Umbraco.Cloud.Platforms.Notifications;
 
 public class OfferingSending : INotificationAsyncHandler<SendingContentNotification> {
+    private readonly IContentLocator _contentLocator;
+    private readonly IPublishedUrlProvider _publishedUrlProvider;
+    
+    public OfferingSending(IContentLocator contentLocator, IPublishedUrlProvider publishedUrlProvider) {
+        _contentLocator = contentLocator;
+        _publishedUrlProvider = publishedUrlProvider;
+    }
+
     public Task HandleAsync(SendingContentNotification notification, CancellationToken cancellationToken) {
         var isOffering = notification.Content
                                      .ContentTypeAlias
@@ -27,10 +38,26 @@ public class OfferingSending : INotificationAsyncHandler<SendingContentNotificat
         if (isOffering) {
             foreach (var variant in notification.Content.Variants) {
                 SetEmbedCode(variant, notification.Content.Key.GetValueOrDefault());
+                SetUrl( notification, variant);
             }
         }
         
         return Task.CompletedTask;
+    }
+    
+    private void SetUrl(SendingContentNotification notification, ContentVariantDisplay variant) {
+        if (variant.State == ContentSavedState.Published) {
+            var donateSlug = _contentLocator.Special(SpecialPages.Donate).Url();
+            var segments = _publishedUrlProvider.GetUrl(notification.Content.Key.GetValueOrThrow())
+                                                .Trim('/')
+                                                .Split('/', StringSplitOptions.RemoveEmptyEntries);
+            
+            var offeringSlug = string.Join("/", segments.Skip(segments.Length - 2));
+        
+            var  offeringUrl = Url.Combine(donateSlug, offeringSlug);
+        
+            notification.Content.Urls = [new UrlInfo(offeringUrl, true, null)];
+        }
     }
     
     private void SetEmbedCode(ContentVariantDisplay variant, Guid contentId) {
