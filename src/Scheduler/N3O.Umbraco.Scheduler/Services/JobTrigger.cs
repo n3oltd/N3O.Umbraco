ea5ef480a92c23f9b6c3837f40f9febf2def10bc
Noor Drummond <noor@ankabut.com>
2026-02-03 17:25:20 +0000

    Use address Kestrel is listening on for triggering jobs

--- src/Scheduler/N3O.Umbraco.Scheduler/Services/JobTrigger.cs
+++ src/Scheduler/N3O.Umbraco.Scheduler/Services/JobTrigger.cs
@@ -1,22 +1,24 @@
-using N3O.Umbraco.Extensions;
-using N3O.Umbraco.Hosting;
+using Flurl;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using N3O.Umbraco.Json;
 using N3O.Umbraco.Scheduler.Models;
-using N3O.Umbraco.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Umbraco.Cms.Core.Web;
 
 namespace N3O.Umbraco.Scheduler;
 
 public class JobTrigger {
     private readonly IJsonProvider _jsonProvider;
+    private readonly IServer _server;
 
-    public JobTrigger(IJsonProvider jsonProvider) {
+    public JobTrigger(IJsonProvider jsonProvider, IServer server) {
         _jsonProvider = jsonProvider;
+        _server = server;
     }
 
     [DisplayName("{0}")]
@@ -26,14 +28,16 @@ public class JobTrigger {
                                    IReadOnlyDictionary<string, string> parameterData) {
         var req = GetProxyReq(triggerKey, modelJson, parameterData);
         
-        var baseUrl = $"https://{EnvironmentData.GetOurValue("Canonical_Domain")}";
+        var addressFeature = _server.Features.Get<IServerAddressesFeature>();
+        
+        var url = new Url(addressFeature.Addresses.First()).AppendPathSegment("/umbraco/api/JobProxy/executeProxied");
         
         using (var httpClient = new HttpClient()) {
             httpClient.Timeout = TimeSpan.FromMinutes(30);
             
             var reqStr = _jsonProvider.SerializeObject(req);
             
-            var request = new HttpRequestMessage(HttpMethod.Post, $"{baseUrl}/umbraco/api/JobProxy/executeProxied");
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
             request.Content = new StringContent(reqStr, null, "application/json");
             
             request.Headers.Add("accept", "*/*");
