41ace4b2cf61569a4ccb54b59105bfd3a3f42368
Noor Drummond <noor@ankabut.com>
2026-02-03 18:56:35 +0000

    Code cleanup

--- src/Validation/N3O.Umbraco.Validation/Controllers/ValidatingApiController.cs
+++ src/Validation/N3O.Umbraco.Validation/Controllers/ValidatingApiController.cs
@@ -3,18 +3,18 @@ using N3O.Umbraco.Hosting;
 using System;
 using System.Threading.Tasks;
 
-namespace N3O.Umbraco.Validation.Hosting.Controllers;
+namespace N3O.Umbraco.Validation.Controllers;
 
-public class ValidatingApiController : ApiController {
+public abstract class ValidatingApiController : ApiController {
     private readonly IValidation _validation;
     private readonly Lazy<IValidationHandler> _validationHandler;
     
-    public ValidatingApiController(IValidation validation, Lazy<IValidationHandler> validationHandler) {
+    protected ValidatingApiController(IValidation validation, Lazy<IValidationHandler> validationHandler) {
         _validation = validation;
         _validationHandler = validationHandler;
     }
 
-    public async Task ValidateAsync<T>(T req) {
+    protected async Task ValidateAsync<T>(T req) {
         var failures = await _validation.ValidateModelAsync(req, true);
 
         if (failures.HasAny()) {
