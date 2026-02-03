9eb0beec309c11af19fe39a8339863c52083a373
Noor Drummond <noor@ankabut.com>
2026-02-03 17:09:25 +0000

    Fix path

--- src/N3O.Umbraco.Extensions/Hosting/StaticRedirects.cs
+++ src/N3O.Umbraco.Extensions/Hosting/StaticRedirects.cs
@@ -7,7 +7,7 @@ public static class StaticRedirects {
     private static readonly Dictionary<string, StaticRedirect> Redirects = new(StringComparer.InvariantCultureIgnoreCase);
 
     public static void Add(string oldPath, string newPath, bool temporary = false) {
-        Redirects.Add(Normalize(oldPath), new StaticRedirect(Normalize(newPath), temporary));
+        Redirects.Add(Normalize(oldPath), new StaticRedirect($"/{Normalize(newPath)}", temporary));
     }
 
     public static StaticRedirect Find(string path) {
