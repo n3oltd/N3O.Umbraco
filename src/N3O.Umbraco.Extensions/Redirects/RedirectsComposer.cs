0094dfc251ab79b0223a549734b919eed837eaee
TalhaMalik <talha.malik@n3o.ltd>
2026-02-04 20:48:24 +0500

    Added redirect middleware

--- src/N3O.Umbraco.Extensions/Redirects/RedirectsComposer.cs
+++ src/N3O.Umbraco.Extensions/Redirects/RedirectsComposer.cs
@@ -1,11 +1,38 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using N3O.Umbraco.Composing;
+using N3O.Umbraco.ContentFinders;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
 
 namespace N3O.Umbraco.Redirects;
 
+[ComposeBefore(typeof(ContentFindersComposer))]
 public class RedirectsComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.Services.AddSingleton<IRedirectManagement, RedirectManagement>();
+        
+        RegisterMiddleware(builder);
+    }
+    
+    private void RegisterMiddleware(IUmbracoBuilder builder) {
+        builder.Services.AddScoped<RedirectMiddleware>();
+        
+        builder.Services.Configure<UmbracoPipelineOptions>(opt => {
+            var filter = new UmbracoPipelineFilter(nameof(RedirectMiddleware));
+
+            filter.PrePipeline = app => {
+                var runtimeState = app.ApplicationServices.GetRequiredService<IRuntimeState>();
+
+                if (runtimeState.Level == RuntimeLevel.Run) {
+                    app.UseMiddleware<RedirectMiddleware>();
+                }
+            };
+
+            opt.PipelineFilters.Insert(0, filter);
+        });
     }
 }
