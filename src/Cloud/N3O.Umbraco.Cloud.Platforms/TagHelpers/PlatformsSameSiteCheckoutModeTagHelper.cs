6fe98693803b1c2ba165a280c11a464548dba446
TalhaMalik <talha.malik@n3o.ltd>
2026-02-04 21:14:11 +0500

    Tag helper for same site checkout

---
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/TagHelpers/PlatformsSameSiteCheckoutModeTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using N3O.Umbraco.Constants;
+using Umbraco.Extensions;
+
+namespace MuslimHands.Core.TagHelpers;
+
+[HtmlTargetElement($"{Prefixes.TagHelpers}platforms-checkout-mode")]
+public class PlatformsSameSiteCheckoutModeTagHelper : TagHelper {
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        output.TagName = null;
+        
+        var metaTag = new TagBuilder("meta");
+        metaTag.MergeAttribute("name", "n3o-checkout-same-site");
+        metaTag.MergeAttribute("content", "true");
+
+        metaTag.TagRenderMode = TagRenderMode.SelfClosing;
+
+        output.Content.AppendHtml(metaTag.ToHtmlString());
+    }
+}
