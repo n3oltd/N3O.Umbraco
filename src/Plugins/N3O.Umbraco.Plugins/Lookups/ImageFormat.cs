9df026007e714ff50a9f35a800a57ff2a9ae9ed8
aminjafer-n3o <129605331+aminjafer-n3o@users.noreply.github.com>
2026-02-10 23:28:31 +0500

    webp support

--- src/Plugins/N3O.Umbraco.Plugins/Lookups/ImageFormat.cs
+++ src/Plugins/N3O.Umbraco.Plugins/Lookups/ImageFormat.cs
@@ -4,6 +4,7 @@ using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Webp;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,6 +28,8 @@ public class ImageFormat : Lookup {
             return ImageFormats.Png;
         } else if (format == GifFormat.Instance) {
             return ImageFormats.Gif;
+        } else if (format == WebpFormat.Instance) {
+            return ImageFormats.Webp;
         } else {
             throw UnrecognisedValueException.For(format);
         }
@@ -37,6 +40,7 @@ public class ImageFormats : StaticLookupsCollection<ImageFormat> {
     public static readonly ImageFormat Gif = new("gif", ".gif", "image/gif", new GifEncoder { SkipMetadata = true});
     public static readonly ImageFormat Jpg = new("jpg", ".jpg", "image/jpeg", new JpegEncoder { SkipMetadata = true, Quality = 80 });
     public static readonly ImageFormat Png = new("png", ".png", "image/png", new PngEncoder { SkipMetadata = true, CompressionLevel = PngCompressionLevel.BestCompression });
+    public static readonly ImageFormat Webp = new("webp", ".webp", "image/webp", new WebpEncoder { SkipMetadata = true, Quality = 80 });
     
     public static IEnumerable<ImageFormat> GetAllFormats() => StaticLookups.GetAll<ImageFormats, ImageFormat>().ToArray();
 }
