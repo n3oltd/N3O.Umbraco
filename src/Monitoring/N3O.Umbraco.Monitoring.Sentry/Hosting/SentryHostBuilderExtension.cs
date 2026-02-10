fc0f75523f9bbeba48cd846621cd8925c58b8a82
TalhaMalik <talha.malik@n3o.ltd>
2026-02-10 09:30:53 +0500

    Temporarily allow logging on all environments

--- src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryHostBuilderExtension.cs
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryHostBuilderExtension.cs
@@ -7,12 +7,10 @@ namespace N3O.Umbraco.Monitoring.Sentry;
 public class SentryHostBuilderExtension : IHostBuilderExtension {
     public void Run(IHostBuilder webBuilder) {
         webBuilder.UseSerilog((context, _, configuration) => {
-            if (context.HostingEnvironment.IsProduction()) {
-                configuration.ReadFrom
-                             .Configuration(context.Configuration)
-                             .Enrich.FromLogContext()
-                             .WriteTo.Sentry();
-            }
+            configuration.ReadFrom
+                         .Configuration(context.Configuration)
+                         .Enrich.FromLogContext()
+                         .WriteTo.Sentry();
         });
     }
 }
