b70dc8a0c484c2b3e8cfba18a9572d5588a48a98
TalhaMalik <talha.malik@n3o.ltd>
2026-02-17 09:40:04 +0500

    Added logging

--- src/N3O.Umbraco.Extensions/ContentFinders/LastChanceFinder.cs
+++ src/N3O.Umbraco.Extensions/ContentFinders/LastChanceFinder.cs
@@ -1,7 +1,7 @@
+using Microsoft.Extensions.Logging;
 using N3O.Umbraco.Content;
 using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Lookups;
-using System.Net;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Routing;
 
@@ -9,17 +9,25 @@ namespace N3O.Umbraco.ContentFinders;
 
 public class LastChanceFinder : IContentLastChanceFinder {
     private readonly IContentCache _contentCache;
+    private readonly ILogger<LastChanceFinder> _logger;
 
-    public LastChanceFinder(IContentCache contentCache) {
+    public LastChanceFinder(IContentCache contentCache, ILogger<LastChanceFinder> logger) {
         _contentCache = contentCache;
+        _logger = logger;
     }
 
     public Task<bool> TryFindContent(IPublishedRequestBuilder request) {
         if (request != null && request.ResponseStatusCode == 404) {
-            var notFound = (int) HttpStatusCode.NotFound;
-            request.SetResponseStatus(notFound);
-            request.SetPublishedContent(_contentCache.Special(SpecialPages.NotFound));
+            _logger.LogError("No page found for path {Path}", request.AbsolutePathDecoded);
+            
+            request.SetIs404();
 
+            var notFoundPage = _contentCache.Special(SpecialPages.NotFound);
+            
+            _logger.LogError("Not found page with id {ID} found", notFoundPage.Key);
+            
+            request.SetPublishedContent(notFoundPage);
+            
             return Task.FromResult(true);
         }
 
