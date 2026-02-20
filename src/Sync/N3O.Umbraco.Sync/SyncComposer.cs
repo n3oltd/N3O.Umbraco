efc4eaf11b54dc25ca33415b944ed6746d524830
TalhaMalik <talha.malik@n3o.ltd>
2026-02-20 21:53:47 +0500

    Add form options test

--- src/Sync/N3O.Umbraco.Sync/SyncComposer.cs
+++ src/Sync/N3O.Umbraco.Sync/SyncComposer.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
 using N3O.Umbraco.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using uSync.BackOffice;
@@ -7,5 +9,11 @@ namespace N3O.Umbraco.Sync;
 public class SyncComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.AdduSync();
+        
+        builder.Services.Configure<FormOptions>(x => {
+            x.ValueLengthLimit = int.MaxValue;
+            x.MultipartBodyLengthLimit = int.MaxValue;
+            x.MemoryBufferThreshold = int.MaxValue;
+        });
     }
 }
