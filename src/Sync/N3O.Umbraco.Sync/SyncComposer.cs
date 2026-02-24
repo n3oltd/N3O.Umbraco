00387d9a41b1ed2656f72d7e5dd18abee19d76af
TalhaMalik <talha.malik@n3o.ltd>
2026-02-24 10:25:53 +0500

    Remove test code

--- src/Sync/N3O.Umbraco.Sync/SyncComposer.cs
+++ src/Sync/N3O.Umbraco.Sync/SyncComposer.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.Extensions.DependencyInjection;
 using N3O.Umbraco.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using uSync.BackOffice;
@@ -9,11 +7,5 @@ namespace N3O.Umbraco.Sync;
 public class SyncComposer : Composer {
     public override void Compose(IUmbracoBuilder builder) {
         builder.AdduSync();
-        
-        builder.Services.Configure<FormOptions>(x => {
-            x.ValueLengthLimit = int.MaxValue;
-            x.MultipartBodyLengthLimit = int.MaxValue;
-            x.MemoryBufferThreshold = int.MaxValue;
-        });
     }
 }
