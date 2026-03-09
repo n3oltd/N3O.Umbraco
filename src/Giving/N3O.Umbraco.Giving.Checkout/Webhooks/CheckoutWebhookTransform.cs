7a8f844882ae483e6dcf370198d2527f4fa3fc7c
TalhaMalik <talha.malik@n3o.ltd>
2026-03-09 20:56:30 +0500

    Revert "Assign price to allocation object calculated from price calculator"
    This reverts commit e8b7f4b8296eeabf73ff79ac001f6e9ba5253447.

--- src/Giving/N3O.Umbraco.Giving.Checkout/Webhooks/CheckoutWebhookTransform.cs
+++ src/Giving/N3O.Umbraco.Giving.Checkout/Webhooks/CheckoutWebhookTransform.cs
@@ -2,7 +2,6 @@ using Humanizer;
 using N3O.Umbraco.Content;
 using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Financial;
-using N3O.Umbraco.Giving.Allocations;
 using N3O.Umbraco.Giving.Allocations.Lookups;
 using N3O.Umbraco.Giving.Allocations.Models;
 using N3O.Umbraco.Json;
@@ -25,14 +24,9 @@ public class CheckoutWebhookTransform : WebhookTransform {
         AliasHelper<PaymentMethodSettingsContent<IPaymentMethodSettings>>.PropertyAlias(x => x.RestrictCollectionDaysTo);
 
     private readonly IContentCache _contentCache;
-    private readonly IPriceCalculator _priceCalculator;
 
-    public CheckoutWebhookTransform(IJsonProvider jsonProvider,
-                                    IContentCache contentCache,
-                                    IPriceCalculator priceCalculator) 
-        : base(jsonProvider) {
+    public CheckoutWebhookTransform(IJsonProvider jsonProvider, IContentCache contentCache) : base(jsonProvider) {
         _contentCache = contentCache;
-        _priceCalculator = priceCalculator;
     }
 
     public override bool IsTransform(object body) => body is Entities.Checkout;
@@ -162,11 +156,6 @@ public class CheckoutWebhookTransform : WebhookTransform {
             foreach (var customField in allocation.Feedback.OrEmpty(x => x.CustomFields)) {
                 allocationJObject[$"cf_{customField.Alias}"] = customField.GetJValue();
             }
-
-            var pricing = ((IHoldPricing) allocation.Feedback.Scheme).Pricing;
-            var price = _priceCalculator.InCurrency(pricing, allocation.FundDimensions, allocation.Value.Currency);
-
-            allocationJObject["value"]["amount"] = price.Amount;
 
             ((JArray) jObject[globalKey]).Add(allocationJObject);
             ((JArray) jObject[schemeKey]).Add(allocationJObject);
