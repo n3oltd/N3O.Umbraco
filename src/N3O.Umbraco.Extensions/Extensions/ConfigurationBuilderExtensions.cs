172599a577e27c43a53694564d5b587c77319aa0
TalhaMalik <talha.malik@n3o.ltd>
2026-04-01 14:12:57 +0500

    Added basic keyvault structure

---
+++ src/N3O.Umbraco.Extensions/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using N3O.Umbraco.Hosting;
+using N3O.Umbraco.Utilities;
+using System;
+using System.Linq;
+
+namespace N3O.Umbraco.Extensions;
+
+public static class ConfigurationBuilderExtensions {
+    public static void RunExtensions(this IConfigurationBuilder configurationBuilder, WebHostBuilderContext context) {
+        var extensions = OurAssemblies.GetTypes(t => t.IsConcreteClass() &&
+                                                     t.HasParameterlessConstructor() &&
+                                                     t.ImplementsInterface<IConfigurationBuilderExtension>())
+                                      .Select(t => (IConfigurationBuilderExtension) Activator.CreateInstance(t))
+                                      .ToList();
+
+        foreach (var extension in extensions) {
+            extension.Run(configurationBuilder, context);
+        }
+    }
+}
