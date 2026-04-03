684a219e58c208d611f667d88e694cfe850a756d
Noor Drummond <noor@ankabut.com>
2026-04-03 10:20:55 +0100

    Fix build errors

--- src/Telemetry/N3O.Umbraco.Telemetry/Services/TelemetryData/TelemetryData.I.cs
+++ src/Telemetry/N3O.Umbraco.Telemetry/Services/TelemetryData/TelemetryData.I.cs
@@ -1,6 +1,6 @@
 ﻿namespace N3O.Umbraco.Telemetry;
 
 public interface ITelemetryData {
+    string GetDeploymentVersion();
     string GetExtensionsVersion();
-    string GetSiteDeploymentVersion();
 }
