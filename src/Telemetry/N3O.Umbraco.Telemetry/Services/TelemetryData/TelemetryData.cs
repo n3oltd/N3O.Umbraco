e43eb23a66c8b6da72470e57c57c2af2e73bab39
Noor Drummond <noor@ankabut.com>
2026-04-03 09:02:55 +0100

    Update TelemetryData.cs
    Signed-off-by: Noor Drummond <noor@ankabut.com>

--- src/Telemetry/N3O.Umbraco.Telemetry/Services/TelemetryData/TelemetryData.cs
+++ src/Telemetry/N3O.Umbraco.Telemetry/Services/TelemetryData/TelemetryData.cs
@@ -6,17 +6,17 @@ using System.Reflection;
 namespace N3O.Umbraco.Telemetry;
 
 public class TelemetryData : ITelemetryData {
+    public string GetDeploymentVersion() {
+        var deploymentVersion = EnvironmentData.GetOurValue(EnvironmentVariables.DeploymentVersion);
+
+        return deploymentVersion;
+    }
+    
     public string GetExtensionsVersion() {
         var productVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
         
         var extensionsVersion = productVersion.Substring(0, productVersion.IndexOf('+'));
 
         return extensionsVersion;
-    }
-
-    public string GetSiteDeploymentVersion() {
-        var siteDeploymentVersion = EnvironmentData.GetOurValue(EnvironmentVariables.SiteDeploymentVersion);
-
-        return siteDeploymentVersion;
     }
 }
