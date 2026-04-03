2289913ea2114eb13a28aa7b222cefffb54db06d
Noor Drummond <noor@ankabut.com>
2026-04-03 09:01:23 +0100

    Rename SiteDeploymentVersion to DeploymentVersion
    Signed-off-by: Noor Drummond <noor@ankabut.com>

--- src/N3O.Umbraco.Extensions/Constants/EnvironmentVariables.cs
+++ src/N3O.Umbraco.Extensions/Constants/EnvironmentVariables.cs
@@ -1,7 +1,7 @@
 ﻿namespace N3O.Umbraco.Constants;
 
 public static class EnvironmentVariables {
-    public static string SiteDeploymentVersion = "SiteDeploymentVersion";
+    public static string DeploymentVersion = "DeploymentVersion";
     public static string SiteLanguage = "SiteLanguage";
     public static string SiteName = "SiteName";
 }
