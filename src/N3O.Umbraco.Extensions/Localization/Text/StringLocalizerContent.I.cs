62b09ff89f3c96482fe4d2e171080039e736f906
Noor Drummond <noor@ankabut.com>
2026-04-13 07:22:36 +0100

    Remove unnecessary blank line in IStringLocalizerContent
    Signed-off-by: Noor Drummond <noor@ankabut.com>

--- src/N3O.Umbraco.Extensions/Localization/Text/StringLocalizerContent.I.cs
+++ src/N3O.Umbraco.Extensions/Localization/Text/StringLocalizerContent.I.cs
@@ -2,6 +2,5 @@ namespace N3O.Umbraco.Localization;
 
 public interface IStringLocalizerContent {
     int CreateFolder(string name, int parentId);
-    
     int CreateTextContainer(string name, int folderId, int? containerId);
 }
