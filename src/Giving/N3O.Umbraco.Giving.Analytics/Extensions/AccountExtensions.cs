03f0c8c6eb58d4f96a5c0c8c3cd87e653f1d934d
aminjafer-n3o <129605331+aminjafer-n3o@users.noreply.github.com>
2026-04-15 18:41:20 +0500

    account extensions for meta advanced Matching User

---
+++ src/Giving/N3O.Umbraco.Giving.Analytics/Extensions/AccountExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using N3O.Umbraco.Accounts.Models;
+using N3O.Umbraco.Extensions;
+
+namespace N3O.Umbraco.Giving.Analytics.Extensions;
+
+public static class AccountExtensions {
+    public static object ToMetaAdvancedMatchingUser(this Account account) {
+        return new {
+                       email = account.Email.Address.Sha256(),
+                       phone = FormatPhoneNumber(account.Telephone.Number),
+                       firstName = account.Individual.Name.FirstName.ToLowerInvariant(),
+                       lastName = account.Individual.Name.LastName.ToLowerInvariant(),
+                       city = account.Address.Locality.RemoveWhitespace().ToLowerInvariant(),
+                       state = account.Address.AdministrativeArea.ToLowerInvariant()[..2],
+                       zip = account.Address.PostalCode,
+                       country = account.Address.Country.Iso2Code,
+                   };
+    }
+
+    private static string FormatPhoneNumber(string phoneNumber) {
+        return string.IsNullOrWhiteSpace(phoneNumber)
+            ? phoneNumber
+            : Regex.Replace(phoneNumber, @"\D", string.Empty);
+    }
+}
