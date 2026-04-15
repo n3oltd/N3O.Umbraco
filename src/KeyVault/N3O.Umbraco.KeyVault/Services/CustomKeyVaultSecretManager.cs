045cc4e2017ef6753b01452b8113f9716b984d9c
Noor Drummond <noor@ankabut.com>
2026-04-15 13:24:35 +0100

    Added explanatory comment
    Signed-off-by: Noor Drummond <noor@ankabut.com>

--- src/KeyVault/N3O.Umbraco.KeyVault/Services/CustomKeyVaultSecretManager.cs
+++ src/KeyVault/N3O.Umbraco.KeyVault/Services/CustomKeyVaultSecretManager.cs
@@ -3,6 +3,7 @@ using Azure.Security.KeyVault.Secrets;
 
 namespace N3O.Umbraco.KeyVault;
 
+// Certain third party configuration settings use underscores but these are not supported in key vault secret names
 public class CustomKeyVaultSecretManager : KeyVaultSecretManager {
     public override string GetKey(KeyVaultSecret secret) {
         return secret.Name.Replace("-", "_");
