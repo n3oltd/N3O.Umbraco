ef9f3a3f8f3b6100108dc2b67beecf51c534ea2f
Noor Drummond <noor@ankabut.com>
2026-04-15 15:08:25 +0100

    Code cleanup

--- src/Giving/N3O.Umbraco.Giving.Analytics/Extensions/AccountExtensions.cs
+++ src/Giving/N3O.Umbraco.Giving.Analytics/Extensions/AccountExtensions.cs
@@ -6,21 +6,18 @@ namespace N3O.Umbraco.Giving.Analytics.Extensions;
 
 public static class AccountExtensions {
     public static object ToMetaAdvancedMatchingUser(this Account account) {
+        if (account == null) {
+            return new object();
+        }
+        
         return new {
-                       email = account.Email.Address.Sha256(),
-                       phone = FormatPhoneNumber(account.Telephone.Number),
-                       firstName = account.Individual.Name.FirstName.ToLowerInvariant(),
-                       lastName = account.Individual.Name.LastName.ToLowerInvariant(),
-                       city = account.Address.Locality.RemoveWhitespace().ToLowerInvariant(),
-                       state = account.Address.AdministrativeArea.ToLowerInvariant()[..2],
-                       zip = account.Address.PostalCode,
-                       country = account.Address.Country.Iso2Code,
-                   };
-    }
-
-    private static string FormatPhoneNumber(string phoneNumber) {
-        return string.IsNullOrWhiteSpace(phoneNumber)
-            ? phoneNumber
-            : Regex.Replace(phoneNumber, @"\D", string.Empty);
+            email = account.Email?.Address?.Sha256(),
+            phone = account.Telephone.IfNotNull(x => x.Number, x => Regex.Replace(x, @"\D", "")),
+            firstName = account.Individual?.Name?.FirstName?.ToLowerInvariant(),
+            lastName = account.Individual?.Name?.LastName?.ToLowerInvariant(),
+            city = account.Address?.Locality?.RemoveWhitespace()?.ToLowerInvariant(),
+            zip = account.Address?.PostalCode,
+            country = account.Address?.Country?.Iso2Code
+        };
     }
 }
