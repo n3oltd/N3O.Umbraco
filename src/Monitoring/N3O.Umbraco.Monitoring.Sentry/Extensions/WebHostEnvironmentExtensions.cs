27eb2dd55205c3840e04acee9cfc27e3c5db4a28
TalhaMalik <talha.malik@n3o.ltd>
2026-06-03 18:04:03 +0500

    Move to extension methods

---
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Extensions/WebHostEnvironmentExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace N3O.Umbraco.Monitoring.Sentry.Extensions;
+
+public static class WebHostEnvironmentExtensions {
+    public static bool IsSentryEnabled(this IWebHostEnvironment webHostEnvironment) {
+        return webHostEnvironment.IsProduction() || webHostEnvironment.IsStaging();
+    }
+}
--- src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryHostBuilderExtension.cs
+++ src/Monitoring/N3O.Umbraco.Monitoring.Sentry/Hosting/SentryHostBuilderExtension.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using N3O.Umbraco.Hosting;
 using N3O.Umbraco.Monitoring.Sentry.Configuration;
+using N3O.Umbraco.Monitoring.Sentry.Extensions;
 using Serilog;
 
 namespace N3O.Umbraco.Monitoring.Sentry;
@@ -9,7 +10,7 @@ namespace N3O.Umbraco.Monitoring.Sentry;
 public class SentryHostBuilderExtension : IHostBuilderExtension {
     public void Run(IHostBuilder webBuilder) {
         webBuilder.UseSerilog((context, _, configuration) => {
-            if (context.HostingEnvironment.IsProduction() || context.HostingEnvironment.IsStaging()) {
+            if (context.HostingEnvironment.IsSentryEnabled()) {
                 var config = new SentryConfiguration();
 
                 context.Configuration.GetSection("Sentry").Bind(config);
