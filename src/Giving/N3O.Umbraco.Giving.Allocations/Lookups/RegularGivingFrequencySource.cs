42a88984eddf75a5938f0a5655ea7bb39b309737
talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>
2026-07-17 13:57:24 +0500

    Update RegularGivingFrequencySource.cs
    Signed-off-by: talhamalik4025 <105341627+talhamalik4025@users.noreply.github.com>

--- src/Giving/N3O.Umbraco.Giving.Allocations/Lookups/RegularGivingFrequencySource.cs
+++ src/Giving/N3O.Umbraco.Giving.Allocations/Lookups/RegularGivingFrequencySource.cs
@@ -5,7 +5,7 @@ namespace N3O.Umbraco.Giving.Allocations.Lookups;
 public class RegularGivingFrequencySource : LookupsDataSource<RegularGivingFrequency> {
     public RegularGivingFrequencySource(ILookups lookups) : base(lookups) { }
     
-    public override string Name => "Regular Giving Collection Frequency";
+    public override string Name => "Regular Giving Frequency";
     public override string Description => "Data source for regular giving frequency";
     public override string Icon => "icon-calendar-alt";
 
