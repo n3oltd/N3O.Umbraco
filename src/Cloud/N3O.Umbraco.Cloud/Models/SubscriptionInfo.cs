512ec95e4c7f5031a2d9ce46640770b9bcfb676e
Noor Drummond <noor@ankabut.com>
2026-07-31 22:05:22 +0100

    Rename SubscriptionDescriptor to SubscriptionId and emit codes in webhook URLs

--- src/Cloud/N3O.Umbraco.Cloud/Models/SubscriptionInfo.cs
+++ src/Cloud/N3O.Umbraco.Cloud/Models/SubscriptionInfo.cs
@@ -6,16 +6,16 @@ namespace N3O.Umbraco.Cloud.Models;
 
 public class SubscriptionInfo : Value {
     [JsonConstructor]
-    public SubscriptionInfo(DataRegion dataRegion, SubscriptionDescriptor descriptor) {
+    public SubscriptionInfo(DataRegion dataRegion, SubscriptionId subscriptionId) {
         DataRegion = dataRegion;
-        Descriptor = descriptor;
+        SubscriptionId = subscriptionId;
     }
 
     public DataRegion DataRegion { get; }
-    public SubscriptionDescriptor Descriptor { get; }
+    public SubscriptionId SubscriptionId { get; }
 
     protected override IEnumerable<object> GetAtomicValues() {
         yield return DataRegion;
-        yield return Descriptor;
+        yield return SubscriptionId;
     }
 }
