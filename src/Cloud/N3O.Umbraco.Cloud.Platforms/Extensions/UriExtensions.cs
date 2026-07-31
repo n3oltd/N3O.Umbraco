90849e12d4651a3b84d2a88b1ad8d91de194abbf
Noor Drummond <noor@ankabut.com>
2026-07-31 15:09:09 +0100

    Handle relative published URLs when rebasing on the site root

--- src/Cloud/N3O.Umbraco.Cloud.Platforms/Extensions/UriExtensions.cs
+++ src/Cloud/N3O.Umbraco.Cloud.Platforms/Extensions/UriExtensions.cs
@@ -1,4 +1,5 @@
 using Flurl;
+using N3O.Umbraco.Extensions;
 using N3O.Umbraco.Utilities;
 using System;
 
@@ -6,12 +7,12 @@ namespace N3O.Umbraco.Cloud.Platforms.Extensions;
 
 public static class UriExtensions {
     public static string RebaseOnSiteRoot(this Uri url, IUrlBuilder urlBuilder) {
-        if (url == null) {
+        if (!url.HasValue()) {
             return null;
         }
 
         var rootUrl = urlBuilder.Root();
-        var rebasedUrl = new Url(url.AbsolutePath);
+        var rebasedUrl = new Url(url.IsAbsoluteUri ? url.AbsolutePath : url.OriginalString);
 
         rebasedUrl.Scheme = rootUrl.Scheme;
         rebasedUrl.Host = rootUrl.Host;
